d115ddf39b44119f2ff59c35779209eeed10956c
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-09 05:30:49 +0300

    Update role.spawn.energy.transfer.js

--- Introduce/role.spawn.energy.transfer.js
+++ Introduce/role.spawn.energy.transfer.js
@@ -12,18 +12,26 @@ var roleSpawnEnergyTransfer = {
             creep.say('stop transfering');
         }
 
-        var targets = creep.room.find(FIND_STRUCTURES, {
-            filter: (structure) => {
-                 return (structure.structureType == STRUCTURE_SPAWN) &&
-                    structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-            }
-        });
+        if(!creep.memory.transfering && creep.store.getFreeCapacity() == 0) {
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                     return (structure.structureType == STRUCTURE_SPAWN) &&
+                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                }
+            });
         
-        if(targets.length > 0) {
-            creep.memory.transfering = true;
-            creep.say('transfer energy to spawn');
+            if(targets.length > 0) {
+                creep.memory.transfering = true;
+                creep.say('transfer energy to a spawn');
+            }
+        }
+
+        if(creep.memory.transfering) {
             if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+            }
+            else {
+                roleBuilder.run(creep);
             }
         }
         else {
