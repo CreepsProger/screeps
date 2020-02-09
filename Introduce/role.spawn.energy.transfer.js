7f6996e67642ba205bcce2b758bb6d4dcef4d57d
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-09 04:52:10 +0300

    Create role.spawn.energy.transfer.js

---
+++ Introduce/role.spawn.energy.transfer.js
@@ -0,0 +1,35 @@
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+
+var roleSpawnEnergyTransfer = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+
+        if(creep.memory.transfering && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.transfering = false;
+            creep.say('stop transfering');
+        }
+
+        var targets = creep.room.find(FIND_STRUCTURES, {
+            filter: (structure) => {
+                 return (structure.structureType == STRUCTURE_SPAWN) &&
+                    structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+            }
+        });
+        
+        if(targets.length > 0) {
+            creep.memory.transfering = true;
+            creep.say('transfer energy to spawn');
+            if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+            }
+        }
+        else {
+            roleBuilder.run(creep);
+        }
+    }
+};
+
+module.exports = roleSpawnEnergyTransfer;
