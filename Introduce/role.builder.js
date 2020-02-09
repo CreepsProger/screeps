42a5fbac90bcce5c74a0de5daf97ff8f3863e361
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-09 13:21:50 +0300

    Update role.builder.js

--- Introduce/role.builder.js
+++ Introduce/role.builder.js
@@ -24,7 +24,7 @@ var roleBuilder = {
             var err = creep.build(target);
             if(err == ERR_NOT_IN_RANGE) {
                 creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
-                creep.say('🚧😎');
+                creep.say('🚧');
             }
             else if(!err) {
                 creep.say('🚧');
