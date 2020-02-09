4617413edc79bd890b2a6f203d4c736677ccc9ca
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-09 10:14:20 +0300

    Update main.settings.js

--- Introduce/main.settings.js
+++ Introduce/main.settings.js
@@ -1,11 +1,19 @@
 var mainSettings = {
 
-    /** @param {Creep} creep **/
+    check: function() {
+        console.log('Checking settings');
+
+        console.log('creeps number = ', Memory.settings.creeps.number);
+        return mainSettings.check;
+    }
     init: function() {
+        console.log('Initializing settings');
+
         Memory.settings =
         { inited: true
         , creeps: { number: 6 }
         }
+        return mainSettings.check;
     }
 };
 
