7bda7cb6ced7e1c8c942173ec55f780cbf894aa1
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-09 04:09:01 +0300

    Update role.upgrader.js

--- Introduce/role.upgrader.js
+++ Introduce/role.upgrader.js
@@ -7,13 +7,12 @@ var roleUpgrader = {
 
         if(creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
             creep.memory.upgrading = false;
-            creep.say('🔄 harvest');
+            creep.say('stop upgrading');
         }
         if(!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
             creep.memory.upgrading = true;
             creep.say('⚡ upgrade');
         }
-
         if(creep.memory.upgrading) {
             if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
