47c433b7851e5e1134c65ba3bed89a893b196b55
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-10 06:06:28 +0300

    Update role.energy.harvester.js

--- Introduce/role.energy.harvester.js
+++ Introduce/role.energy.harvester.js
@@ -2,15 +2,50 @@ var roleEnergyHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
+        if(!creep.memory.movements)
+            creep.memory.movements = 0;
+        if(!creep.memory.lastmovements)
+            creep.memory.lastmovements = 0;
+        if(!creep.memory.target_index)
+            creep.memory.target_index = 0;
+
         if(creep.memory.harvesting && creep.store[RESOURCE_ENERGY] == 0) {
             creep.memory.harvesting = false;
         }
 
-        if(creep.store.getFreeCapacity() > 0) {
-            var sources = creep.room.find(FIND_SOURCES);
-            creep.say('⚡');
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+        if(!creep.memory.harvesting && creep.store.getFreeCapacity() > 0) {
+            var targets = creep.room.find(FIND_SOURCES);
+            if(targets.length > 0) {
+                creep.memory.harvesting = true;
+                creep.memory.target = targets[creep.memory.target_index % targets.length].id;
+            }
+        }
+
+        if(creep.memory.harvesting) {
+            var target = Game.getObjectById(creep.memory.target);
+            var err = creep.harvest(target);
+            if(err == ERR_NOT_IN_RANGE) {
+                creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+                creep.memory.movements += 1;
+                if(creep.memory.lastmovements && creep.memory.lastmovements < 2 * creep.memory.movements) {
+                    creep.memory.harvesting = false;
+                    creep.memory.target_index += 1;
+                    creep.say('❓');
+                }
+                else {
+                    creep.say('⚡');
+                }
+            }
+            else if(!err) {
+                creep.say('⚡');
+                creep.memory.lastmovements = creep.memory.movements;
+                creep.memory.movements = 0;
+            }
+            else {
+                creep.memory.lastmovements = 0;
+                creep.memory.movements = 0;
+                creep.memory.target_index = 0;
+                creep.memory.harvesting = false;
             }
         }
     }
