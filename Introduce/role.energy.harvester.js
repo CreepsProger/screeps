f811379597cb8efabf52e7401cf71634ac5dac93
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-12 15:46:48 +0300

    Update role.energy.harvester.js

--- Introduce/role.energy.harvester.js
+++ Introduce/role.energy.harvester.js
@@ -38,7 +38,7 @@ var roleEnergyHarvester = {
             }
         }
 
-        var maxHarvesterMovementsToSource = Math.max(100,Math.floor(2 * Memory.harvestersMovements.Value.movingAverage.delta / Memory.harvestersMovements.Count.movingAverage.delta));
+        var maxHarvesterMovementsToSource = Math.max(200,Math.floor(2 * Memory.harvestersMovements.Value.movingAverage.delta / Memory.harvestersMovements.Count.movingAverage.delta));
 
         if(creep.memory.harvesting) {
             var target = Game.getObjectById(creep.memory.target);
