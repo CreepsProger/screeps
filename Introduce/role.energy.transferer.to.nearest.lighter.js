05c59032a0b59094c3db5fc75703a578e206f1a4
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-14 06:08:47 +0300

    Update role.energy.transferer.to.nearest.lighter.js

--- Introduce/role.energy.transferer.to.nearest.lighter.js
+++ Introduce/role.energy.transferer.to.nearest.lighter.js
@@ -61,7 +61,7 @@ var roleEnergyTransfererToNearestLighter = {
             creep.memory.transfering.energy.to.nearest.lighter = false;
         }
 
-        if(!creep.memory.transfering.energy.to.nearest.lighter && creep.store[RESOURCE_ENERGY] > 45) {
+        if(!creep.memory.transfering.energy.to.nearest.lighter && creep.store[RESOURCE_ENERGY] > creep.store.getCapacity()/2) {
             var targets = lookNearestLighterForCreep(creep);
 //              var targets = creep.room.find(FIND_MY_CREEPS, {
 //                  filter: (nearestLighter) => {
