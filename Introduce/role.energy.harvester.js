e38a6d33aa4fa4ef970880bf0dc03cd6d2340d0f
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-15 13:37:44 +0300

    Update role.energy.harvester.js

--- Introduce/role.energy.harvester.js
+++ Introduce/role.energy.harvester.js
@@ -49,7 +49,7 @@ var roleEnergyHarvester = {
         if(creep.memory.harvesting) {
             var target = Game.getObjectById(creep.memory.target);
             var err = creep.harvest(target);
-            if(err == ERR_NOT_IN_RANGE || err == -12) {
+            if(err == ERR_NOT_IN_RANGE || err == ERR_NO_BODYPART) {
                 creep.moveTo(target, {visualizePaathStyle: {stroke: '#ffffff'}});
                 if(creep.memory.starttimemoving &&
                    Game.time - creep.memory.starttimemoving > maxHarvesterMovementsToSource) {
