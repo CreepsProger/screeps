d0dd6b9250d485b94483037627903bce708137cf
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-15 17:44:48 +0300

    Update role.energy.harvester.js

--- Introduce/role.energy.harvester.js
+++ Introduce/role.energy.harvester.js
@@ -67,9 +67,18 @@ var roleEnergyHarvester = {
                 }
             }
             else if(err == ERR_NO_BODYPART) {
-                var new_target = target.pos.findClosestByPath(FIND_MY_CREEPS, {
+                var new_target;
+                if(!new_target) {
+                    new_target = target.pos.findClosestByPath(FIND_MY_CREEPS, {
                     filter: (creep2) => {
                         return creep2.store.getUsedCapacity(RESOURCE_ENERGY) > creep2.store.getFreeCapacity(RESOURCE_ENERGY) &&
+                            creep.memory.weight < creep2.memory.weight;
+                        }
+                });
+                if(!new_target) {
+                    new_target = target.pos.findClosestByPath(FIND_MY_CREEPS, {
+                    filter: (creep2) => {
+                        return creep2.store.getUsedCapacity(RESOURCE_ENERGY) > creep2.store.getFreeCapacity(RESOURCE_ENERGY)/3 &&
                             creep.memory.weight < creep2.memory.weight;
                         }
                 });
