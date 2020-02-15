0ff5000a693cd5aeb07fe3cb57fa773ee14d6852
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-15 17:50:48 +0300

    Update role.upgrader.js

--- Introduce/role.upgrader.js
+++ Introduce/role.upgrader.js
@@ -37,7 +37,7 @@ var roleUpgrader = {
                 if(!new_target) {
                     new_target = creep.room.controller.pos.findClosestByPath(FIND_MY_CREEPS, {
                     filter: (creep2) => {
-                        return creep2.store.getUsedCapacity(RESOURCE_ENERGY) < creep2.store.getFreeCapacity(RESOURCE_ENERGY) &&
+                        return creep2.store.getFreeCapacity(RESOURCE_ENERGY) > 0 &&
                             creep.memory.weight > creep2.memory.weight;
                         }
                     });
