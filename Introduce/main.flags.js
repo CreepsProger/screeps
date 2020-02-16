f7b31d267079e09ea2f11d3bc575fbc74012256a
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-16 14:17:53 +0300

    Update main.flags.js

--- Introduce/main.flags.js
+++ Introduce/main.flags.js
@@ -1,7 +1,6 @@
-var mainFlags = {
 
     /** @param commit **/
-    checkMainCommit: function(commit) {
+export function checkMainCommit(commit) {
         if(!Memory.commits.main ||
            Memory.commits.main != commit ||
            Game.flags['commit']) {
@@ -50,108 +49,114 @@ var mainFlags = {
                    , Count: { v: 0, movingAverage: { vs: [0,1,2,3,4,5,6,7,8,9], i: 0, summ: 0, delta: 0, ma:0 }}
                    ,   Avg: { v: 0, movingAverage: { vs: [0,1,2,3,4,5,6,7,8,9], i: 0, summ: 0, delta: 0, ma:0 }}};
         }
-    }
-    
-    check: function() {
+    };
+
+
+export function checkFlags() {
     //Test;
     var T = Game.flags['T'];
     if(T) {
-    var N = 1234;
-    console.log( 'Ｔ', Math.trunc(Game.time/10000), Game.time%10000
-                  , T
-                  , 'String.fromCharCode(65):'
-                  , String.fromCharCode(65)
-                  , 'N:'
-                  , N
-                  , 'N.toString(16).toUpperCase():'
-                  , N.toString(16).toUpperCase()
-                 );
-      T.remove();
-   }
+        var N = 1234;
+        console.log( 'Ｔ', Math.trunc(Game.time/10000), Game.time%10000
+                    , T
+                    , 'String.fromCharCode(65):'
+                    , String.fromCharCode(65)
+                    , 'N:'
+                    , N
+                    , 'N.toString(16).toUpperCase():'
+                    , N.toString(16).toUpperCase()
+                   );
+        T.remove();
+    }
 
 
-   //Destroy Rampart;
-   var DR = Game.flags['DR'];
-   if(DR) {
-      var rampart = DR.pos.findClosestByPath(FIND_STRUCTURES, {
-         filter: (structure) => {
-            return structure.structureType == STRUCTURE_RAMPART;
-         }
-      });
-      if(rampart) {
-         var err = rampart.destroy();
-         console.log( '❌🌕', Math.trunc(Game.time/10000), Game.time%10000
-                     , 'Destroy Rampart:'
-                     , rampart
-                     , 'err:'
-                     , err
-                     , JSON.stringify(rampart)
-                     , JSON.stringify(DR));
-         if(!err) {
+    //Destroy Rampart;
+    var DR = Game.flags['DR'];
+    if(DR) {
+        var rampart = DR.pos.findClosestByPath(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return structure.structureType == STRUCTURE_RAMPART;
+            }
+        });
+        if(rampart) {
+            var err = rampart.destroy();
+            console.log( '❌🌕', Math.trunc(Game.time/10000), Game.time%10000
+                        , 'Destroy Rampart:'
+                        , rampart
+                        , 'err:'
+                        , err
+                        , JSON.stringify(rampart)
+                        , JSON.stringify(DR));
+            if(!err) {
+                DR.remove();
+            }
+        }
+        else {
             DR.remove();
-         }
-      }
-      else {
-         DR.remove();
-      }
-   }
-   //Destroy Extention;
-   var DE = Game.flags['DE'];
-   if(DE) {
-      var extention = DE.pos.findClosestByPath(FIND_MY_STRUCTURES, {
-         filter: (structure) => {
-            return structure.structureType == STRUCTURE_EXTENSION;
-         }
-      });
-      if(extention) {
-         var err = extention.destroy();
-         console.log( '❌🌕', Math.trunc(Game.time/10000), Game.time%10000
-                     , JSON.stringify(DE)
-                     , 'destroying extention:'
-                     , extention
-                     , 'err:'
-                     , err);
-         if(!err) {
+        }
+    }
+    
+    
+    //Destroy Extention;
+    var DE = Game.flags['DE'];
+    if(DE) {
+        var extention = DE.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+            filter: (structure) => {
+                return structure.structureType == STRUCTURE_EXTENSION;
+            }
+        });
+        
+        if(extention) {
+            var err = extention.destroy();
+            console.log( '❌🌕', Math.trunc(Game.time/10000), Game.time%10000
+                        , JSON.stringify(DE)
+                        , 'destroying extention:'
+                        , extention
+                        , 'err:'
+                        , err);
+            if(!err) {
+                DE.remove();
+            }
+        }
+        else {
             DE.remove();
-         }
-      }
-      else {
-         DE.remove();
-      }
-   }
-   //Remeve all constructions sites';
-   var RACS = Game.flags['RACS'];
-   if(RACS) {
-      console.log( '❌⚪️⚪️⚪️', Math.trunc(Game.time/10000), Game.time%10000
-                       , JSON.stringify(RACS)
-                       , JSON.stringify(Game.constructionSites));
-      for(var name in Game.constructionSites) {
-         var constructionSite = Game.constructionSites[name];
-         if(constructionSite) {
-            console.log( '✒️', Math.trunc(Game.time/10000), Game.time%10000
-                             , 'removing constructionSite:'
-                             , constructionSite);
-            constructionSite.remove();
-         }
-      }
-      RACS.remove();
-   }
-   //Flag1: Rerun last flag
-   var Flag1 = Game.flags['Flag1'];
-   if(Flag1) {
-      console.log( '🏳️‍✒️', Math.trunc(Game.time/10000), Game.time%10000
-                  , JSON.stringify(Flag1));
-      Flag1.remove();
-   }
-   for(var name in Game.flags) {
-      var flag = Game.flags[name];
-      if(flag) {
-         console.log( '🏳️‍🌈', Math.trunc(Game.time/10000), Game.time%10000
-                          , 'flag:'
-                          , flag);
-      }
-   }
-
+        }
+    }
+    
+    
+    //Remeve all constructions sites';
+    var RACS = Game.flags['RACS'];
+    if(RACS) {
+        console.log( '❌⚪️⚪️⚪️', Math.trunc(Game.time/10000), Game.time%10000
+                    , JSON.stringify(RACS)
+                    , JSON.stringify(Game.constructionSites));
+        for(var name in Game.constructionSites) {
+            var constructionSite = Game.constructionSites[name];
+            if(constructionSite) {
+                console.log( '✒️', Math.trunc(Game.time/10000), Game.time%10000
+                            , 'removing constructionSite:'
+                            , constructionSite);
+                constructionSite.remove();
+            }
+        }
+        RACS.remove();
+    }
+    
+    
+    //Flag1: Rerun last flag
+    var Flag1 = Game.flags['Flag1'];
+    if(Flag1) {
+        console.log( '🏳️‍✒️', Math.trunc(Game.time/10000), Game.time%10000
+                    , JSON.stringify(Flag1));
+        Flag1.remove();
+    }
+    
+    for(var name in Game.flags) {
+        var flag = Game.flags[name];
+        if(flag) {
+            console.log( '🏳️‍🌈', Math.trunc(Game.time/10000), Game.time%10000
+                        , 'flag:'
+                        , flag);
+        }
+    }
 };
-
-module.exports = mainFlags;
