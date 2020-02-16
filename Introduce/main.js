158381f189a8b1bc80575c6eafcb1bd39065b1f9
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-16 03:07:58 +0300

    Update main.js

--- Introduce/main.js
+++ Introduce/main.js
@@ -269,8 +269,8 @@ module.exports.loop = function () {
          var N = Memory.totals.CreepsNumber;
 
 
-         if(CL >= 4) err = tryCreateCreep(err, 0x873, 1, 50); // E 1300  Worker
-         if(CL >= 4) err = tryCreateCreep(err, 0x6D1, 1, 59); // E 1300   Miner
+         if(CL >= 4) err = tryCreateCreep(err, 0x882, 1, 50); // E 1300  Worker
+         if(CL >= 4) err = tryCreateCreep(err, 0x5F1, 1, 59); // E 1300   Miner
          if(CL >= 4) err = tryCreateCreep(err, 0x0BF, 1, 55); // E 1300 Carrier
 
          if(CL >= 3) err = tryCreateCreep(err, 0x444, N?0:1, 60); // E 800 Worker
