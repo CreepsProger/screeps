6643bf7ff0e69cae5390dab82d48f26fc9c04a48
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-18 00:16:44 +0300

    Create role.attacker.js

---
+++ Introduce/role.attacker.js
@@ -0,0 +1,41 @@
+var roleEnergyTransfererToNearestLighter = require('role.energy.transferer.to.nearest.lighter');
+
+var roleAttacker = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+        if(creep.memory.attacking && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.attacking = false;
+        }
+
+        if(!creep.memory.attacking &&
+           creep.store[RESOURCE_ENERGY] > 0 &&
+           creep.getActiveBodyparts(RANGED_ATTACK)) {
+           const targets = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 3);
+           if(targets.length > 0) {
+              creep.memory.target = targets[0].id;
+              }
+        }
+
+        if(creep.memory.attacking) {
+            var target = Game.getObjectById(creep.memory.target);
+            var err = creep.rangedAttack(target);
+            if(!err) {
+                console.log( '✒️', Math.trunc(Game.time/10000), Game.time%10000
+                           , creep.name, 'attaking hostile creeps'
+                           , target.name);
+                creep.say('RA');
+                creep.memory.attacking = false;
+            }
+            else {
+                creep.memory.attacking = false;
+                roleEnergyTransfererToNearestLighter.run(creep);
+            }
+        }
+        else {
+            roleEnergyTransfererToNearestLighter.run(creep);
+        }
+    }
+};
+
+module.exports = roleEnergyTransfererToNearestLighter;
