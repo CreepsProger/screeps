ac65ee1736b1529991a40987bd34e8402dba837c
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-18 00:01:47 +0300

    Update role.builder.js

--- Introduce/role.builder.js
+++ Introduce/role.builder.js
@@ -36,8 +36,8 @@ var roleBuilder = {
                 if(!new_target && creep.store.getUsedCapacity(RESOURCE_ENERGY) < creep.store.getFreeCapacity(RESOURCE_ENERGY)) {
                     new_target = creep.pos.findClosestByPath(FIND_MY_CREEPS, {
                     filter: (creep2) => {
-                        return creep2.store.getUsedCapacity(RESOURCE_ENERGY) > creep2.store.getFreeCapacity(RESOURCE_ENERGY) &&
-                            creep.memory.weight < creep2.memory.weight;
+                        return creep2.memory.building &&
+                            creep2.memory.weight < creep.memory.weight;
                         }
                     });
                     if(new_target)
