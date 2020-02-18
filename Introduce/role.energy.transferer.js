4cf9924644b8fab406a27648817df48afea396b8
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-18 15:28:52 +0300

    Update role.energy.transferer.js

--- Introduce/role.energy.transferer.js
+++ Introduce/role.energy.transferer.js
@@ -9,8 +9,8 @@ var roleEnergyTransferer = {
         }
 
         if(!creep.memory.transfering &&
-           (creep.store.getUsedCapacity[RESOURCE_ENERGY] > 0 && creep.store.getFreeCapacity() == 0) ||
-            (creep.store.getUsedCapacity[RESOURCE_ENERGY] > 0 && creep.memory.rerun)) {
+           (creep.store.getUsedCapacity(RESOURCE_ENERGY) > 0 && creep.store.getFreeCapacity() == 0) ||
+            (creep.store.getUsedCapacity(RESOURCE_ENERGY) > 0 && creep.memory.rerun)) {
             var target;
 
             if(!target) {
