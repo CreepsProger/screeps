f4408c8758839b5268f10f4863bfe9f6fa2c5d62
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-18 15:22:08 +0300

    Update role.builder.js

--- Introduce/role.builder.js
+++ Introduce/role.builder.js
@@ -11,7 +11,7 @@ var roleBuilder = {
 
         if(!creep.memory.building &&
            (creep.store.getUsedCapacity[RESOURCE_ENERGY] > 0 && creep.store.getFreeCapacity() == 0) ||
-            (creep.store.getUsedCapacity[RESOURCE_ENERGY] > 0 && creep.memory.rerun))) {
+            (creep.store.getUsedCapacity[RESOURCE_ENERGY] > 0 && creep.memory.rerun)) {
             var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
             if(targets.length > 0) {
                 creep.memory.building = true;
