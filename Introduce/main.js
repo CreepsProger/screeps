c1fd2d5c89b8a9b123254aefa16c645aeebbb1af
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-18 07:24:42 +0300

    Update main.js

--- Introduce/main.js
+++ Introduce/main.js
@@ -31,10 +31,10 @@ module.exports.loop = function () {
       var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
          filter: (structure) => { 
             if(structure.structureType == STRUCTURE_WALL) {
-               return structure.hits < 80000;// 8000 E = 10 * 8000 / 800 = 100
+               return structure.hits < 0;// 8000 E = 10 * 8000 / 800 = 100
             }
             if(structure.structureType == STRUCTURE_RAMPART) {
-               return structure.hits < 80000;// 8000 E = 10 * 8000 / 800 = 100
+               return structure.hits < 0;// 8000 E = 10 * 8000 / 800 = 100
             }
             return structure.hitsMax - structure.hits > 800;
          }
