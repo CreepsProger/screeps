921f3fda7f96b4e1c2995a7d55930cd82fe825f4
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-19 14:01:05 +0300

    Update role.noenergy.transferer.js

--- Introduce/role.noenergy.transferer.js
+++ Introduce/role.noenergy.transferer.js
@@ -4,35 +4,56 @@ var roleNoEnergyTransferer = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if(creep.memory.noenergytransferer &&
+        if(creep.memory.noenergytransfering &&
             creep.store.getUsedCapacity() == creep.store.getUsedCapacity(RESOURCE_ENERGY)) {
-            creep.memory.noenergytransferer = false;
+            creep.memory.noenergytransfering = false;
         }
 
-        if(!creep.memory.noenergytransferer &&
-          creep.store.getFreeCapacity() > 0 &&
+        if(!creep.memory.noenergytransfering &&
           creep.store.getUsedCapacity() > creep.store.getUsedCapacity(RESOURCE_ENERGY)) {
-            var target;
+            creep.memory.noenergytransfering = false;
+        }
 
+        if(creep.memory.noenergytransfering) {
+            var target;
             if(!target) {
                 target = creep.room.storage;
             }
             if(target) {
-                creep.memory.noenergytransferer = true;
-                creep.memory.target = target.id;
+                var energy_amount = creep.store.getUsedCapacity(RESOURCE_ENERGY);
+                var noenergy_amount = creep.store.getUsedCapacity() - energy_amount;
+                var err = creep.transfer(target);
+                if(err == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+                    creep.say('🚐💦');
+                    console.log( '🚐💦', Math.trunc(Game.time/10000), Game.time%10000
+                                , creep.name
+                                , 'moving for transfering no/energy(' + noenergy_amount + '/' + energy_amount + ') to:'
+                                , target.name?target.name:target.structureType);
+                }
+                else if(!err) {
+                    creep.say('💦');
+                    console.log( '💦', Math.trunc(Game.time/10000), Game.time%10000
+                                , creep.name
+                                , 'transfering no/energy to:'
+                                , target.name?target.name:target.structureType);
+                }
+                else {
+                    creep.memory.noenergytransfering = false;
+                    console.log( '💦⚠️', Math.trunc(Game.time/10000), Game.time%10000
+                                , creep.name
+                                , 'transfering no/energy to:'
+                                , target.name?target.name:target.structureType
+                                , 'with err:'
+                                , err);
+                }
+            }
+            else {
+                    creep.memory.noenergytransfering = false;
             }
         }
 
-        if(creep.memory.noenergytransferer) {
-            var target = Game.getObjectById(creep.memory.target);
-            var err = creep.transfer(target);
-            if(err == ERR_NOT_IN_RANGE) {
-                creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
-                creep.say('NE🚐');
-            }
-        }
-
-        if(!creep.memory.noenergytransferer) {
+        if(!creep.memory.noenergytransfering) {
             roleNext.run(creep);
         }
     }
