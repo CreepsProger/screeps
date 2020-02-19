2ce69aba06ce138abab4e423c16865ea2e1c001a
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-19 14:43:41 +0300

    Update role.builder.js

--- Introduce/role.builder.js
+++ Introduce/role.builder.js
@@ -12,66 +12,102 @@ var roleBuilder = {
         if(!creep.memory.building &&
            (creep.store.getUsedCapacity(RESOURCE_ENERGY) > 0 && creep.store.getFreeCapacity() == 0) ||
             (creep.store.getUsedCapacity(RESOURCE_ENERGY) > 0 && creep.memory.rerun)) {
-            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            if(targets.length > 0) {
-                creep.memory.building = true;
-                creep.memory.target = targets[0].id;
-            }
-//             else if(targets.length > 0) {
-//                 creep.memory.building = true;
-//                 creep.memory.target = targets[1].id;
-//             }
+            creep.memory.building = true;
         }
 
         if(creep.memory.building) {
-            var target = Game.getObjectById(creep.memory.target);
-            var err = creep.build(target);
-            if(err == ERR_NOT_IN_RANGE) {
-                creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
-                creep.say('🔜🏗');
+            var target;
+            if(!target) {
+                var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+                if(targets.length > 0) {
+                    creep.memory.target = targets[0].id;
+                }
             }
-            else if(err == ERR_NO_BODYPART) {
-                var new_target;
-                if(!new_target && creep.store.getUsedCapacity(RESOURCE_ENERGY) < creep.store.getFreeCapacity(RESOURCE_ENERGY)) {
-                    new_target = creep.pos.findClosestByPath(FIND_MY_CREEPS, {
-                    filter: (target_creep) => {
-                        return target_creep.memory.harvesting &&
-                            target_creep.memory.weight > creep.memory.weight;
-                        }
-                    });
-                    if(new_target)
-                        creep.say('🤫⚡🏗');
+
+            if(target) {
+                var err = creep.build(target);
+                if(err == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+                    creep.say('🔜🏗');
+                    console.log( '🔜🏗', Math.trunc(Game.time/10000), Game.time%10000
+                                , creep.name
+                                , 'moving for building:'
+                                , target.name?target.name:target.structureType);
                 }
-                if(!new_target) {
-                    new_target = creep.pos.findClosestByPath(FIND_MY_CREEPS, {
-                    filter: (target_creep) => {
-                        return target_creep.memory.building &&
-                            target_creep.store.getFreeCapacity(RESOURCE_ENERGY) > 50 &&
-                            target_creep.memory.weight < creep.memory.weight;
-                        }
-                    });
-                    if(new_target)
-                        creep.say('🤫🏗');
+                else if(!err) {
+                    creep.say('🏗');
+                    console.log( '🏗', Math.trunc(Game.time/10000), Game.time%10000
+                                , creep.name
+                                , 'building:'
+                                , target.name?target.name:target.structureType);
                 }
-                if(new_target) {
-                    creep.moveTo(new_target, {visualizePaathStyle: {stroke: '#ffffff'}});
-                    creep.memory.target = new_target.id;
-                }
-                else
+                else {
                     creep.memory.building = false;
-            }
-            else if(!err) {
-                creep.say('🏗');
+                    console.log( '🏗⚠️', Math.trunc(Game.time/10000), Game.time%10000
+                                , creep.name
+                                , 'building:'
+                                , target.name?target.name:target.structureType
+                                , 'with err:'
+                                , err);
+                }
             }
             else {
-                creep.memory.building = false;
-                roleNext.run(creep);
+                    creep.memory.building = false;
             }
         }
-        else {
+
+        if(!creep.memory.building) {
             roleNext.run(creep);
         }
     }
 };
 
+
 module.exports = roleBuilder;
+
+                
+                
+                
+//             else if(err == ERR_NO_BODYPART) {
+//                 var new_target;
+//                 if(!new_target && creep.store.getUsedCapacity(RESOURCE_ENERGY) < creep.store.getFreeCapacity(RESOURCE_ENERGY)) {
+//                     new_target = creep.pos.findClosestByPath(FIND_MY_CREEPS, {
+//                     filter: (target_creep) => {
+//                         return target_creep.memory.harvesting &&
+//                             target_creep.memory.weight > creep.memory.weight;
+//                         }
+//                     });
+//                     if(new_target)
+//                         creep.say('🤫⚡🏗');
+//                 }
+//                 if(!new_target) {
+//                     new_target = creep.pos.findClosestByPath(FIND_MY_CREEPS, {
+//                     filter: (target_creep) => {
+//                         return target_creep.memory.building &&
+//                             target_creep.store.getFreeCapacity(RESOURCE_ENERGY) > 50 &&
+//                             target_creep.memory.weight < creep.memory.weight;
+//                         }
+//                     });
+//                     if(new_target)
+//                         creep.say('🤫🏗');
+//                 }
+//                 if(new_target) {
+//                     creep.moveTo(new_target, {visualizePaathStyle: {stroke: '#ffffff'}});
+//                     creep.memory.target = new_target.id;
+//                 }
+//                 else
+//                     creep.memory.building = false;
+//             }
+//             else if(!err) {
+//                 creep.say('🏗');
+//             }
+//             else {
+//                 creep.memory.building = false;
+//                 roleNext.run(creep);
+//             }
+//         }
+//         else {
+//             roleNext.run(creep);
+//         }
+//     }
+// };
