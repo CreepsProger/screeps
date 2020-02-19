e3ada933d32b68b6132b722800d3330b3cbfed6d
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-19 12:44:20 +0300

    Update main.js

--- Introduce/main.js
+++ Introduce/main.js
@@ -86,7 +86,7 @@ module.exports.loop = function () {
    if(!Spawn.spawning) {
         if(Spawn.store.getUsedCapacity(RESOURCE_ENERGY) > 0) {
             Spawn.pos.findInRange(FIND_MY_CREEPS, 1).forEach(function(creep) {
-                if(creep.ticksToLive < 1000) {
+                if(creep.ticksToLive < 1000 && creep.weight != 59) {
                    if(OK == Spawn.renewCreep(creep)) {
                       console.log( '✒️', Math.trunc(Game.time/10000), Game.time%10000
                                   , Spawn.name, 'renew', creep.name);
