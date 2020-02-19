4e886af8a515467edabb1df75786ef6758445de8
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-19 12:19:59 +0300

    Update role.energy.transferer.js

--- Introduce/role.energy.transferer.js
+++ Introduce/role.energy.transferer.js
@@ -35,7 +35,7 @@ var roleEnergyTransferer = {
                 target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
                     filter: (structure) => {
                         return (structure.structureType == STRUCTURE_SPAWN) &&
-                            structure.store.getFreeCapacity(RESOURCE_ENERGY) < 100;
+                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 100;
                     }
                 });
             }
