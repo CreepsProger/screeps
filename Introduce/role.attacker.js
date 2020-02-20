01d1411d2d46c12ba3ce5057c8230547a251aee6
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-21 02:55:45 +0300

    Update role.attacker.js

--- Introduce/role.attacker.js
+++ Introduce/role.attacker.js
@@ -9,31 +9,59 @@ var roleAttacker = {
         }
 
         if(!creep.memory.attacking &&
-           creep.store.getUsedCapacity(RESOURCE_ENERGY) > 0 &&
-           creep.getActiveBodyparts(RANGED_ATTACK)) {
-           const targets = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 3);
-           if(targets.length > 0) {
-               creep.memory.target = targets[0].id;
-               creep.memory.attacking = true;
-           }
+           creep.getActiveBodyparts(RANGED_ATTACK) > 0 &&
+           creep.getActiveBodyparts(CARRY) == 0) {
+            creep.memory.attacking = true;
         }
 
         if(creep.memory.attacking) {
-            var target = Game.getObjectById(creep.memory.target);
-            var err = creep.rangedAttack(target);
-            if(!err) {
-                console.log( '✒️', Math.trunc(Game.time/10000), Game.time%10000
-                           , creep.name, 'attaking hostile creeps'
-                           , target.name);
-                creep.say('RA');
-                creep.memory.attacking = false;
+            var target;
+            if(!target) {
+                const targets = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 3);
+                if(targets.length > 0) {
+                    target = targets[0];
+                }
+            }
+            if(!target) {
+                target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+            }
+            if(!target) {
+                target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+            }
+            if(target)
+            {
+                var err = creep.rangedAttack(target);
+                if(err == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+                    creep.say('🔜💡');
+                    console.log( '🔜💡', Math.trunc(Game.time/10000), Game.time%10000
+                                , creep.name
+                                , 'moving for transfering energy to:'
+                                , target.name?target.name:target.structureType);
+                }
+                else if(!err) {
+                    creep.say('💡');
+                    console.log( '💡', Math.trunc(Game.time/10000), Game.time%10000
+                                , creep.name
+                                , 'transfering energy to:'
+                                , target.name?target.name:target.structureType);
+                }
+                else {
+                    console.log( '💡⚠️', Math.trunc(Game.time/10000), Game.time%10000
+                                , creep.name
+                                , 'transfering energy to:'
+                                , target.name?target.name:target.structureType
+                                , 'with err:'
+                                , err);
+                    creep.memory.attacking = false;
+                }
             }
             else {
-                creep.memory.attacking = false;
-                roleNext.run(creep);
+                    creep.memory.attacking = false;
             }
         }
-        else {
+
+        if(!creep.memory.attacking) {
             roleNext.run(creep);
         }
     }
