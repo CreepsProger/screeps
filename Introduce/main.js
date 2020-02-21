c028eceb34433e82a0f482ccfacb8e0033dc57b1
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-21 04:06:16 +0300

    Update main.js

--- Introduce/main.js
+++ Introduce/main.js
@@ -148,7 +148,8 @@ module.exports.loop = function () {
          var N = Memory.totals.CreepsNumber;
 
          if(CL >= 4) mainFlags.tryCreateCreep(err,       40404, N<(maxCreepsNumber+1)?5:0, 50); // V 1-1 E 800    Worker
-         if(CL >= 4) mainFlags.tryCreateCreep(err, 50100000006, N<(maxCreepsNumber+1)?2:0, 50); // V 1-1 E 800    Attaker
+//          if(CL >= 4) mainFlags.tryCreateCreep(err, 50100000006, N<(maxCreepsNumber+1)?2:0, 50); // V 1-1 E 800    Attaker
+         if(CL >= 4) mainFlags.tryCreateCreep(err,   200000002, N<(maxCreepsNumber+1)?2:0, 50); // V 1-1 E 800    Attaker
 //          if(CL >= 4) mainFlags.tryCreateCreep(err,     80804, N<(maxCreepsNumber+1)?1:0, 50); // V 1-2 E 1300    Worker
 //          if(CL >= 4) mainFlags.tryCreateCreep(err,     60603, N<(maxCreepsNumber+1)?1:0, 59); // V 1-2 E 1300 Harvester
 //          if(CL >= 4) mainFlags.tryCreateCreep(err, 400001204, N<(maxCreepsNumber+1)?1:0, 55); // V 1-2 E 1400   Carrier
