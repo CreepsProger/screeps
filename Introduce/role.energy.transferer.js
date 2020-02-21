71ff6cac59ec49f67c8df065d15f2d1a652968ad
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-21 10:04:50 +0300

    Update role.energy.transferer.js

--- Introduce/role.energy.transferer.js
+++ Introduce/role.energy.transferer.js
@@ -104,7 +104,7 @@ var roleEnergyTransferer = {
                 if(err == ERR_NOT_IN_RANGE) {
                     creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
                     creep.say('🔜💡');
-                    if(Game.flags['LEH'] || Game.flags['L']) {
+                    if(Game.flags['LET'] || Game.flags['LE'] || Game.flags['L']) {
                         console.log( '🔜💡', Math.trunc(Game.time/10000), Game.time%10000
                                     , creep.name
                                     , 'moving for transfering energy to:'
@@ -113,7 +113,7 @@ var roleEnergyTransferer = {
                 }
                 else if(!err) {
                     creep.say('💡');
-                    if(Game.flags['LEH'] || Game.flags['L']) {
+                    if(Game.flags['LET'] || Game.flags['LE'] || Game.flags['L']) {
                         console.log( '💡', Math.trunc(Game.time/10000), Game.time%10000
                                     , creep.name
                                     , 'transfering energy to:'
@@ -122,7 +122,7 @@ var roleEnergyTransferer = {
                 }
                 else {
                     creep.memory.transfering = false;
-                    if(Game.flags['LEH'] || Game.flags['L']) {
+                    if(Game.flags['LET'] || Game.flags['LE'] || Game.flags['L']) {
                         console.log( '💡⚠️', Math.trunc(Game.time/10000), Game.time%10000
                                     , creep.name
                                     , 'transfering energy to:'
