8f773fb581bf1f3c07963c707cdac6851cf6d1f2
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-21 11:21:58 +0300

    Update main.js

--- Introduce/main.js
+++ Introduce/main.js
@@ -164,9 +164,9 @@ module.exports.loop = function () {
          var err = ERR_NOT_ENOUGH_ENERGY;
          var N = Memory.totals.CreepsNumber;
 
-         if(CL >= 4) mainFlags.tryCreateCreep(err, 5000100000006, N<(maxCreepsNumber+1)?10:0, 20); // V 1-1 E  500  Attaker
-         if(CL >= 4) mainFlags.tryCreateCreep(err,          1212, N<(maxCreepsNumber+1)? 1:0, 55); // V 1-1 E 1800  Claimer
-         if(CL >= 4) mainFlags.tryCreateCreep(err,         40404, N<(maxCreepsNumber+1)? 4:0, 50); // V 1-1 E  800   Worker
+         if(CL >= 4) mainFlags.tryCreateCreep(err,          1212, N<(maxCreepsNumber+1)? 1:0, 55); // V 1-1 E 1800  Carrier
+         if(CL >= 4) mainFlags.tryCreateCreep(err,         40404, N<(maxCreepsNumber+1)? 5:0, 50); // V 1-1 E  800   Worker
+         if(CL >= 4) mainFlags.tryCreateCreep(err, 5000300000008, N<(maxCreepsNumber+1)?10:0, 20); // V 1-1 E  900  Attaker
 //          if(CL >= 4) mainFlags.tryCreateCreep(err,   200000002, N<(maxCreepsNumber+1)?2:0, 50); // V 1-1 E 800    Attaker
 //          if(CL >= 4) mainFlags.tryCreateCreep(err,     80804, N<(maxCreepsNumber+1)?1:0, 50); // V 1-2 E 1300    Worker
 //          if(CL >= 4) mainFlags.tryCreateCreep(err,     60603, N<(maxCreepsNumber+1)?1:0, 59); // V 1-2 E 1300 Harvester
