85c5e6ebb2a47c1172ace69dd98ed58229a61a4e
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-22 04:52:36 +0300

    Update main.log.js

--- Introduce/main.log.js
+++ Introduce/main.log.js
@@ -2,15 +2,13 @@
 var mainLog = {
 
   canLog: function(flagNames) {
-    checkFlags: function(ticksToLog = 10) {
-        var flags = [];
-        for(var name in flagNames) {
-            var flag = Game.flags[name];
-            if(!!Game.flags[name]) {
-              return true;
-            }
-        }
+    var flags = [];
+    for(var name in flagNames) {
+      var flag = Game.flags[name];
+      if(!!Game.flags[name]) {
+        return true;
+      }
     }
-};
+  };
 
 module.exports = mainLog;
