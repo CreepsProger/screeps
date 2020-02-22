30f935aede84f38df5ea8624fcb8eeb66964df30
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-22 16:49:50 +0300

    Update role.energy.harvester.js

--- Introduce/role.energy.harvester.js
+++ Introduce/role.energy.harvester.js
@@ -1,138 +1,217 @@
 // var roleRerun= require('role.attacker');
 
-var roleEnergyHarvester = {
-    
-    
-    /** @param {Creep} creep **/
-    run: function(creep) {
-        var commit = 4;
-        if(!Memory.commits.EnergyHarvester ||
-           Memory.commits.EnergyHarvester != commit) {
-            Memory.commits.EnergyHarvester = commit;
-            console.log( '✒️', Math.trunc(Game.time/10000), Game.time%10000
-                        , 'Commit EnergyHarvester'
-                        , Memory.commits.EnergyHarvester);
-        }         
+var flags = require('main.flags');
+var log = require('main.log');
 
-        if(creep.memory.harvesting &&
-           creep.store.getFreeCapacity() == 0) {
-            creep.memory.harvesting = false;
-        }
+var git = '$Format:%H$';
 
-        if(!creep.memory.harvesting &&
-           ((creep.store.getUsedCapacity(RESOURCE_ENERGY) == 0 && creep.store.getFreeCapacity(RESOURCE_ENERGY) > 0) ||
-            (creep.memory.rerun && creep.store.getUsedCapacity(RESOURCE_ENERGY) > 0 && creep.store.getFreeCapacity(RESOURCE_ENERGY) > 0))) {
-            creep.memory.harvesting = true;
-        }
-        
-        if(creep.memory.harvesting) {
-            var target;
-            if(!target && creep.room.energyAvailable != creep.room.energyCapacityAvailable) {
-                var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_CONTAINER) &&
-                            structure.store.getUsedCapacity(RESOURCE_ENERGY) > 0;
-                    }
-                });
-            }
-            if(!target && creep.room.energyAvailable != creep.room.energyCapacityAvailable) {
-                var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_STORAGE) &&
-                            structure.store.getUsedCapacity(RESOURCE_ENERGY) > 5000;
-                    }
-                });
-            }
-            if(!target && creep.getActiveBodyparts(WORK)) {
-                var target = creep.pos.findClosestByPath(FIND_SOURCES, {
-                    filter: (source) => source.energy >= (creep.memory.rerun? 0:1)
-                });
-            }
-            if(!target && !creep.getActiveBodyparts(WORK)) {
-                target = creep.pos.findClosestByPath(FIND_MY_CREEPS, {
-                    filter: (creep2) => {
-                        return creep2.store.getUsedCapacity(RESOURCE_ENERGY) > 0 &&
-                            creep2.store.getFreeCapacity(RESOURCE_ENERGY) == 0 &&
-                            creep2.memory.weight > creep2.memory.weight;
-                    }
-                });
-            }
-            if(!target && !creep.getActiveBodyparts(WORK) && creep.memory.rerun) {
-                target = creep.pos.findClosestByPath(FIND_MY_CREEPS, {
-                    filter: (creep2) => {
-                        return creep2.store.getUsedCapacity(RESOURCE_ENERGY) > 0 &&
-                            creep2.memory.weight > creep.memory.weight;
-                    }
-                });
-            }
-            if(!target && !creep.getActiveBodyparts(WORK) && creep.memory.rerun) {
-                target = creep.pos.findClosestByPath(FIND_MY_CREEPS, {
-                    filter: (creep2) => {
-                        return creep2.memory.weight > creep2.memory.weight;
-                    }
-                });
-            }
-            if(target) {
-                var err = target.name? // a creep
-                    ERR_NOT_IN_RANGE:
-                target.structureType?
-                    creep.withdraw(target, RESOURCE_ENERGY): // a structure
-                creep.harvest(target); // a source
+var role = {
+	name: 'energy_harvesting',
 
-                if(err == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
-                    creep.say('🔜⚡');
-                    if(Game.flags['LEH'] || Game.flags['LE'] || Game.flags['L']) {
-                        console.log( '🔜⚡', Math.trunc(Game.time/10000), Game.time%10000
-                                    , creep.name
-                                    , 'moving for harvesting energy from:'
-                                    , target.name?target.name:target.structureType?target.structureType:target.id);
-                    }
-                }
-                else if(!err) {
-                    creep.say('⚡');
-                    if(Game.flags['LEH'] || Game.flags['LE'] || Game.flags['L']) {
-                        console.log( '⚡', Math.trunc(Game.time/10000), Game.time%10000
-                                    , creep.name
-                                    , 'harvesting energy from:'
-                                    , target.name?target.name:target.structureType?target.structureType:target.id);
-                    }
-                }
-                else {
-                    creep.memory.harvesting = false;
-                    if(Game.flags['LEH'] || Game.flags['LE'] || Game.flags['L']) {
-                        console.log( '⚡⚠️', Math.trunc(Game.time/10000), Game.time%10000
-                                    , creep.name
-                                    , 'harvesting energy from:'
-                                    , target.name?target.name:target.structureType?target.structureType:target.id
-                                    , 'with err:'
-                                    , err);
-                    }
-                }
-            }
-            else {
-                    creep.memory.harvesting = false;
-            }
-        }
+	logFlags: ['LEH','LE ','L'], 
+	
+	log: function(sign, creep, ...args) {
+			if(log.canLog(role.logFlags)) {
+				console.log( sign, Math.trunc(Game.time/10000), Game.time%10000
+										, creep.name
+										, role.name
+										, JSON.stringify(creep.memory[role.name])
+									  , args);
+			}
+	},
 
-        if(!creep.memory.harvesting) {
-            if(creep.memory.rerun) {
-                if(creep.pos.x == 26 && creep.pos.y == 34) {
-                    creep.move(BOTTOM_RIGHT);
-                    creep.move(Game.time%8+1); // TOP:1 ,..., TOP_LEFT:8
-                }
-            }
-            else {
-                creep.say('🔃');
-                creep.memory.rerun = 1;
-                //             roleRerun.run(creep);
-                require('role.claimer').run(creep);
-            }
-        }
-    }
+	intConfig: function(creep) {
+		if(!creep.memory[role.name].room) {
+			creep.memory[role.name].room = 'W26S33';
+		}
+		return true;
+	},
+
+	checkFreeSlot: function(creep) {
+		if(!creep.memory[role.name].room) {
+			creep.memory[role.name].room = 'W26S33';
+		}
+		return true;
+	},
+
+	reserveFreeSlot: function(creep) {
+		return true;
+	},
+
+	getTarget: function(creep) {
+		var target;
+		if(!target && creep.room.energyAvailable != creep.room.energyCapacityAvailable) {
+			var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+				filter: (structure) => {
+					return (structure.structureType == STRUCTURE_CONTAINER) &&
+						structure.store.getUsedCapacity(RESOURCE_ENERGY) > 0;
+				}
+			});
+		}
+		if(!target && creep.room.energyAvailable != creep.room.energyCapacityAvailable) {
+			var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+				filter: (structure) => {
+					return (structure.structureType == STRUCTURE_STORAGE) &&
+						structure.store.getUsedCapacity(RESOURCE_ENERGY) > 5000;
+				}
+			});
+		}
+		if(!target && creep.getActiveBodyparts(WORK)) {
+			var target = creep.pos.findClosestByPath(FIND_SOURCES, {
+				filter: (source) => source.energy >= (creep.memory.rerun? 0:1)
+			});
+		}
+		if(!target && !creep.getActiveBodyparts(WORK)) {
+			target = creep.pos.findClosestByPath(FIND_MY_CREEPS, {
+				filter: (creep2) => {
+					return creep2.store.getUsedCapacity(RESOURCE_ENERGY) > 0 &&
+						creep2.store.getFreeCapacity(RESOURCE_ENERGY) == 0 &&
+						creep2.memory.weight > creep2.memory.weight;
+				}
+			});
+		}
+		if(!target && !creep.getActiveBodyparts(WORK) && creep.memory.rerun) {
+			target = creep.pos.findClosestByPath(FIND_MY_CREEPS, {
+				filter: (creep2) => {
+					return creep2.store.getUsedCapacity(RESOURCE_ENERGY) > 0 &&
+						creep2.memory.weight > creep.memory.weight;
+				}
+			});
+		}
+		if(!target && !creep.getActiveBodyparts(WORK) && creep.memory.rerun) {
+			target = creep.pos.findClosestByPath(FIND_MY_CREEPS, {
+				filter: (creep2) => {
+					return creep2.memory.weight > creep2.memory.weight;
+				}
+			});
+		}
+		return target;
+	},
+	
+	init: function(creep) {
+		if(creep.memory[role.name] === undefined ||
+			 creep.memory[role.name].on === undefined) {
+			creep.memory[role.name] = { on: false
+																, room: ''
+																};
+		}
+	},
+
+	checkOff: function(creep) {
+		if(creep.memory[role.name].on &&
+			creep.store.getFreeCapacity() == 0) {
+			creep.memory[role.name].on = false;
+		}
+	},
+
+	checkOn: function(creep) {
+		if(!creep.memory[role.name].on &&
+			 role.checkFreeSlot(creep) &&
+			 ((creep.store.getUsedCapacity(RESOURCE_ENERGY) == 0 &&
+				 creep.store.getFreeCapacity(RESOURCE_ENERGY) > 0) ||
+				(creep.memory.rerun &&
+				 creep.store.getUsedCapacity(RESOURCE_ENERGY) > 0 &&
+				 creep.store.getFreeCapacity(RESOURCE_ENERGY) > 0))) {
+			creep.memory[role.name].on = true;
+// 			role.log(creep,"On");
+		}
+	},
+	
+	run: function(creep) {
+		role.init(creep);
+		role.checkOff(creep);
+		role.checkOn(creep);
+		
+		if(creep.memory[role.name].on) {
+			var target = role.getTarget(creep);
+			if(target) {
+				var err = target.name? // a creep
+						ERR_NOT_IN_RANGE:
+				target.structureType?
+						creep.withdraw(target, RESOURCE_ENERGY): // a structure
+				creep.harvest(target); // a source
+				
+				if(err == ERR_NOT_IN_RANGE) {
+					creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+					creep.say('🔜⚡');
+					role.log('🔜⚡', creep, 'moving from', JSON.stringify(creep.pos), 'to', JSON.stringify(target));
+				}
+				else if(!err) {
+					creep.say('⚡');
+					role.log('⚡', creep, 'harvest', JSON.stringify(target));
+				}
+				else {
+					role.log( '⚡⚠️', creep, 'err:', err, JSON.stringify(creep.harvest));
+					creep.memory[role.name].on = false;
+				}
+			}
+			else {
+				creep.memory[role.name].on = false;
+			}
+			
+			if(!creep.memory[role.name].on) {
+			roleNext.run(creep);
+		}
+	
+		if(!creep.memory[role.name].on) {
+			if(creep.memory.rerun) {
+				creep.say('🔃'); 
+			}
+			else {
+				creep.memory.rerun = 1;
+				require('role.claimer').run(creep);
+			}
+		}
+	}
 };
 
-module.exports = roleEnergyHarvester;
+module.exports = role;
+
+
+// var roleEnergyHarvester = {
+    
+    
+//     /** @param {Creep} creep **/
+//     run: function(creep) {
+//         var commit = 4;
+//         if(!Memory.commits.EnergyHarvester ||
+//            Memory.commits.EnergyHarvester != commit) {
+//             Memory.commits.EnergyHarvester = commit;
+//             console.log( '✒️', Math.trunc(Game.time/10000), Game.time%10000
+//                         , 'Commit EnergyHarvester'
+//                         , Memory.commits.EnergyHarvester);
+//         }         
+
+//         if(creep.memory.harvesting &&
+//            creep.store.getFreeCapacity() == 0) {
+//             creep.memory.harvesting = false;
+//         }
+
+//         if(!creep.memory.harvesting &&
+//            ((creep.store.getUsedCapacity(RESOURCE_ENERGY) == 0 && creep.store.getFreeCapacity(RESOURCE_ENERGY) > 0) ||
+//             (creep.memory.rerun && creep.store.getUsedCapacity(RESOURCE_ENERGY) > 0 && creep.store.getFreeCapacity(RESOURCE_ENERGY) > 0))) {
+//             creep.memory.harvesting = true;
+//         }
+        
+//         if(creep.memory.harvesting) {
+//         if(!creep.memory.harvesting) {
+//             if(creep.memory.rerun) {
+//                 if(creep.pos.x == 26 && creep.pos.y == 34) {
+//                     creep.move(BOTTOM_RIGHT);
+//                     creep.move(Game.time%8+1); // TOP:1 ,..., TOP_LEFT:8
+//                 }
+//             }
+//             else {
+//                 creep.say('🔃');
+//                 creep.memory.rerun = 1;
+//                 //             roleRerun.run(creep);
+//                 require('role.claimer').run(creep);
+//             }
+//         }
+//     }
+// };
+
+// module.exports = roleEnergyHarvester;
 
 //                                 if(target) {
 //                             var err = creep.withdraw(target, RESOURCE_ENERGY);
