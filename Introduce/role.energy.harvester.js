2518aa3f963ab2b6e27fbdbe43b16bff2080c8cb
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-22 18:52:57 +0300

    Update role.energy.harvester.js

--- Introduce/role.energy.harvester.js
+++ Introduce/role.energy.harvester.js
@@ -29,7 +29,7 @@ var role = {
 			creep.memory[role.name] = { on: false
 																, room: ''
 																};
-			if(!role.W25S33.workers.filter(name => name === '1') &&
+			if(!role.W25S33.workers.filter(name => name === creep.name) &&
 				 !role.W26S33.workers.filter(name => name === '1'))
 // 			if(false && !role.W25S33.workers.find(name => name === creep.name) &&
 // 				 !role.W26S33.workers.find(name => name === creep.name))
