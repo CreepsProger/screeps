bc8caf562f000565d3b17e03f40948f64ea59782
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-22 05:58:42 +0300

    Update role.claimer.js

--- Introduce/role.claimer.js
+++ Introduce/role.claimer.js
@@ -8,7 +8,7 @@ var roleClaimer = {
 	
 	/** @param {Creep} creep **/
 	run: function(creep) {
-		if(true || !creep.memory.claiming.on)
+		if(!creep.memory.claiming.on)
 		{
 			creep.memory.claiming = {on: false, room: ''};
 		}
