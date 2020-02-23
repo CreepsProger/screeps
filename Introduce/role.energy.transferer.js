dbc863ee98b5662a8b4a07fe8e3fe89d803494d1
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-23 19:23:31 +0300

    Update role.energy.transferer.js

--- Introduce/role.energy.transferer.js
+++ Introduce/role.energy.transferer.js
@@ -32,109 +32,129 @@ var roleEnergyTransferer = {
 			}
 			
 			if(!target && creep.memory.rerun) {
-						if(creep.room != 'W25S33' /*creep.memory[role.name].room*/) {
-							const exitDir = Game.map.findExit(creep.room, 'W25S33' /*creep.memory[role.name].room*/);
-							target = creep.pos.findClosestByRange(exitDir);
-						}
+				if(creep.room != 'W25S33' /*creep.memory[role.name].room*/) {
+					const exitDir = Game.map.findExit(creep.room, 'W25S33' /*creep.memory[role.name].room*/);
+					target = creep.pos.findClosestByRange(exitDir);
+				}
+			}
+			
+			if(!target) {
+				var closests = creep.pos.findInRange(FIND_MY_CREEPS, 1, {
+					filter: (creep2) => {
+						return creep2.store.getFreeCapacity(RESOURCE_ENERGY) > 0 &&
+							creep2.memory.weight < creep.memory.weight;
 					}
-            if(!target) {
-                var closests = creep.pos.findInRange(FIND_MY_CREEPS, 1, {
-                    filter: (creep2) => {
-                        return creep2.store.getFreeCapacity(RESOURCE_ENERGY) > 0 &&
-                            creep2.memory.weight < creep.memory.weight;
-                    }
-                });
-                if(closests.length > 0) {
-                    target = closests[0];
-                }
-            }            
-            if(!target) {
-                target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION) &&
-                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-                    }
-                });
-            }
-            if(!target) {
-                target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_SPAWN) &&
-                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 200;
-                    }
-                });
-            }
-            if(!target) {
-                target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_TOWER) &&
-                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 300;
-                    }
-                });
-            }
-            if(!target &&
-							 creep.room.storage !== undefined &&
-							 creep.room.energyAvailable == creep.room.energyCapacityAvailable &&
-               creep.room.storage.store.getUsedCapacity(RESOURCE_ENERGY) < 25000 &&
-               creep.room.storage.store.getFreeCapacity(RESOURCE_ENERGY) > 25000) {
-                target = creep.room.storage;
-            }
-            if(!target) {
-                var closests = creep.pos.findInRange(FIND_MY_STRUCTURES, 1, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_SPAWN) &&
-                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-                    }
-                });
-                if(closests.length > 0) {
-                    target = closests[0];
-                }
-            }            
-            if(target) {
-							var err = ERR_NOT_IN_RANGE;
-							  if(target.id) {
-	                err = creep.transfer(target, RESOURCE_ENERGY);
-								}
-                if(err == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
-                    creep.say('🔜💡');
-                    if(!!Game.flags['LET'] || !!Game.flags['LE'] || !!Game.flags['L']) {
-                        console.log( '🔜💡', Math.trunc(Game.time/10000), Game.time%10000
-                                    , creep.name
-                                    , 'moving for transfering energy to:'
-                                    , target.name?target.name:target.structureType);
-                    }
-                }
-                else if(!err) {
-                    creep.say('💡');
-                    if(!!Game.flags['LET'] || !!Game.flags['LE'] || !!Game.flags['L']) {
-                        console.log( '💡', Math.trunc(Game.time/10000), Game.time%10000
-                                    , creep.name
-                                    , 'transfering energy to:'
-                                    , target.name?target.name:target.structureType);
-                    }
-                }
-                else {
-                    creep.memory.transfering = false;
-                    if(!!Game.flags['LET'] || !!Game.flags['LE'] || !!Game.flags['L']) {
-                        console.log( '💡⚠️', Math.trunc(Game.time/10000), Game.time%10000
-                                    , creep.name
-                                    , 'transfering energy to:'
-                                    , target.name?target.name:target.structureType
-                                    , 'with err:'
-                                    , err);
-                    }
-                }
-            }
-            else {
-                    creep.memory.transfering = false;
-            }
-        }
+				});
+				if(closests.length > 0) {
+					target = closests[0];
+				}
+			}            
+			
+			if(!target) {
+				target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+					filter: (structure) => {
+						return (structure.structureType == STRUCTURE_EXTENSION) &&
+							structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+					}
+				});
+			}
+			
+			if(!target && creep.ticksToLive < constants.TICKS_TO_LIVE_TO_RENEW) {
+				target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+					filter: (structure) => {
+						return (structure.structureType == STRUCTURE_SPAWN) &&
+							structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+					}
+				});
+			}
+			
+			if(!target) {
+				target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+					filter: (structure) => {
+						return (structure.structureType == STRUCTURE_SPAWN) &&
+							structure.store.getUsedCapacity(RESOURCE_ENERGY) == 0;
+					}
+				});
+			}
 
-        if(!creep.memory.transfering) {
-            roleNext.run(creep);
-        }
-    }
+			if(!target) {
+				target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+					filter: (structure) => {
+						return (structure.structureType == STRUCTURE_TOWER) &&
+							structure.store.getFreeCapacity(RESOURCE_ENERGY) > 300;
+					}
+				});
+			}
+			
+			if(!target) {
+				var closests = creep.pos.findInRange(FIND_MY_STRUCTURES, 1, {
+					filter: (structure) => {
+						return (structure.structureType == STRUCTURE_SPAWN) &&
+							structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+					}
+				});
+				
+				if(closests.length > 0) {
+					target = closests[0];
+				}
+			}            
+			
+			if(!target &&
+				 creep.room.storage !== undefined &&
+				 creep.room.energyAvailable == creep.room.energyCapacityAvailable &&
+				 creep.room.storage.store.getUsedCapacity(RESOURCE_ENERGY) < 25000 &&
+				 creep.room.storage.store.getFreeCapacity(RESOURCE_ENERGY) > 25000) {
+				target = creep.room.storage;
+			}
+			
+			if(target) {
+				
+				var err = ERR_NOT_IN_RANGE;
+				
+				if(target.id) {
+					err = creep.transfer(target, RESOURCE_ENERGY);
+				}
+				
+				if(err == ERR_NOT_IN_RANGE) {
+					creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+					creep.say('🔜💡');
+					if(!!Game.flags['LET'] || !!Game.flags['LE'] || !!Game.flags['L']) {
+						console.log( '🔜💡', Math.trunc(Game.time/10000), Game.time%10000
+												, creep.name
+												, 'moving for transfering energy to:'
+												, target.name?target.name:target.structureType);
+					}
+				}
+				else if(!err) {
+					creep.say('💡');
+					if(!!Game.flags['LET'] || !!Game.flags['LE'] || !!Game.flags['L']) {
+						console.log( '💡', Math.trunc(Game.time/10000), Game.time%10000
+												, creep.name
+												, 'transfering energy to:'
+												, target.name?target.name:target.structureType);
+					}
+				}
+				else {
+					creep.memory.transfering = false;
+					if(!!Game.flags['LET'] || !!Game.flags['LE'] || !!Game.flags['L']) {
+						console.log( '💡⚠️', Math.trunc(Game.time/10000), Game.time%10000
+												, creep.name
+												, 'transfering energy to:'
+												, target.name?target.name:target.structureType
+												, 'with err:'
+												, err);
+					}
+				}
+			}
+			else {
+				creep.memory.transfering = false;
+			}
+		}
+
+		if(!creep.memory.transfering) {
+			roleNext.run(creep);
+		}
+	}
 };
 
 module.exports = roleEnergyTransferer;
