bc047542c009f7f22ace6b3bf46f9dbe3028b9f7
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-24 23:34:23 +0300

    Update role.energy.harvester.js

--- Introduce/role.energy.harvester.js
+++ Introduce/role.energy.harvester.js
@@ -36,8 +36,7 @@ var role = {
 																														 , {name: '2', time: 0, needs_weight: 50}
 																														 , {name: '3', time: 0, needs_weight: 50}
 																														 , {name: '4', time: 0, needs_weight: 40}
-																														 , {name: '5', time: 0, needs_weight: 40}
-																														 , {name: '6', time: 0, needs_weight: 40}]
+																														 , {name: '5', time: 0, needs_weight: 40}]
 																							 },
 																			 W27S33: { containers: {weight: 45}
 																							 ,    workers: [ {name: '1', time: 0, needs_weight: 50}
