c08598431cb7ba625bb38cfc29e4156b862b9323
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-26 07:46:27 +0300

    Update main.js

--- Introduce/main.js
+++ Introduce/main.js
@@ -6,7 +6,7 @@ var roleCreep = require('role.claimer');
 
 var commit = 31;
 var ticksToCheckCreepsNumber = 10;
-var maxCreepsNumber = 17;
+var maxCreepsNumber = 15;
 //var mainSettings = require('main.settings');
 
 function updateMovingAverage(x) { 
@@ -195,7 +195,7 @@ module.exports.loop = function () {
 
          if(CL >= 5) mainFlags.tryCreateCreep(5000200000007,100, N<(maxCreepsNumber+1)? 2:0); // V 1-1 E   650 Attacker
          if(CL >= 5) mainFlags.tryCreateCreep(        90909, 50, N<(maxCreepsNumber+1)? 7:0); // V 1-1 E  1800   Worker
-         if(CL >= 5) mainFlags.tryCreateCreep(        90909, 40, N<(maxCreepsNumber+1)? 7:0); // V 1-1 E  1800   Worker
+         if(CL >= 5) mainFlags.tryCreateCreep(        51711, 40, N<(maxCreepsNumber+1)? 5:0); // V 1-1 E  1800   Worker
          if(CL >= 5) mainFlags.tryCreateCreep(  20000000002,100, N<(maxCreepsNumber+1)? 1:0); // V 1-1 E  1300  Claimer
 
          if(CL >= 4) mainFlags.tryCreateCreep(        80808, 30, Memory.totals.WORK< 8? 1:0); // E 1600 Worker
