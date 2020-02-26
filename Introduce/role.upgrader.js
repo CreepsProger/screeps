0288fc9185c3ed4dc591917921c109369cb5e30c
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-26 16:05:06 +0300

    Update role.upgrader.js

--- Introduce/role.upgrader.js
+++ Introduce/role.upgrader.js
@@ -57,7 +57,7 @@ var roleUpgrader = {
 
 			if(creep.memory.upgrading) {
 				var target;
-				if(!target && creep.room.controller.my) {
+				if(!target && creep.room.controller.my !== undefined) {
 					target = creep.room.controller;
 				}
 				if(target) {
