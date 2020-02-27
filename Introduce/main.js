7a26d09e6cd0a6f74f7cbc35ee5fa524738975a7
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-27 20:17:33 +0300

    Update main.js

--- Introduce/main.js
+++ Introduce/main.js
@@ -193,7 +193,7 @@ module.exports.loop = function () {
          const CL = Controller.level;
          var N = Memory.totals.CreepsNumber;
 
-         if(CL >= 4) mainFlags.tryCreateCreep(        60606, 50, N<(maxCreepsNumber+1)?10:0); // V 1-1 E  1200   Worker
+         if(CL >= 4) mainFlags.tryCreateCreep(        60606, 50, N<(maxCreepsNumber+1)?13:0); // V 1-1 E  1200   Worker
          if(CL >= 5) mainFlags.tryCreateCreep(        90909, 50, N<(maxCreepsNumber+1)?10:0); // V 1-1 E  1800   Worker
          if(CL >= 5) mainFlags.tryCreateCreep(         2412, 36, N<(maxCreepsNumber+1)? 3:0); // V 1-1 E  1800   Worker
          if(CL >= 5) mainFlags.tryCreateCreep(  20000000002,100, N<(maxCreepsNumber+1)? 1:0); // V 1-1 E  1300  Claimer
