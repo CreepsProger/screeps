29033140fb5d542a3c8b0a99aa1c0f475587aa4f
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-28 14:10:53 +0300

    Update main.spawns.js

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -1,4 +1,5 @@
 const constants = require('main.constants');
+const flags = require('main.flags');
 
 var last_game_time_created_creep = 0;
 
@@ -107,21 +108,21 @@ var spawns = {
 				const CL = Controller.level;
 				var N = Memory.totals.CreepsNumber;
 				
-				if(CL >= 4) mainFlags.tryCreateCreep(        60606, 50, N<(maxCreepsNumber+1)?13:0); // V 1-1 E  1200   Worker
-				if(CL >= 5) mainFlags.tryCreateCreep(        90909, 50, N<(maxCreepsNumber+1)?10:0); // V 1-1 E  1800   Worker
-				if(CL >= 5) mainFlags.tryCreateCreep(         2412, 36, N<(maxCreepsNumber+1)? 3:0); // V 1-1 E  1800   Worker
-				if(CL >= 4) mainFlags.tryCreateCreep(  20000000002,100, N<(maxCreepsNumber+1)? 1:0); // V 1-1 E  1300  Claimer
-				if(CL >= 4) mainFlags.tryCreateCreep(5000200000007,100, N<(maxCreepsNumber+1)? 2:0); // V 1-1 E   650 Attacker
+				if(CL >= 4) spawns.tryCreateCreep(        60606, 50, N<(maxCreepsNumber+1)?13:0); // V 1-1 E  1200   Worker
+				if(CL >= 5) spawns.tryCreateCreep(        90909, 50, N<(maxCreepsNumber+1)?10:0); // V 1-1 E  1800   Worker
+				if(CL >= 5) spawns.tryCreateCreep(         2412, 36, N<(maxCreepsNumber+1)? 3:0); // V 1-1 E  1800   Worker
+				if(CL >= 4) spawns.tryCreateCreep(  20000000002,100, N<(maxCreepsNumber+1)? 1:0); // V 1-1 E  1300  Claimer
+				if(CL >= 4) spawns.tryCreateCreep(5000200000007,100, N<(maxCreepsNumber+1)? 2:0); // V 1-1 E   650 Attacker
 				
-				if(CL >= 4) mainFlags.tryCreateCreep(        80808, 30, Memory.totals.WORK< 8? 1:0); // E 1600 Worker
-				if(CL >= 4) mainFlags.tryCreateCreep(        70707, 30, Memory.totals.WORK< 7? 1:0); // E 1400 Worker
-				if(CL >= 3) mainFlags.tryCreateCreep(        60606, 30, Memory.totals.WORK< 6? 1:0); // E 1200 Worker
-				if(CL >= 3) mainFlags.tryCreateCreep(        50505, 30, Memory.totals.WORK< 5? 1:0); // E 1000 Worker
-				if(CL >= 3) mainFlags.tryCreateCreep(        40404, 30, Memory.totals.WORK< 4? 1:0); // E  800 Worker
+				if(CL >= 4) spawns.tryCreateCreep(        80808, 30, Memory.totals.WORK< 8? 1:0); // E 1600 Worker
+				if(CL >= 4) spawns.tryCreateCreep(        70707, 30, Memory.totals.WORK< 7? 1:0); // E 1400 Worker
+				if(CL >= 3) spawns.tryCreateCreep(        60606, 30, Memory.totals.WORK< 6? 1:0); // E 1200 Worker
+				if(CL >= 3) spawns.tryCreateCreep(        50505, 30, Memory.totals.WORK< 5? 1:0); // E 1000 Worker
+				if(CL >= 3) spawns.tryCreateCreep(        40404, 30, Memory.totals.WORK< 4? 1:0); // E  800 Worker
 				
-				if(CL >= 2) mainFlags.tryCreateCreep(          506, 45, Memory.totals.CARRY< 6? 1:0); // E 550 Carier
+				if(CL >= 2) spawns.tryCreateCreep(          506, 45, Memory.totals.CARRY< 6? 1:0); // E 550 Carier
 				
-				if(CL >= 1) mainFlags.tryCreateCreep(          303, 45, Memory.totals.CARRY< 3? 1:0); // E 300 Carier
+				if(CL >= 1) spawns.tryCreateCreep(          303, 45, Memory.totals.CARRY< 3? 1:0); // E 300 Carier
 			}
 			
 			if(!!Spawn && Spawn.spawning) {
