e7f1976b1277a8e873262b9f61287e4b0b9f573b
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-02-29 12:24:50 +0300

    Update main.spawns.js

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -118,8 +118,8 @@ var spawns = {
 				const CL = controller.level;
 				var N = Memory.totals.CreepsNumber;
 				
-				if(CL >= 5) spawns.tryCreateCreep(spawn,        90909, 39, N<(config.maxCreepsNumber+1)? 2:0); // V 1-1 E  1800   Worker
-				if(CL >= 5) spawns.tryCreateCreep(spawn,        90909, 49, N<(config.maxCreepsNumber+1)? 5:0); // V 1-1 E  1800   Worker
+				if(CL >= 5) spawns.tryCreateCreep(spawn,        90909, 39, N<(config.maxCreepsNumber+1)? 3:0); // V 1-1 E  1800   Worker
+				if(CL >= 5) spawns.tryCreateCreep(spawn,        90909, 49, N<(config.maxCreepsNumber+1)? 6:0); // V 1-1 E  1800   Worker
 				if(CL >= 5) spawns.tryCreateCreep(spawn,        90909, 59, N<(config.maxCreepsNumber+1)? 3:0); // V 1-1 E  1800   Worker
 				if(CL >= 4) spawns.tryCreateCreep(spawn,         1709, 30, N<(config.maxCreepsNumber+1)? 1:0); // V 1-1 E  1300   Carier
 				if(CL >= 4) spawns.tryCreateCreep(spawn,         1709, 40, N<(config.maxCreepsNumber+1)? 2:0); // V 1-1 E  1300   Carier
