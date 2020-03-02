386a8960f7189c70b825a82f5dd23a3e9ed90f8f
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-03-02 14:15:52 +0300

    Update main.config.js

--- Introduce/main.config.js
+++ Introduce/main.config.js
@@ -29,7 +29,8 @@ var config = {
 																							 ,      links: [ {from: '1', to: '0'}
 																														 , {from: '1', to: '0'}
 																														 ]
-																							 ,    workers: [ {name: '1', time: 0, min_weight: 30, max_weight: 39}
+																							 ,    energy_harvesting:
+																								             [ {name: '1', time: 0, min_weight: 30, max_weight: 39}
 																														 , {name: '2', time: 0, min_weight: 30, max_weight: 39}
 																														 , {name: '3', time: 0, min_weight: 30, max_weight: 39}
 																														 , {name: '4', time: 0, min_weight: 30, max_weight: 39}
@@ -37,7 +38,8 @@ var config = {
 																														 ]
 																							 },
 																			 W26S33: { containers: {weight: 45}
-																							 ,    workers: [ {name: '1', time: 0, min_weight: 40, max_weight: 49}
+																							 ,    energy_harvesting:
+																								             [ {name: '1', time: 0, min_weight: 40, max_weight: 49}
 																														 , {name: '2', time: 0, min_weight: 40, max_weight: 49}
 																														 , {name: '3', time: 0, min_weight: 40, max_weight: 49}
 																														 , {name: '4', time: 0, min_weight: 40, max_weight: 49}
@@ -49,7 +51,8 @@ var config = {
 																														 ]
 																							 },
 																			 W27S33: { containers: {weight: 55}
-																							 ,    workers: [ {name: '1', time: 0, min_weight: 50, max_weight: 59}
+																							 ,    energy_harvesting:
+																								             [ {name: '1', time: 0, min_weight: 50, max_weight: 59}
  																														 , {name: '2', time: 0, min_weight: 50, max_weight: 59}
  																														 , {name: '3', time: 0, min_weight: 50, max_weight: 59}
  																														 , {name: '4', time: 0, min_weight: 50, max_weight: 59}
@@ -57,9 +60,18 @@ var config = {
  																														 , {name: '6', time: 0, min_weight: 50, max_weight: 59}
  																														 , {name: '7', time: 0, min_weight: 50, max_weight: 59}
 																														 ]
+																							 ,    claiming:
+																								             [ {name: '1', time: 0, min_weight: 50, max_weight: 59}
+ 																														 , {name: '2', time: 0, min_weight: 50, max_weight: 59}
+																														 ]
 																							 },
 																			 W28S33: { containers: {weight: 65}
-																							 ,    workers: [ {name: '1', time: 0, min_weight: 60, max_weight: 69}
+																							 ,    energy_harvesting:
+																								             [ {name: '1', time: 0, min_weight: 60, max_weight: 69}
+ 																														 , {name: '2', time: 0, min_weight: 60, max_weight: 69}
+																														 ]
+																							 ,    claiming:
+																								             [ {name: '1', time: 0, min_weight: 60, max_weight: 69}
  																														 , {name: '2', time: 0, min_weight: 60, max_weight: 69}
 																														 ]
 																							 }
@@ -73,32 +85,32 @@ var config = {
 		var already = false;
 		for(var room_name in Memory.config.rooms) {
 			var room_config = Memory.config.rooms[room_name];
-			room_config.workers.forEach(function(w) {
+			room_config[role].forEach(function(slot) {
 				if(already) {
-					if(w.name === creep.name) {
-            var v = w;
-						w.name = '-' + creep.name;
-						w.time = Game.time;
-						config.log('🏢', creep, 'setRoom: slot\'s removed', JSON.stringify(v), JSON.stringify(w));
+					if(slot.name === creep.name) {
+						slot.name = '-' + creep.name;
+						slot.time = Game.time;
+						config.log('🏢', creep, role, 'setRoom: slot\'s removed', JSON.stringify(slot));
 					}
 				}
 				else {
-					if(w.name === creep.name) {
+					if(slot.name === creep.name) {
 						creep.memory[role].room = room_name;
-						w.time = Game.time;
+						slot.time = Game.time;
 						already = true;
-						config.log('🏢', creep, 'setRoom: time\'s  updated', JSON.stringify(w));
+						config.log('🏢', creep, role, 'setRoom: time\'s  updated', JSON.stringify(slot));
 					}
-					else if(w.time < Game.time - 100 &&
+					else if(slot.time < Game.time - 100 &&
 									creep.memory.weight >= w.min_weight &&
 									creep.memory.weight <= w.max_weight) {
-            var v = w;
+            const old_name = slot.name;
+            const old_time = slot.time;
 						var reset = (creep.memory[role].room != room_name);
 						creep.memory[role].room = room_name;
-						w.name = creep.name;
-						w.time = Game.time;
+						slot.name = creep.name;
+						slot.time = Game.time;
 						already = true;
-						config.log('🏢', creep, 'setRoom: reset('+reset+')', JSON.stringify(v), JSON.stringify(w));
+						config.log('🏢', creep, role, 'setRoom: reset('+reset+')', JSON.stringify(slot), 'prev slot:', old_name, old_time);
 					}
 				}
 			});
