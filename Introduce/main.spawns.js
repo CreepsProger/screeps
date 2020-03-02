d453ace3f8f9720d0d8ee7bee0c9000fa5a0dfb2
Creeps Proger <creepsproger@gmail.com>
2020-03-02 19:32:51 +0300

    5

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -119,7 +119,7 @@ var spawns = {
 				var N = Memory.totals.CreepsNumber;
 
 				if(CL >= 4) spawns.tryCreateCreep(spawn,         1709, 30, N<(config.maxCreepsNumber+1)? 1:0); // V 1-1 E  1300   Carier
-				if(CL >= 5) spawns.tryCreateCreep(spawn,        90909, 39, N<(config.maxCreepsNumber+1)? 2:0); // V 1-1 E  1800   Worker
+				if(CL >= 5) spawns.tryCreateCreep(spawn,        90909, 39, N<(config.maxCreepsNumber+1)? 5:0); // V 1-1 E  1800   Worker
 				if(CL >= 4) spawns.tryCreateCreep(spawn,         1709, 40, N<(config.maxCreepsNumber+1)? 1:0); // V 1-1 E  1300   Carier
 				if(CL >= 5) spawns.tryCreateCreep(spawn,        90909, 49, N<(config.maxCreepsNumber+1)? 5:0); // V 1-1 E  1800   Worker
  				if(CL >= 4) spawns.tryCreateCreep(spawn,         1709, 50, N<(config.maxCreepsNumber+1)? 3:0); // V 1-1 E  1300   Carier
@@ -129,7 +129,7 @@ var spawns = {
 				if(CL >= 5) spawns.tryCreateCreep(spawn,        90909, 69, N<(config.maxCreepsNumber+1)? 3:0); // V 1-1 E  1800   Worker
 				if(CL >= 4) spawns.tryCreateCreep(spawn,  20000000002, 61, N<(config.maxCreepsNumber+1)? 1:0); // V 1-1 E  1300  Claimer
 				if(CL >= 4) spawns.tryCreateCreep(spawn,5000200000007,100, N<(config.maxCreepsNumber+1)? 4:0); // V 1-1 E   650 Attacker
-				
+
 				if(CL >= 4) spawns.tryCreateCreep(spawn,        80808, 30, Memory.totals.WORK< 8? 1:0); // E 1600 Worker
 				if(CL >= 4) spawns.tryCreateCreep(spawn,        70707, 30, Memory.totals.WORK< 7? 1:0); // E 1400 Worker
 				if(CL >= 3) spawns.tryCreateCreep(spawn,        60606, 30, Memory.totals.WORK< 6? 1:0); // E 1200 Worker
