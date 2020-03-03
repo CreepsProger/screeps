6abbbeb124f65ca12eaecc8ec68ce178170dd1dd
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-03-03 12:46:07 +0300

    Update role.energy.transferer.js

--- Introduce/role.energy.transferer.js
+++ Introduce/role.energy.transferer.js
@@ -177,9 +177,9 @@ var roleEnergyTransferer = {
 						const pu = Math.floor(p.store.getUsedCapacity(RESOURCE_ENERGY)/30000);
 						const vu = Math.floor(v.store.getUsedCapacity(RESOURCE_ENERGY)/30000);
  						console.log(p.room.name, pu, v.room.name, vu, pu<vu, pu<vu? p.room.name:v.room.name);
- 						console.log(creep, 'target storage room name:', target.room.name);
 						return (pu < vu ? p : v );
 					});
+					console.log(creep, 'target storage room name:', target.room.name);
 				}
 			}
 
