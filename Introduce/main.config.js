ca3f7de93208f7f248503203f5a2ab4802d378f8
Creeps Proger <creepsproger@gmail.com>
2020-03-05 22:01:03 +0300

    W28S33: 'W28S34'

--- Introduce/main.config.js
+++ Introduce/main.config.js
@@ -90,8 +90,8 @@ var config = {
 												 , {name: '3', time: 0, min_weight: 100, max_weight: 100}
 												 , {name: '4', time: 0, min_weight: 100, max_weight: 100}
 												 ]
-									   , heal_room: 'W26S33'
-										 , path_rooms: {W25S33: 'W26S33', W26S33: 'W27S33', W27S33: 'W28S34'}
+									   , heal_room: 'W26S33',
+										 , path_rooms: {W25S33: 'W26S33', W26S33: 'W27S33', W27S33: 'W28S34', W28S33: 'W28S34'}
 										 },
 						 W29S33: { containers: {weight: 75}
 										 , energy_harvesting:
