24a31882e9a2d6279cc1ea125009a9e5950ddbd7
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-03-05 13:57:01 +0300

    Update role.builder.js

--- Introduce/role.builder.js
+++ Introduce/role.builder.js
@@ -38,7 +38,7 @@ var roleBuilder = {
 				var target;
 
 				if(!target) {
-					var targets = creep.pos.findInRange(FIND_STRUCTURES, 5, {
+					var targets = creep.pos.findInRange(FIND_STRUCTURES, 20, {
 						filter: (structure) => { 
 							if(structure.structureType == STRUCTURE_ROAD &&
 								 structure.hitsMax - structure.hits > 1000) {
