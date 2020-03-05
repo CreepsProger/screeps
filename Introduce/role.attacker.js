0d2164858b771ce94daec67d14c985f87364b480
Creeps Proger <creepsproger@gmail.com>
2020-03-05 22:12:30 +0300

    +

--- Introduce/role.attacker.js
+++ Introduce/role.attacker.js
@@ -37,8 +37,8 @@ var role = {
 	  		const this_room_config = Memory.config.rooms[this_room];
 				const my_room = creep.memory[role.name].room;
 	  		const my_room_config = Memory.config.rooms[my_room];
-				const my_heal_room = my_room_config['heal_room'];//'W25S33';
-				console.log(my_room, JSON.stringify(my_room_config));
+				const my_heal_room = my_room_config.heal_room;//'W25S33';
+				console.log(my_room, JSON.stringify(my_room_config.heal_room));
 				const my_path_room = my_room_config.path_rooms[this_room];
 
     		var target;
