e41ff4fdc594b07307b725b090597d93032b2333
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-03-05 16:57:32 +0300

    Update main.metrix.js

--- Introduce/main.metrix.js
+++ Introduce/main.metrix.js
@@ -95,6 +95,12 @@ var metrix = {
 //                   , Memory.harvestersMovements.Avg.movingAverage.delta
                   , JSON.stringify(Memory.CreepsNumberByType));
 		}
+
+		if(Game.time % config.ticksToCheckCreepsNumber % 2 == 0) {
+			console.log( '✒️', Math.trunc(Game.time/10000), Game.time%10000
+									, JSON.stringify(BODYPARTS_ALL));
+		}
+
 	}
 };
 
