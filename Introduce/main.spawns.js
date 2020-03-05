09c80ec5fd67319f9fb29650a0c98dff5d5cfb06
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-03-05 08:36:06 +0300

    Update main.spawns.js

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -118,16 +118,16 @@ var spawns = {
 				const CL = controller.level;
 				var N = Memory.totals.CreepsNumber;
 				// 22*1800+2*1300+4*650 = 44800 -> 30 per game tick
-				spawns.tryCreateCreep(spawn,         2412, 30, 1); // V 1-1 E  1800   Carier
+				spawns.tryCreateCreep(spawn,         1608, 30, 1); // V 1-1 E  1200   Carier
 				spawns.tryCreateCreep(spawn,       120408, 39, 1); // V 1-1 E  1800   Worker
 				spawns.tryCreateCreep(spawn,         2412, 40, 2); // V 1-1 E  1800   Carier
-				spawns.tryCreateCreep(spawn,       100608, 49, 3); // V 1-1 E  1800   Worker
+				spawns.tryCreateCreep(spawn,        90909, 49, 3); // V 1-1 E  1800   Worker
 				spawns.tryCreateCreep(spawn,  20000000002, 50, 1); // V 1-1 E  1300  Claimer
  				spawns.tryCreateCreep(spawn,         1608, 51, 1); // V 1-1 E  1200   Carier
-				spawns.tryCreateCreep(spawn,       100608, 59, 3); // V 1-1 E  1700   Worker
+				spawns.tryCreateCreep(spawn,        90909, 59, 3); // V 1-1 E  1700   Worker
 				spawns.tryCreateCreep(spawn,  20000000002, 60, 1); // V 1-1 E  1300  Claimer
         spawns.tryCreateCreep(spawn,         2412, 61, 1); // V 1-1 E  1800   Carier
-				spawns.tryCreateCreep(spawn,       100608, 69, 3); // V 1-1 E  1700   Worker
+				spawns.tryCreateCreep(spawn,        90909, 69, 3); // V 1-1 E  1700   Worker
 				spawns.tryCreateCreep(spawn,5000200000007,100, 2); // V 1-1 E   650 Attacker
 
 				if(CL >= 4) spawns.tryCreateCreep(spawn,        80808, 20, Memory.totals.WORK< 8? 1:0); // E 1600 Worker
