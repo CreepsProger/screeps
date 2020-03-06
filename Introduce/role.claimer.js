f80648631884573d2dfe4eca5d37c5492036300e
Creeps Proger <creepsproger@gmail.com>
2020-03-06 21:31:09 +0300

    ++

--- Introduce/role.claimer.js
+++ Introduce/role.claimer.js
@@ -23,9 +23,11 @@ var role = {
 
 	init: function(creep) {
 		if(creep.memory[role.name] === undefined ||
-			 creep.memory[role.name].on === undefined) {
-			creep.memory[role.name] = { on: false
-																, room: ''
+			 creep.memory[role.name].v === undefined ||
+			 creep.memory[role.name].v != config.version) {
+			creep.memory[role.name] = { v: config.version
+																, on: false
+																, room: creep.room.name
 																};
 		}
 	},
