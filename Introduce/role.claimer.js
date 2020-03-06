a5f7f5aa94867801fbba73e8dcdfbd48facbdb07
Creeps Proger <creepsproger@gmail.com>
2020-03-06 05:20:28 +0300

    +

--- Introduce/role.claimer.js
+++ Introduce/role.claimer.js
@@ -58,6 +58,8 @@ var role = {
 		role.checkOff(creep);
 		role.checkOn(creep);
 
+		if(creep.memory[role.name].on) {
+
 		const this_room = creep.room.name;
 		const this_room_config = Memory.config.rooms[this_room];
 		const my_room = creep.memory[role.name].room;
@@ -65,7 +67,6 @@ var role = {
 		console.log(creep, my_room, JSON.stringify(my_room_config));
 		const my_path_room = my_room_config.path_rooms[this_room];
 
-		if(creep.memory[role.name].on) {
 			var target;
 
   		if(!target && this_room != my_room) {
