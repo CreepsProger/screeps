b8925cc61b2d961003ff71023d8000da117ced48
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-03-06 14:27:26 +0300

    Update main.config.js

--- Introduce/main.config.js
+++ Introduce/main.config.js
@@ -6,7 +6,7 @@ var git = '$Format:%H$';
 
 var config = {
 
-	version: 90,
+	version: 91,
 	ticksToCheckCreepsNumber: 10,
 	// maxCreepsNumber:40,
 
@@ -199,12 +199,12 @@ var config = {
 												 ]
 										 , attacker:
 												 [ {name: '1', time: 0, min_weight:100, max_weight:119}
-												 , {name: '2', time: 0, min_weight:100, max_weight:119}
-												 , {name: '3', time: 0, min_weight:100, max_weight:119}
-												 , {name: '4', time: 0, min_weight:100, max_weight:119}
-												 , {name: '5', time: 0, min_weight:100, max_weight:119}
-												 , {name: '6', time: 0, min_weight:100, max_weight:119}
-												 , {name: '7', time: 0, min_weight:100, max_weight:119}
+// 												 , {name: '2', time: 0, min_weight:100, max_weight:119}
+// 												 , {name: '3', time: 0, min_weight:100, max_weight:119}
+// 												 , {name: '4', time: 0, min_weight:100, max_weight:119}
+// 												 , {name: '5', time: 0, min_weight:100, max_weight:119}
+// 												 , {name: '6', time: 0, min_weight:100, max_weight:119}
+// 												 , {name: '7', time: 0, min_weight:100, max_weight:119}
 												 ]
 									   , heal_room: 'W26S33'
 										 , path_rooms: { W25S33: 'W26S33', W26S33: 'W27S33', W27S33: 'W28S33', W28S33: 'W29S33', W29S33: 'W30S33'
