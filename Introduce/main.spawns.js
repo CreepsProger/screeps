37451d4b1e9d731f985ee04b139e7454dbdd21d6
Creeps Proger <creepsproger@gmail.com>
2020-03-06 07:48:13 +0300

    5000200000007,100, 6

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -130,8 +130,8 @@ var spawns = {
 				spawns.tryCreateCreep(spawn,         90909, 69, 3); // V 1-1 E  1800   Worker
 				// spawns.tryCreateCreep(spawn,   20000000002, 71, 1); // V 1-1 E  1300  Claimer
 				// spawns.tryCreateCreep(spawn,   20000000002, 81, 1); // V 1-1 E  1300  Claimer
-				spawns.tryCreateCreep(spawn, 5000200000007,100, 4); // V 1-1 E   700 Attacker
-				spawns.tryCreateCreep(spawn,   20000000002,101, 4); // V 1-1 E  2600  Claimer
+				spawns.tryCreateCreep(spawn, 5000200000007,100, 6); // V 1-1 E   700 Attacker
+				spawns.tryCreateCreep(spawn,   20000000002,101, 2); // V 1-1 E  2600  Claimer
 				// spawns.tryCreateCreep(spawn,             1, 92, 4); // V 1-1 E    50 Attacker
 				// spawns.tryCreateCreep(spawn,10000800000018, 93, 4); // V 1-1 E  2200 Attacker
 				// spawns.tryCreateCreep(spawn,25000800000017, 94, 4); // V 1-2 E  2300 Attacker
