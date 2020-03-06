1dca8927338a933912f56a886cbac4a39a479661
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-03-06 10:10:07 +0300

    Update main.config.js

--- Introduce/main.config.js
+++ Introduce/main.config.js
@@ -182,6 +182,26 @@ var config = {
 									                 , W30S33: 'W30S34', W30S34: 'W30S35', W30S35: 'W30S36', W30S36: 'W30S37', W30S37: 'W30S38'
 																	 , W30S38: 'W29S38'}
 										 }
+									 },
+			 	   W28S37: { containers: {weight: 125}
+										 , energy_harvesting:
+												 [ {name: '1', time: 0, min_weight:120, max_weight:129}
+												 , {name: '2', time: 0, min_weight:120, max_weight:129}
+												 ]
+										 , claiming:
+												 [ {name: '1', time: 0, min_weight:120, max_weight:129}
+												 , {name: '2', time: 0, min_weight:120, max_weight:129}
+												 ]
+										 , attacker:
+												 [ {name: '1', time: 0, min_weight:120, max_weight:129}
+												 , {name: '2', time: 0, min_weight:120, max_weight:129}
+												 , {name: '3', time: 0, min_weight:120, max_weight:129}
+												 ]
+									   , heal_room: 'W29S38'
+										 , path_rooms: { W25S33: 'W26S33', W26S33: 'W27S33', W27S33: 'W28S33', W28S33: 'W29S33', W29S33: 'W30S33'
+									                 , W30S33: 'W30S34', W30S34: 'W30S35', W30S35: 'W30S36', W30S36: 'W30S37', W30S37: 'W30S38'
+																	 , W30S38: 'W29S38', W29S38: 'W28S38', W28S38: 'W28S37'}
+										 }
 									 }
 						};
 			config.log('init config', config.inited, 'Memory.config:', JSON.stringify(Memory.config));
