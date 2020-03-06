7ecfcffa17af796c41f7790a812f09937e1f6011
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-03-06 10:01:50 +0300

    Update main.spawns.js

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -128,10 +128,11 @@ var spawns = {
         spawns.tryCreateCreep(spawn,          2412, 60, 2); // V 1-1 E  1800   Carier
 				spawns.tryCreateCreep(spawn,   20000000002, 61, 1); // V 1-1 E  1300  Claimer
 				spawns.tryCreateCreep(spawn,         90909, 69, 3); // V 1-1 E  1800   Worker
-				// spawns.tryCreateCreep(spawn,   20000000002, 71, 1); // V 1-1 E  1300  Claimer
-				// spawns.tryCreateCreep(spawn,   20000000002, 81, 1); // V 1-1 E  1300  Claimer
+		 // spawns.tryCreateCreep(spawn,   20000000002, 71, 1); // V 1-1 E  1300  Claimer
+ 		 // spawns.tryCreateCreep(spawn,   20000000002, 81, 1); // V 1-1 E  1300  Claimer
 				spawns.tryCreateCreep(spawn, 5000200000007,100, 3); // V 1-1 E   700 Attacker
-				spawns.tryCreateCreep(spawn,   20000000002,101, 4); // V 1-1 E  1300  Claimer
+				spawns.tryCreateCreep(spawn,   20000000002,101, 3); // V 1-1 E  1300  Claimer
+				spawns.tryCreateCreep(spawn,   20000000002,110, 1); // V 1-1 E   700 Attacker
 				// spawns.tryCreateCreep(spawn,             1, 92, 4); // V 1-1 E    50 Attacker
 				// spawns.tryCreateCreep(spawn,10000800000018, 93, 4); // V 1-1 E  2200 Attacker
 				// spawns.tryCreateCreep(spawn,25000800000017, 94, 4); // V 1-2 E  2300 Attacker
