7d918b5e0c6eab702f88c54356d7bc71c0dc4f9a
Creeps Proger <creepsproger@gmail.com>
2020-03-06 20:29:23 +0300

    link

--- Introduce/main.links.js
+++ Introduce/main.links.js
@@ -1,7 +1,7 @@
 const constants = require('main.constants');
 
 var links = {
-    
+
    run: function() {
 		 {
 	     const linkFrom = Game.getObjectById('5e56dc7a28e44c6f77878b87');
@@ -9,16 +9,16 @@ var links = {
 	  	 linkFrom.transferEnergy(linkTo);
 		 }
 		 {
-	     const linkFrom = Game.getObjectById('5e5ab771eadd04714b92ed7d');
-		   const linkTo = Game.getObjectById('5e5ab4f1142d6b46f3c86280');
-	  	 linkFrom.transferEnergy(linkTo);
+	     // const linkFrom = Game.getObjectById('5e5ab771eadd04714b92ed7d');
+		   // const linkTo = Game.getObjectById('5e5ab4f1142d6b46f3c86280');
+	  	 // linkFrom.transferEnergy(linkTo);
 		 }
 		 {
 			 const linkFrom = Game.getObjectById('5e5f8ed0124b9b1087db5d47');
 		   const linkTo = Game.getObjectById('5e583a7b7a54e3585a982b96');
 	  	 linkFrom.transferEnergy(linkTo);
 		 }
-	 }	
+	 }
 };
 
 module.exports = links;
