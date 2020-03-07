292b6468a49334d9a5e3222c9d30e3a632d63f0d
Creeps Proger <creepsproger@gmail.com>
2020-03-07 23:50:10 +0300

    version: 123

--- Introduce/main.config.js
+++ Introduce/main.config.js
@@ -6,7 +6,7 @@ var git = '$Format:%H$';
 
 var config = {
 
-	version: 122,
+	version: 123,
 	ticksToCheckCreepsNumber: 10,
 	// maxCreepsNumber:40,
 
@@ -70,8 +70,8 @@ var config = {
 												 , {name: '3', time: 0, min_weight: 90, max_weight:100}
 												 , {name: '4', time: 0, min_weight: 90, max_weight:100}
 												 , {name: '5', time: 0, min_weight: 90, max_weight:100}
-												 // , {name: '6', time: 0, min_weight: 60, max_weight:100}
-												 // , {name: '7', time: 0, min_weight: 60, max_weight:100}
+												 , {name: '6', time: 0, min_weight: 90, max_weight:100}
+												 , {name: '7', time: 0, min_weight: 90, max_weight:100}
 												 ]
 									   , heal_room: 'W26S33'
 										 , path_rooms: {W25S33: 'W26S33', W26S33: 'W26S34', W25S34: 'W26S34', W27S33: 'W26S33'}
