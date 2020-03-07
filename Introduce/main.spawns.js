801ae5aeecc6f663c6bdf667736888e302597239
Creeps Proger <creepsproger@gmail.com>
2020-03-07 11:11:32 +0300

    economy 17 creeps

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -129,14 +129,14 @@ var spawns = {
 					spawns.tryCreateCreep(spawn,          1005, 31, 1); // V 1-1 E    Carier
 					spawns.tryCreateCreep(spawn,        120408, 39, 1); // V 1-1 E    Worker
 					spawns.tryCreateCreep(spawn,          1005, 41, 2); // V 1-1 E    Carier
-					spawns.tryCreateCreep(spawn,        111111, 49, 2); // V 1-1 E    Worker
-					spawns.tryCreateCreep(spawn,   20000000002, 51, 1); // V 1-1 E   Claimer
-	 				spawns.tryCreateCreep(spawn,          3015, 50, 1); // V 1-1 E    Carier
+					spawns.tryCreateCreep(spawn,        111111, 49, 3); // V 1-1 E    Worker
+					spawns.tryCreateCreep(spawn,   20000000002, 50, 1); // V 1-1 E   Claimer
+	 				spawns.tryCreateCreep(spawn,          3015, 51, 2); // V 1-1 E    Carier
 					spawns.tryCreateCreep(spawn,        111111, 59, 2); // V 1-1 E    Worker
-					spawns.tryCreateCreep(spawn,   20000000002, 61, 1); // V 1-1 E   Claimer
-	        spawns.tryCreateCreep(spawn,          3015, 60, 1); // V 1-1 E    Carier
+					spawns.tryCreateCreep(spawn,   20000000002, 60, 1); // V 1-1 E   Claimer
+	        spawns.tryCreateCreep(spawn,          3015, 61, 2); // V 1-1 E    Carier
 					spawns.tryCreateCreep(spawn,        111111, 69, 2); // V 1-1 E    Worker
-          //                                             14
+          //                                             17
           spawns.tryCreateCreep(spawn, 25000800000017,66, 2); // V 1-2 E  Attacker
 		    	// spawns.tryCreateCreep(spawn,25000800000017, 53, 8); // V 1-2 E  2300 Attacker
 				// spawns.tryCreateCreep(spawn, 5000400000014,100, 7); // V 1-1 E   700 Attacker
