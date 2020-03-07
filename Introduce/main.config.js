f66898b32e87c005769d9403117de1a5c04572d6
Creeps Proger <creepsproger@gmail.com>
2020-03-07 15:07:17 +0300

    defence of W28S33

--- Introduce/main.config.js
+++ Introduce/main.config.js
@@ -101,10 +101,10 @@ var config = {
 												 , {name: '2', time: 0, min_weight: 60, max_weight: 69}
 												 ]
 										 , attacker:
-												 [ {name: '1', time: 0, min_weight: 60, max_weight:69}
-												 , {name: '2', time: 0, min_weight: 60, max_weight:69}
-												 // , {name: '3', time: 0, min_weight: 60, max_weight:100}
-												 // , {name: '4', time: 0, min_weight: 60, max_weight:100}
+												 [ {name: '1', time: 0, min_weight: 60, max_weight:100}
+												 , {name: '2', time: 0, min_weight: 60, max_weight:100}
+												 , {name: '3', time: 0, min_weight: 60, max_weight:100}
+												 , {name: '4', time: 0, min_weight: 60, max_weight:100}
 												 // , {name: '5', time: 0, min_weight: 60, max_weight:100}
 												 // , {name: '6', time: 0, min_weight: 60, max_weight:100}
 												 // , {name: '7', time: 0, min_weight: 60, max_weight:100}
