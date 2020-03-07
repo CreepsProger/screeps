8055c30d55d4a4c6f596697d513b25a832fb9a32
Creeps Proger <creepsproger@gmail.com>
2020-03-07 19:53:38 +0300

    e

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -143,7 +143,7 @@ var spawns = {
 					spawns.tryCreateCreep(spawn,             1005, 31, 1); // V 1-1 E    Carier
 					spawns.tryCreateCreep(spawn,           120408, 39, 1); // V 1-1 E    Worker
 					spawns.tryCreateCreep(spawn,             1608, 41, 2); // V 1-1 E    Carier
-					spawns.tryCreateCreep(spawn,           111111, 49, 3); // V 1-1 E    Worker
+					spawns.tryCreateCreep(spawn,           111212, 49, 3); // V 1-1 E    Worker
 					spawns.tryCreateCreep(spawn,    2000000000002, 50, 1); // V 1-1 E   Claimer
 	 				spawns.tryCreateCreep(spawn,             3015, 51, 2); // V 1-1 E    Carier
 					spawns.tryCreateCreep(spawn,           111111, 59, 2); // V 1-1 E    Worker
