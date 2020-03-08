51e28e07387272a8584d83b96dbfb482eb66e180
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-03-08 13:01:40 +0300

    Update main.spawns.js

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -142,8 +142,8 @@ var spawns = {
 				else {
 					spawns.tryCreateCreep(spawn,             2010, 31, 1); // V 1-1 E    Carier
 					spawns.tryCreateCreep(spawn,           100808, 39, 1); // V 1-1 E    Worker
-					spawns.tryCreateCreep(spawn,             3015, 41, 2); // V 1-1 E    Carier
-					spawns.tryCreateCreep(spawn,           111212, 49, 3); // V 1-1 E    Worker
+					spawns.tryCreateCreep(spawn,             3015, 41, 3); // V 1-1 E    Carier
+					spawns.tryCreateCreep(spawn,           111212, 49, 5); // V 1-1 E    Worker
 					spawns.tryCreateCreep(spawn,    2000000000002, 50, 1); // V 1-1 E   Claimer
 	 				spawns.tryCreateCreep(spawn,             3015, 51, 2); // V 1-1 E    Carier
 					spawns.tryCreateCreep(spawn,           111212, 59, 2); // V 1-1 E    Worker
