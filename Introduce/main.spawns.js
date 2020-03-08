db3ca0e24a4f0032f9f6b80d1a66b80245aac6ba
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-03-08 08:14:01 +0300

    Update main.spawns.js

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -158,8 +158,8 @@ var spawns = {
 					spawns.tryCreateCreep(spawn,  500100000000015,100, 2); // V 1-2 E  RAttacker
 					spawns.tryCreateCreep(spawn,  200000008000005,100, 2); // V 1-2 E     Healer
 
-					spawns.tryCreateCreep(spawn,           111212, 79, 2); // V 1-1 E    Worker
-   				// 																						 +2 = 19
+					spawns.tryCreateCreep(spawn,           111212, 79, 1); // V 1-1 E    Worker
+   				// 																						 +1 = 19
 				}
 
 				if(CL >= 4) spawns.tryCreateCreep(spawn,        80808, 20, Memory.totals.WORK< 8? 1:0); // E 1600 Worker
