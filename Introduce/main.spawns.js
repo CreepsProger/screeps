26883c82f6ecd58ea5f703eb229453887e27ae05
Creeps Proger <creepsproger@gmail.com>
2020-03-08 12:39:36 +0300

    spawn,           100608, 39

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -133,15 +133,15 @@ var spawns = {
 				var N = Memory.totals.CreepsNumber;
 				// 22*1800+2*1300+4*650 = 44800 -> 30 per game tick
 				if(false) {
-					spawns.tryCreateCreep(spawn,             1608, 30, 1); // V 1-1 E  1800   Carier
-					spawns.tryCreateCreep(spawn,           120408, 39, 1); // V 1-1 E  1800   Worker
+					spawns.tryCreateCreep(spawn,             1608, 31, 1); // V 1-1 E  1800   Carier
+					spawns.tryCreateCreep(spawn,           100608, 39, 1); // V 1-1 E  1800   Worker
 					spawns.tryCreateCreep(spawn,             3015, 40, 1); // V 1-1 E  1800   Carier
 					spawns.tryCreateCreep(spawn,           111111, 49, 2); // V 1-1 E  1800   Worker
 					spawns.tryCreateCreep(spawn, 2500000800000017,53, 8); // V 1-2 E  2300 Attacker
 				}
 				else {
-					spawns.tryCreateCreep(spawn,             1005, 31, 1); // V 1-1 E    Carier
-					spawns.tryCreateCreep(spawn,           120408, 39, 1); // V 1-1 E    Worker
+					spawns.tryCreateCreep(spawn,             1608, 31, 1); // V 1-1 E    Carier
+					spawns.tryCreateCreep(spawn,           100608, 39, 1); // V 1-1 E    Worker
 					spawns.tryCreateCreep(spawn,             2412, 41, 2); // V 1-1 E    Carier
 					spawns.tryCreateCreep(spawn,           111212, 49, 3); // V 1-1 E    Worker
 					spawns.tryCreateCreep(spawn,    2000000000002, 50, 1); // V 1-1 E   Claimer
