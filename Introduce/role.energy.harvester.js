3404cc05ee1e6ef6f04dd884dfec08f6beb436a7
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-03-08 08:45:54 +0300

    Update role.energy.harvester.js

--- Introduce/role.energy.harvester.js
+++ Introduce/role.energy.harvester.js
@@ -90,8 +90,8 @@ var role = {
 				filter: (source) => {
 					return source.energy == source.energyCapacity &&
 					 source.room.name == this_room &&
-						(!dropped.pos.findInRange(FIND_HOSTILE_STRUCTURES, 5)
-														|| (!!Game.flags['DP2'] && Game.flags['DP2'].pos.findPathTo(dropped).length <= 5)) &&
+						(!source.pos.findInRange(FIND_HOSTILE_STRUCTURES, 5)
+														|| (!!Game.flags['DP2'] && Game.flags['DP2'].pos.findPathTo(source).length <= 5)) &&
 						 tools.checkTarget(executer,source.id);
 				 }
 			 });
@@ -103,8 +103,8 @@ var role = {
 		if(!target && creep.getActiveBodyparts(WORK)) {
 			target = creep.pos.findClosestByPath(FIND_SOURCES, {
 				filter: (source) => source.energy > 0 &&
-						(!dropped.pos.findInRange(FIND_HOSTILE_STRUCTURES, 5)
-														|| (!!Game.flags['DP2'] && Game.flags['DP2'].pos.findPathTo(dropped).length <= 5)) &&
+						(!source.pos.findInRange(FIND_HOSTILE_STRUCTURES, 5)
+														|| (!!Game.flags['DP2'] && Game.flags['DP2'].pos.findPathTo(source).length <= 5)) &&
 				source.room.name == this_room
 			});
 		}
