34b42f00341f77e6226e54598b5a9e8426658719
Creeps Proger <creepsproger@gmail.com>
2020-03-09 17:13:02 +0300

    e 21 d 6

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -143,12 +143,12 @@ var spawns = {
 					spawns.tryCreateCreep(spawn,             3015, 41, 3); // V 1-1 E    Carier
 					spawns.tryCreateCreep(spawn,           130911, 49, 3); // V 1-1 E    Worker
 					spawns.tryCreateCreep(spawn,    2000000000002, 50, 1); // V 1-1 E   Claimer
-	 				spawns.tryCreateCreep(spawn,             3015, 51, 3); // V 1-1 E    Carier
+	 				spawns.tryCreateCreep(spawn,             3015, 51, 2); // V 1-1 E    Carier
 					spawns.tryCreateCreep(spawn,           130911, 59, 2); // V 1-1 E    Worker
 					spawns.tryCreateCreep(spawn,    2000000000002, 60, 1); // V 1-1 E   Claimer
-	        spawns.tryCreateCreep(spawn,             3015, 61, 1); // V 1-1 E    Carier
-					spawns.tryCreateCreep(spawn,           130911, 69, 3); // V 1-1 E    Worker
-          //                                             17+3
+	        spawns.tryCreateCreep(spawn,             3015, 61, 2); // V 1-1 E    Carier
+					spawns.tryCreateCreep(spawn,           130911, 69, 2); // V 1-1 E    Worker
+          //                                                19
 					//                           TTClRrAaHhWwCcMm, 66, 3); // V 1-2 E   Attacker
           spawns.tryCreateCreep(spawn, 1500001000000025, 66, 1); // V 1-2 E   Attacker
           spawns.tryCreateCreep(spawn, 1000000007000009, 67, 1); // V 1-2 E     Healer
@@ -156,9 +156,9 @@ var spawns = {
 					//                           TTClRrAaHhWwCcMm,100, 3); // V 1-2 E   Attacker
 					spawns.tryCreateCreep(spawn,  500100000000015,100, 2); // V 1-2 E  RAttacker
 					spawns.tryCreateCreep(spawn,  200000008000005, 99, 1); // V 1-2 E     Healer
-
+          //                                                 6
 					spawns.tryCreateCreep(spawn,           130911, 79, 2); // V 1-1 E    Worker
-   				//																						 +2 = 19
+   				//																					 +2 = 21
 				}
 
 				if(CL >= 4) spawns.tryCreateCreep(spawn,        80808, 20, Memory.totals.WORK< 8? 1:0); // E 1600 Worker
