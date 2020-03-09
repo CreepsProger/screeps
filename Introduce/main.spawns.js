e61d752b55c4a1dc7426ce35cb91fdb67c90f3a4
Creeps Proger <creepsproger@gmail.com>
2020-03-09 19:49:12 +0300

    ed 30

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -144,11 +144,11 @@ var spawns = {
 					spawns.tryCreateCreep(spawn,           121011, 49, 3); // V 1-1 E    Worker
 					spawns.tryCreateCreep(spawn,    2000000000002, 50, 1); // V 1-1 E   Claimer
 	 				spawns.tryCreateCreep(spawn,             3015, 51, 2); // V 1-1 E    Carier
-					spawns.tryCreateCreep(spawn,           121011, 59, 2); // V 1-1 E    Worker
+					spawns.tryCreateCreep(spawn,           121011, 59, 3); // V 1-1 E    Worker
 					spawns.tryCreateCreep(spawn,    2000000000002, 60, 1); // V 1-1 E   Claimer
 	        spawns.tryCreateCreep(spawn,             3015, 61, 2); // V 1-1 E    Carier
-					spawns.tryCreateCreep(spawn,           121011, 69, 2); // V 1-1 E    Worker
-          //                                                20
+					spawns.tryCreateCreep(spawn,           121011, 69, 3); // V 1-1 E    Worker
+          //                                                22
 					//                           TTClRrAaHhWwCcMm, 66, 3); // V 1-2 E   Attacker
           spawns.tryCreateCreep(spawn, 1500001000000025, 66, 1); // V 1-2 E   Attacker
           spawns.tryCreateCreep(spawn, 1000000007000009, 67, 1); // V 1-2 E     Healer
@@ -158,7 +158,7 @@ var spawns = {
 					spawns.tryCreateCreep(spawn,  200000008000005, 99, 1); // V 1-2 E     Healer
           //                                                 6
 					spawns.tryCreateCreep(spawn,           130911, 79, 2); // V 1-1 E    Worker
-   				//																					 +2 = 22
+   				//																					 +2 = 24
 				}
 
 				if(CL >= 4) spawns.tryCreateCreep(spawn,        80808, 20, Memory.totals.WORK< 8? 1:0); // E 1600 Worker
