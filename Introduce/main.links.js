1d4495c58ee60e66979fa9b6ebefb5ee84ff01b0
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-03-10 15:56:19 +0300

    Update main.links.js

--- Introduce/main.links.js
+++ Introduce/main.links.js
@@ -6,6 +6,7 @@ var links = {
    links: [
 		   { from: '5e62c05bf15a4888dff60e26', to: '5e5ab4f1142d6b46f3c86280'}
 		 , { from: '5e61d337f15a48007bf5b603', to: '5e5ab4f1142d6b46f3c86280'}
+		 , { from: '5e6778dfcfa64f5485786f84', to: '5e5ab4f1142d6b46f3c86280'}
 		 , { from: '5e56dc7a28e44c6f77878b87', to: '5e583a7b7a54e3585a982b96'}
 		 , { from: '5e5f8ed0124b9b1087db5d47', to: '5e583a7b7a54e3585a982b96'}
 	 ],
@@ -17,6 +18,7 @@ var links = {
 				 return (structure.structureType == STRUCTURE_LINK) &&
 					 (structure.id == '5e62c05bf15a4888dff60e26' ||
 	 				  structure.id == '5e61d337f15a48007bf5b603' ||
+						structure.id == '5e6778dfcfa64f5485786f84' ||
 					  structure.id == '5e56dc7a28e44c6f77878b87' ||
 						structure.id == '5e5f8ed0124b9b1087db5d47') &&
 					 structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 &&
