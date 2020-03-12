67e998f51130558c900db5f55ae526092cfab1a8
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-03-12 04:41:45 +0300

    Update main.log.js

--- Introduce/main.log.js
+++ Introduce/main.log.js
@@ -2,13 +2,13 @@
 
 var mainLog = {
 
-	canLog: function(flagNames) {
+	canLog: function(flagNames, creep) {
 // 		console.log(flagNames);
 		for(var i in flagNames) {
 			var name = flagNames[i];
 			var flag = Game.flags[name];
 // 			console.log(i,name,flag);
-			if(!!flag) {
+			if(!!flag && flag.pos.roomName == creep.room.name && (!false || creep.memory.n == 6898)) {
 				return (Game.time % flag.color == 0);
 			}
 		}
