88c286035f7d409ec4e8ba5edb75c1e8dff5f5dc
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-03-13 12:25:44 +0300

    Update role.energy.harvester.js

--- Introduce/role.energy.harvester.js
+++ Introduce/role.energy.harvester.js
@@ -59,9 +59,9 @@ var role = {
 		const this_room_config = Memory.config.rooms[this_room];
 		const my_room = creep.memory[role.name].room;
 		const my_room_config = Memory.config.rooms[my_room];
-		const this_room_sources_is_empty = !creep.pos.findClosestByRange(FIND_SOURCES, {
-				filter: (source) => source.energy > 0 && source.room.name == this_room
-			});
+// 		const this_room_sources_is_empty = !creep.pos.findClosestByRange(FIND_SOURCES, {
+// 				filter: (source) => source.energy > 0 && source.room.name == this_room
+// 			});
 
 		var target;
 
