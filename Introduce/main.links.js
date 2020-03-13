509c19b9c2b7826b79072a2eaec3355555dd0f9e
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-03-13 16:02:25 +0300

    Update main.links.js

--- Introduce/main.links.js
+++ Introduce/main.links.js
@@ -38,11 +38,15 @@ var links = {
 		 var target;
 		 if(true) {
 			 var link = Game.getObjectById('5e583a7b7a54e3585a982b96');
-			 if(creep.room == link.room && link.store.getUsedCapacity(RESOURCE_ENERGY) > 0 && tools.checkTarget(executer,link.id)) {
+			 if(creep.room.name == link.room.name &&
+					link.store.getUsedCapacity(RESOURCE_ENERGY) > 0 &&
+					tools.checkTarget(executer,link.id)) {
 				 target = tools.setTarget(creep,link,link.id,role_run);
 			 }
 			 link = Game.getObjectById('5e5ab4f1142d6b46f3c86280');
-			 if(creep.room == link.room && link.store.getUsedCapacity(RESOURCE_ENERGY) > 0 && tools.checkTarget(executer,link.id)) {
+			 if(creep.room.name == link.room.name &&
+					link.store.getUsedCapacity(RESOURCE_ENERGY) > 0 &&
+					tools.checkTarget(executer,link.id)) {
 				 target = tools.setTarget(creep,link,link.id,role_run);
 			 }
 		 }
