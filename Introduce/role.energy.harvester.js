deb67a5a4a58b5e9e98735f30a73680f24ab5173
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-03-14 07:12:56 +0300

    Update role.energy.harvester.js

--- Introduce/role.energy.harvester.js
+++ Introduce/role.energy.harvester.js
@@ -59,9 +59,7 @@ var role = {
 		const this_room_config = Memory.config.rooms[this_room];
 		const my_room = creep.memory[role.name].room;
 		const my_room_config = Memory.config.rooms[my_room];
-// 		const this_room_sources_is_empty = !creep.pos.findClosestByRange(FIND_SOURCES, {
-// 				filter: (source) => source.energy > 0 && source.room.name == this_room
-// 			});
+ 		const this_room_sources_are_not_empty = !tools.areEmptySources(creep);
 
 		var target;
 
@@ -76,7 +74,9 @@ var role = {
 			target = links.getTargetLinkToHarvest(creep,executer,role.run);
 		}
 
-		if(!target && !creep.getActiveBodyparts(WORK)) {
+		if(!target &&
+			 !creep.getActiveBodyparts(WORK) &&
+			 creep.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
 			var conts = creep.room.find(FIND_STRUCTURES, {
 				filter: (structure) => {
 					return (structure.structureType == STRUCTURE_CONTAINER) &&
@@ -100,7 +100,10 @@ var role = {
 
 		const DP2 = Game.flags['DP2'];
 
-		if(!target && creep.getActiveBodyparts(WORK)) {
+		if(!target &&
+			 this_room_sources_are_not_empty &&
+			 creep.getActiveBodyparts(WORK) &&
+			 creep.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
 			var sources = creep.room.find(FIND_SOURCES, {
 				filter: (source) => {
 					return source.energy == source.energyCapacity &&
@@ -110,11 +113,19 @@ var role = {
 				 }
 			 });
 			 if(sources.length > 0) {
-				 target = tools.setTarget(creep,sources[0],sources[0].id,role.run);
+				 var source = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+					 filter: (structure) => sources.find(s => s.id == structure.id);
+				 });
+				 if(!!source) {
+					 target = tools.setTarget(creep,sources[0],sources[0].id,role.run);
+				 }
 			 }
 		}
 
-		if(!target && creep.getActiveBodyparts(WORK)) {
+		if(!target &&
+			 this_room_sources_are_not_empty &&
+			 creep.getActiveBodyparts(WORK) &&
+			 creep.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
 			var sources = creep.room.find(FIND_SOURCES, {
 				filter: (source) => {
 					return source.energy > 0 &&
@@ -123,7 +134,12 @@ var role = {
 						}
 			});
 			if(sources.length > 0) {
-				 target = sources[0];
+				 var source = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+					 filter: (structure) => sources.find(s => s.id == structure.id);
+				 });
+				 if(!!source) {
+					 target = source;
+				 }
 			 }
 		}
 
