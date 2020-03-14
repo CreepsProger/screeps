d5926c81ca5d0cea8d90caff12ea345e844fced9
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-03-14 08:48:39 +0300

    Update role.energy.transferer.js

--- Introduce/role.energy.transferer.js
+++ Introduce/role.energy.transferer.js
@@ -118,7 +118,7 @@ var roleEnergyTransferer = {
 			}
 
 			//if(!target) {
-			if(!target && (this_room != my_room || !this_room_sources_is_empty)) {
+			if(!target && (this_room != my_room || this_room_sources_are_not_empty)) {
 				var closests = creep.pos.findInRange(FIND_MY_CREEPS, 2, {
 					filter: (creep2) => {
 						return creep2.store.getFreeCapacity(RESOURCE_ENERGY) > 0 &&
