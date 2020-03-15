33bbd439baa5c602695d7ba62049f5abb211176f
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-03-15 20:43:03 +0300

    Update main.js

--- Introduce/main.js
+++ Introduce/main.js
@@ -46,8 +46,8 @@ module.exports.loop = function () {
 			role.run(creep);
 			metrix.cpu.creep_time(creep);
 		}
-		if((Game.time % (constants.TICKS_TO_CHECK_CREEPS_NUMBER/2) == 0 &&
-				Memory.cpu.creep.n == Memory.totals.CreepsNumber) ||
+		if(((Game.time % constants.TICKS_TO_CHECK_CPU == 0) &&
+				Memory.cpu.creep.n >= Memory.totals.CreepsNumber) ||
 			 Memory.cpu.creep.t > 0.9*Game.cpu.tickLimit) {
 			console.log( '⏳', Math.trunc(Game.time/10000), Game.time%10000
 									, Game.cpu.tickLimit
