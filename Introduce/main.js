bb32a01c125180c617c173c545cfd3461ff18081
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-03-16 05:28:55 +0300

    Update main.js

--- Introduce/main.js
+++ Introduce/main.js
@@ -40,20 +40,22 @@ module.exports.loop = function () {
 
 	for(var name in Game.creeps) {
 		var creep = Game.creeps[name];
-// 		console.log( '✒️', Math.trunc(Game.time/10000), Game.time%10000, 'go', creep);
 		if(!creep.spawning) {
 			creep.memory.rerun = 0;
 			role.run(creep);
-
-		metrix.cpu.creep_time(creep);
-		if(((Game.time % constants.TICKS_TO_CHECK_CPU == 0) &&
-				Memory.cpu.creep.n >= Memory.totals.CreepsNumber) ||
-			 Memory.cpu.creep.t > 0.9*Game.cpu.tickLimit) {
+			metrix.cpu.creep_time(creep);
+			if(Memory.cpu.creep.t > 0.9*Game.cpu.tickLimit) {
+				console.log( '⏳', Math.trunc(Game.time/10000), Game.time%10000
+										, Game.cpu.tickLimit
+										, Game.cpu.bucket
+										, JSON.stringify(Memory.cpu));
+			}
+		}
+		if(Game.time % constants.TICKS_TO_CHECK_CPU == 0) {
 			console.log( '⏳', Math.trunc(Game.time/10000), Game.time%10000
 									, Game.cpu.tickLimit
 									, Game.cpu.bucket
 									, JSON.stringify(Memory.cpu));
-		}
 		}
 	}
 // 	console.log('targets:',	JSON.stringify(Memory.targets));
