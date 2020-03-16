c8a59f48813af83dde9aa04852533c20ee821569
Creeps Proger <creepsproger@gmail.com>
2020-03-16 21:47:15 +0300

    +

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -170,7 +170,7 @@ var spawns = {
 				}
 				else {
 					if(spawn.name == 'Spawn1' || !!rerun) spawns.tryCreateCreep(spawn,             1608, 31, 2); // V 1-1 E    Carier
-					if(spawn.name == 'Spawn1' || !!rerun) spawns.tryCreateCreep(spawn,          1604010, 34, 1); // V 1-1 E    Worker
+					if(spawn.name == 'Spawn1' || !!rerun) spawns.tryCreateCreep(spawn,           160410, 34, 1); // V 1-1 E    Worker
 					if(spawn.name == 'Spawn2' || !!rerun) spawns.tryCreateCreep(spawn,             1608, 41, 3); // V 1-1 E    Carier
 					if(spawn.name == 'Spawn2' || !!rerun) spawns.tryCreateCreep(spawn,           130911, 44, 3); // V 1-1 E    Worker
 					if(spawn.name == 'Spawn3' || !!rerun) spawns.tryCreateCreep(spawn,    2000000000002, 50, 1); // V 1-1 E   Claimer
