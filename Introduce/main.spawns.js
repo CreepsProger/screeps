a0fce919c67c035c4e84087470ef51a0daece82f
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-03-16 18:06:43 +0300

    Update main.spawns.js

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -54,7 +54,8 @@ var spawns = {
 			if(!!Memory.CreepsIdleTicksByWeight[weight]) {
 				var creepNs = Object.keys(Memory.CreepsIdleTicksByWeight[weight]);
 				idle = creepNs.reduce((p,c) => p + Memory.CreepsIdleTicksByWeight[weight][c],0);
- 				idle = Math.round(idle/(1+creepNs.length));
+				const bucketWeight = Game.cpu.bucket <= constants.CPU_BUCKET_TO_SPAWN? 1:Game.cpu.bucket;
+ 				idle = Math.round(idle/(creepNs.length + bucketWeight));
 			}
 			if(!!Memory.CreepsMinTicksToLive[weight] && !!Memory.CreepsMinTicksToLive[weight].pos) {
 				range = tools.getRangeTo(spawn.pos,Memory.CreepsMinTicksToLive[weight].pos);
