7cd9fd16e411e3c7210bf0ecf48e3f9055d1086d
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-03-18 13:42:55 +0300

    Update role.attacker.js

--- Introduce/role.attacker.js
+++ Introduce/role.attacker.js
@@ -104,7 +104,7 @@ var role = {
 				}
 
 				if(!target && creep.getActiveBodyparts(HEAL) &&
-					 this_room != my_room &&
+					 this_room != my_room && (creep.pos.x%48 > 1 && creep.pos.y%48 > 1)
 					 !hostile_creeps_near &&
 					 creep.hitsMax - creep.hits > 0 ) {
 					target = creep;
