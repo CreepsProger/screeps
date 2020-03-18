440b5c904660460cc10dceb1290a4d7d83456683
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-03-18 12:40:05 +0300

    Update role.attacker.js

--- Introduce/role.attacker.js
+++ Introduce/role.attacker.js
@@ -98,11 +98,11 @@ var role = {
 					}
 				}
 
-				if(!target && creep.getActiveBodyparts(HEAL) && !canAttack) {
+				if(!target && creep.getActiveBodyparts(HEAL) && !canAttack2) {
 					target = creep;
 				}
 
-				if(!target && this_room != my_heal_room && !canAttack2) {
+				if(!target && this_room != my_heal_room && !canAttack) {
 					const exit = creep.room.findExitTo(my_next_escape_room);
 					target = creep.pos.findClosestByPath(exit);
 				}
