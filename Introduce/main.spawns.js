c4953c8e085d7573e6e4998e101588d65c22b886
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-03-20 04:39:58 +0300

    Update main.spawns.js

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -166,6 +166,14 @@ var spawns = {
 				var controller = spawn.room.controller;
 				const CL = controller.level;
 				var N = Memory.totals.CreepsNumber;
+				
+				if(CL >= 4) spawns.tryCreateCreep(spawn,        80808, 20, Memory.totals.WORK< 8? 1:0); // E 1600 Worker
+				if(CL >= 4) spawns.tryCreateCreep(spawn,        70707, 20, Memory.totals.WORK< 7? 1:0); // E 1400 Worker
+				if(CL >= 3) spawns.tryCreateCreep(spawn,        60606, 20, Memory.totals.WORK< 6? 1:0); // E 1200 Worker
+				if(CL >= 3) spawns.tryCreateCreep(spawn,        50505, 20, Memory.totals.WORK< 5? 1:0); // E 1000 Worker
+				if(CL >= 3) spawns.tryCreateCreep(spawn,        40404, 20, Memory.totals.WORK< 4? 1:0); // E  800 Worker
+				if(CL >= 2) spawns.tryCreateCreep(spawn,          506, 15, Memory.totals.CARRY< 6? 1:0); // E 550 Carier
+				if(CL >= 1) spawns.tryCreateCreep(spawn,          303, 10, Memory.totals.CARRY< 3? 1:0); // E 300 Carier
 				// 22*1800+2*1300+4*650 = 44800 -> 30 per game tick
 				if(false) {
 					spawns.tryCreateCreep(spawn,             3015, 31, 1); // V 1-1 E  1800   Carier
@@ -218,16 +226,6 @@ var spawns = {
 						if(spawn.name == 'Spawn2' || !!rerun) spawns.tryCreateCreep(spawn,  500000007000006, 77, 1); // V 1-1 E    Healer
 					}
 				}
-
-				if(CL >= 4) spawns.tryCreateCreep(spawn,        80808, 20, Memory.totals.WORK< 8? 1:0); // E 1600 Worker
-				if(CL >= 4) spawns.tryCreateCreep(spawn,        70707, 20, Memory.totals.WORK< 7? 1:0); // E 1400 Worker
-				if(CL >= 3) spawns.tryCreateCreep(spawn,        60606, 20, Memory.totals.WORK< 6? 1:0); // E 1200 Worker
-				if(CL >= 3) spawns.tryCreateCreep(spawn,        50505, 20, Memory.totals.WORK< 5? 1:0); // E 1000 Worker
-				if(CL >= 3) spawns.tryCreateCreep(spawn,        40404, 20, Memory.totals.WORK< 4? 1:0); // E  800 Worker
-
-				if(CL >= 2) spawns.tryCreateCreep(spawn,          506, 15, Memory.totals.CARRY< 6? 1:0); // E 550 Carier
-
-				if(CL >= 1) spawns.tryCreateCreep(spawn,          303, 10, Memory.totals.CARRY< 3? 1:0); // E 300 Carier
 			}
 
 			if(spawn.spawning) {
