f4adbf73f1ba993b6781499352607151bb77e112
Creeps Proger <creepsproger@gmail.com>
2020-03-22 22:36:48 +0300

    new Error().stack

--- Introduce/role.energy.transferer.js
+++ Introduce/role.energy.transferer.js
@@ -161,7 +161,7 @@ var roleEnergyTransferer = {
 				}
 			}
 
-			metrix.cpu.step_time(creep, 'transfering', ''+(new Error).lineNumber);
+			metrix.cpu.step_time(creep, 'transfering', new Error().stack);
 
 			if(target) {
 
@@ -207,7 +207,7 @@ var roleEnergyTransferer = {
 				creep.memory.transfering = false;
 			}
 		}
-		metrix.cpu.step_time(creep, 'transfering', ''+(new Error).lineNumber);
+		metrix.cpu.step_time(creep, 'transfering', new Error().stack);
 
 		metrix.cpu.role_time(creep, 'transfering');
 		if(!creep.memory.transfering) {
