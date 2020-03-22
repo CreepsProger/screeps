86408fb399f8c4111b87213908ba91542e49cd3f
Creeps Proger <creepsproger@gmail.com>
2020-03-22 15:22:35 +0300

    TO_SPAWN_DEFENDERS_ROOM

--- Introduce/main.conditions.js
+++ Introduce/main.conditions.js
@@ -1,23 +1,23 @@
 const constants = require('main.constants');
 
 const conditions = {
-	  MAIN_ROOM_CRISIS: 				function() {
+	  MAIN_ROOM_CRISIS: 						function() {
 																			return 	Memory.totals.WORK < 25 && Memory.totals.CARRY < 50;}
-	, CLAIMING_ROOM_CRISIS: 		function() {
+	, CLAIMING_ROOM_CRISIS: 				function() {
 																			return 	Memory.totals.WORK < 50;}
-	, TO_SPAWN_MAIN_ROOMS: 			function() {
+	, TO_SPAWN_MAIN_ROOMS: 					function() {
 																			return 	Game.cpu.bucket > constants.CPU_BUCKET_TO_SPAWN_MAIN_ROOMS;}
-	, TO_SPAWN_CLAIMING_ROOMS: 	function() {
+	, TO_SPAWN_CLAIMING_ROOMS: 			function() {
 																			return 	Game.cpu.bucket > constants.CPU_BUCKET_TO_SPAWN_CLAIMING_ROOMS &&
 						 																	!conditions.MAIN_ROOM_CRISIS();}
-	, TO_SPAWN_CLAIMING_ROOMS2:	function() {
+	, TO_SPAWN_CLAIMING_ROOMS2:			function() {
 																			return 	Game.cpu.bucket > constants.CPU_BUCKET_TO_SPAWN_CLAIMING_ROOMS2 &&
 		 																					!conditions.MAIN_ROOM_CRISIS();}
-	, TO_SPAWN_KEEPERS_ROOMS: 	function() {
+	, TO_SPAWN_KEEPERS_ROOMS: 			function() {
 																			return 	Game.cpu.bucket > constants.CPU_BUCKET_TO_SPAWN_KEEPERS_ROOMS &&
 																							!conditions.CLAIMING_ROOM_CRISIS() &&
 																							!Memory.stop_upgrading;}
-	, TO_SPAWN_TO_ATTACK: 			function(){
+	, TO_SPAWN_TO_ATTACK: 					function(){
 																			if(Game.cpu.bucket > constants.CPU_BUCKET_TO_START_SPAWN_TO_ATTACK) {
 																				Memory.spawn_to_attack = true;
 																			}
@@ -27,6 +27,8 @@ const conditions = {
 																			return 	Memory.spawn_to_attack &&
 																							!conditions.CLAIMING_ROOM_CRISIS() &&
 																							!Memory.stop_upgrading;}
+	, TO_SPAWN_DEFENDERS_ROOM:			function(roomName) {
+																			return 	Game.rooms[roomName].find(FIND_HOSTILE_CREEPS).length > 0;}
 };
 
 
--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -199,6 +199,13 @@ var spawns = {
 						if(spawn.name == 'Spawn3' || !!rerun) spawns.tryCreateCreep(spawn,            90909, 64, 1, 3); // V 1-1 E    Worker
 						if(spawn.name == 'Spawn3' || !!rerun) spawns.tryCreateCreep(spawn,             2412, 61, 1, 3); // V 1-1 E    Carier
 					}
+					if(conditions.TO_SPAWN_DEFENDERS_ROOM('W29S33')) {
+						if(spawn.name == 'Spawn3' || !!rerun) spawns.tryCreateCreep(spawn, 1000000500000015,105, 1, 1); // V 1-2 E  Attacker
+					}
+					if(conditions.TO_SPAWN_DEFENDERS_ROOM('W29S35')) {
+						if(spawn.name == 'Spawn3' || !!rerun) spawns.tryCreateCreep(spawn, 1000000500000015,125, 1, 1); // V 1-2 E  Attacker
+						if(spawn.name == 'Spawn2' || !!rerun) spawns.tryCreateCreep(spawn,  500000007000006,127, 1, 1); // V 1-1 E    Healer
+					}
 					if(conditions.TO_SPAWN_CLAIMING_ROOMS()) {
 						if(spawn.name == 'Spawn2' || spawn.name == 'Spawn3' || !!rerun)
 						 																			spawns.tryCreateCreep(spawn,    2000000000002, 50, 1, 1); // V 1-1 E   Claimer
@@ -222,13 +229,11 @@ var spawns = {
 						if(spawn.name == 'Spawn3' || !!rerun) spawns.tryCreateCreep(spawn,    2000000000002,140, 1, 1); // V 1-1 E   Claimer
 						if(spawn.name == 'Spawn3' || !!rerun) spawns.tryCreateCreep(spawn,    2000000000002,150, 1, 1); // V 1-1 E   Claimer
 						if(spawn.name == 'Spawn3' || !!rerun) spawns.tryCreateCreep(spawn,            90909,154, 1, 1); // V 1-1 E    Worker
-						// if(spawn.name == 'Spawn3' || !!rerun) spawns.tryCreateCreep(spawn, 1000000500000015,125, 1, 1); // V 1-2 E  Attacker
 						// if(spawn.name == 'Spawn3' || !!rerun) spawns.tryCreateCreep(spawn,            90909,124, 1, 2); // V 1-1 E    Worker
 						// if(spawn.name == 'Spawn3' || !!rerun) spawns.tryCreateCreep(spawn,             2412,121, 1, 2); // V 1-1 E    Carier
 						// if(spawn.name == 'Spawn3' || !!rerun) spawns.tryCreateCreep(spawn,            90909,134, 1, 2); // V 1-1 E    Worker
 						// if(spawn.name == 'Spawn3' || !!rerun) spawns.tryCreateCreep(spawn,             2412,131, 1, 2); // V 1-1 E    Carier
 						// if(spawn.name == 'Spawn3' || !!rerun) spawns.tryCreateCreep(spawn,             2412,111, 1); // V 1-1 E    Carier
-						if(spawn.name == 'Spawn3' || !!rerun) spawns.tryCreateCreep(spawn, 1000000500000015,105, 1, 1); // V 1-2 E  Attacker
 						// if(spawn.name == 'Spawn3' || !!rerun) spawns.tryCreateCreep(spawn, 1000000500000015,115, 1, 1); // V 1-2 E  Attacker
 					}
 					//                                                                                      30)
