162974cfb8b32d9f5a61305dfa9d0e8605c9ea46
Creeps Proger <creepsproger@gmail.com>
2020-03-25 14:57:31 +0300

    +

--- Introduce/main.js
+++ Introduce/main.js
@@ -69,7 +69,7 @@ module.exports.loop = function () {
 
 	metrix.run(); 	console.log( '⏳', Math.trunc(Game.time/10000), Game.time%10000, 'main metrix run dt:', (dt = Game.cpu.getUsed()-dt,dt));
 	config.run(); 	console.log( '⏳', Math.trunc(Game.time/10000), Game.time%10000, 'main config run dt:', (dt = Game.cpu.getUsed()-dt,dt));
-	flags.run()	console.log( '⏳', Math.trunc(Game.time/10000), Game.time%10000, 'main flags run dt:', (dt = Game.cpu.getUsed()-dt,dt));
+	flags.run();	console.log( '⏳', Math.trunc(Game.time/10000), Game.time%10000, 'main flags run dt:', (dt = Game.cpu.getUsed()-dt,dt));
 	links.run();	console.log( '⏳', Math.trunc(Game.time/10000), Game.time%10000, 'main links run dt:', (dt = Game.cpu.getUsed()-dt,dt));
 	towers.run();	console.log( '⏳', Math.trunc(Game.time/10000), Game.time%10000, 'main towers run dt:', (dt = Game.cpu.getUsed()-dt,dt));
 	spawns.run();	console.log( '⏳', Math.trunc(Game.time/10000), Game.time%10000, 'main spawns run dt:', (dt = Game.cpu.getUsed()-dt,dt));
