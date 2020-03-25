28dae3262b9e456d8f4f921bbb5f4a0a80b30e1c
Creeps Proger <creepsproger@gmail.com>
2020-03-25 20:54:08 +0300

    +

--- Introduce/main.links.js
+++ Introduce/main.links.js
@@ -47,7 +47,7 @@ var links = {
 		 if(true) {
 			 var link_objs = cash.getLinks(creep.room).filter( (link) => {
 					 return link.store.getUsedCapacity(RESOURCE_ENERGY) > 0 &&
-					 				!!(links.links.find((ft) => ft.to == link.id)) &&
+					 				!!links.links.find((ft) => ft.to == link.id) &&
 					 				tools.checkTarget(executer,link.id);
 			   }
 			 );
