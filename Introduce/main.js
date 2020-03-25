a5fe6452ecb2f700b728b97408e166a2c952b72b
Creeps Proger <creepsproger@gmail.com>
2020-03-25 14:48:58 +0300

    TICKS_TO_CHECK_NON_EXISTING

--- Introduce/main.js
+++ Introduce/main.js
@@ -12,56 +12,61 @@ const cash = require('cash');
 var role = require('role.claimer');
 
 module.exports.loop = function () {
-	var dt = Game.cpu.getUsed();
-	// console.log( '⏳', Game.cpu.getUsed() + '/' + Game.cpu.tickLimit);
-	for(var name in Memory.creeps) {
-		var creep = Game.creeps[name];
-		if(!creep) {
-			const code0 = '0'.charCodeAt(0);
-			const nnn = name.charCodeAt(7) - code0;
-			var weight = nnn >= 0 && nnn < 10? nnn:0;
-			const nn = name.charCodeAt(8) - code0;
-			if(nn >= 0 && nn < 10)
-				weight = weight*10+nn;
-			const n = name.charCodeAt(9) - code0;
-			if(n >= 0 && n < 10)
-				weight = weight*10+n;
-			const	total_sum_pst = Object.keys(Memory.CreepsIdleTicksByWeight).reduce((p,w) => p +
-													Object.keys(Memory.CreepsIdleTicksByWeight[w]).reduce((pp,c) => pp +
-																			(!Memory.CreepsIdleTicksByWeight[w][c].pc?1:Memory.CreepsIdleTicksByWeight[w][c].pc),0),0);
-			const	total_cnt_pst = Object.keys(Memory.CreepsIdleTicksByWeight).reduce((p,w) => p +
-													Object.keys(Memory.CreepsIdleTicksByWeight[w]).reduce((pp,c) => pp +
-																			!!Memory.CreepsIdleTicksByWeight[w][c].w,0),0);
-			const	total_avg_pst = Math.round(total_sum_pst/total_cnt_pst);
-			const sum_pst = Object.keys(Memory.CreepsIdleTicksByWeight[weight]).reduce((p,c) => p +
-															(!Memory.CreepsIdleTicksByWeight[weight][c].pc?1:Memory.CreepsIdleTicksByWeight[weight][c].pc),0);
 
-			console.log( '✒️', Math.trunc(Game.time/10000), Game.time%10000
-									, 'Clearing non-existing creep memory:'
-									, name, weight//, 'cpu:', JSON.stringify(creep.memory.cpu)
-									, 'sum idle pst:', sum_pst, JSON.stringify(Memory.CreepsIdleTicksByWeight[weight])
-									, 'total_sum_pst:', total_sum_pst
-									, 'total_cnt_pst:', total_cnt_pst
-									, 'total_avg_pst:', total_avg_pst
-									, JSON.stringify(Memory.CreepsIdleTicksByWeight));
-			Memory.CreepsIdleTicksByWeight[weight] = {};
-			delete Memory.creeps[name];
-		}
-		else if(creep.ticksToLive == 1) {
-			const sum_role_cpu = Object.keys(creep.memory.cpu).reduce((sum,role) => sum + creep.memory.cpu[role],0);
-			const max_role_cpu = Object.keys(creep.memory.cpu).reduce((a,role) =>
-																																!creep.memory.cpu[a.max_role] || creep.memory.cpu[a.max_role] < creep.memory.cpu[role]?
-																															  {max_role:role, v:creep.memory.cpu[role]}:
-																																{max_role:a.max_role, v:a.v},
-																																{max_role:'', v:0});
-			console.log( '✒️', Math.trunc(Game.time/10000), Game.time%10000
-									, 'ticksToLive:', creep.ticksToLive
-									, name
-									, 'idle:', creep.memory.idle, JSON.stringify(Memory.CreepsIdleTicksByWeight[creep.memory.weight][creep.memory.n])
-									, 'cpu:', sum_role_cpu, JSON.stringify(max_role_cpu), JSON.stringify(creep.memory.cpu)
-								 );
+	var dt = Game.cpu.getUsed();
+
+	if(Game.time % constants.TICKS_TO_CHECK_NON_EXISTING == 0) {
+		// console.log( '⏳', Game.cpu.getUsed() + '/' + Game.cpu.tickLimit);
+		for(var name in Memory.creeps) {
+			var creep = Game.creeps[name];
+			if(!creep) {
+				const code0 = '0'.charCodeAt(0);
+				const nnn = name.charCodeAt(7) - code0;
+				var weight = nnn >= 0 && nnn < 10? nnn:0;
+				const nn = name.charCodeAt(8) - code0;
+				if(nn >= 0 && nn < 10)
+					weight = weight*10+nn;
+				const n = name.charCodeAt(9) - code0;
+				if(n >= 0 && n < 10)
+					weight = weight*10+n;
+				const	total_sum_pst = Object.keys(Memory.CreepsIdleTicksByWeight).reduce((p,w) => p +
+														Object.keys(Memory.CreepsIdleTicksByWeight[w]).reduce((pp,c) => pp +
+																				(!Memory.CreepsIdleTicksByWeight[w][c].pc?1:Memory.CreepsIdleTicksByWeight[w][c].pc),0),0);
+				const	total_cnt_pst = Object.keys(Memory.CreepsIdleTicksByWeight).reduce((p,w) => p +
+														Object.keys(Memory.CreepsIdleTicksByWeight[w]).reduce((pp,c) => pp +
+																				!!Memory.CreepsIdleTicksByWeight[w][c].w,0),0);
+				const	total_avg_pst = Math.round(total_sum_pst/total_cnt_pst);
+				const sum_pst = Object.keys(Memory.CreepsIdleTicksByWeight[weight]).reduce((p,c) => p +
+																(!Memory.CreepsIdleTicksByWeight[weight][c].pc?1:Memory.CreepsIdleTicksByWeight[weight][c].pc),0);
+
+				console.log( '✒️', Math.trunc(Game.time/10000), Game.time%10000
+										, 'Clearing non-existing creep memory:'
+										, name, weight//, 'cpu:', JSON.stringify(creep.memory.cpu)
+										, 'sum idle pst:', sum_pst, JSON.stringify(Memory.CreepsIdleTicksByWeight[weight])
+										, 'total_sum_pst:', total_sum_pst
+										, 'total_cnt_pst:', total_cnt_pst
+										, 'total_avg_pst:', total_avg_pst
+										, JSON.stringify(Memory.CreepsIdleTicksByWeight));
+				Memory.CreepsIdleTicksByWeight[weight] = {};
+				delete Memory.creeps[name];
+			}
+			else if(0 < creep.ticksToLive && creep.ticksToLive <= constants.TICKS_TO_CHECK_NON_EXISTING) {
+				const sum_role_cpu = Object.keys(creep.memory.cpu).reduce((sum,role) => sum + creep.memory.cpu[role],0);
+				const max_role_cpu = Object.keys(creep.memory.cpu).reduce((a,role) =>
+																																	!creep.memory.cpu[a.max_role] || creep.memory.cpu[a.max_role] < creep.memory.cpu[role]?
+																																  {max_role:role, v:creep.memory.cpu[role]}:
+																																	{max_role:a.max_role, v:a.v},
+																																	{max_role:'', v:0});
+				console.log( '✒️', Math.trunc(Game.time/10000), Game.time%10000
+										, 'ticksToLive:', creep.ticksToLive
+										, name
+										, 'idle:', creep.memory.idle, JSON.stringify(Memory.CreepsIdleTicksByWeight[creep.memory.weight][creep.memory.n])
+										, 'cpu:', sum_role_cpu, JSON.stringify(max_role_cpu), JSON.stringify(creep.memory.cpu)
+									 );
+			}
 		}
 	}
+
 	console.log( '⏳', Math.trunc(Game.time/10000), Game.time%10000, 'main run dt:', (dt = Game.cpu.getUsed()-dt,dt));
 
 	metrix.run();
