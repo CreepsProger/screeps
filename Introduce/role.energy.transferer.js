53321ec38f871bebd3357e1d510149327150049a
Creeps Proger <creepsproger@gmail.com>
2020-03-25 05:59:36 +0300

    +

--- Introduce/role.energy.transferer.js
+++ Introduce/role.energy.transferer.js
@@ -129,11 +129,10 @@ var roleEnergyTransferer = {
 				 creep.store.getFreeCapacity(RESOURCE_ENERGY) == 0 &&
 				 creep.memory.rerun) {
 				var t = Game.cpu.getUsed();
-				var storages = cash.getStorages();
+				var storages = cash.getStorages().filter((s) => s.store.getUsedCapacity(RESOURCE_ENERGY) < constants.START_UPGRADING_ENERGY);;
 				if(storages.length > 0) {
-					target = storages.reduce((p,c) => (p.store.getUsedCapacity(RESOURCE_ENERGY) < constants.START_UPGRADING_ENERGY &&
-																	p.store.getUsedCapacity(RESOURCE_ENERGY) * tools.getRangeTo(creep.pos,p.pos)
-																		< c.store.getUsedCapacity(RESOURCE_ENERGY) * tools.getRangeTo(creep.pos,c.pos))? p:c);
+					target = storages.reduce((p,c) => p.store.getUsedCapacity(RESOURCE_ENERGY) * tools.getRangeTo(creep.pos,p.pos)
+																					< c.store.getUsedCapacity(RESOURCE_ENERGY) * tools.getRangeTo(creep.pos,c.pos)? p:c);
 					const range_to_store = tools.getRangeTo(creep.pos,target.pos);
 					const store_energy_value = target.store.getUsedCapacity(RESOURCE_ENERGY);
 					if(range_to_store >= constants.RANGE_TO_STORE_1_TO_CONSOLE_LOG &&
@@ -156,11 +155,10 @@ var roleEnergyTransferer = {
 				 creep.store.getFreeCapacity(RESOURCE_ENERGY) == 0 &&
 				 creep.memory.rerun) {
 				var t = Game.cpu.getUsed();
-				var storages = cash.getStorages();
+				var storages = cash.getStorages().filter((s) => s.store.getUsedCapacity(RESOURCE_ENERGY) < 2*constants.START_UPGRADING_ENERGY);;
 				if(storages.length > 0) {
-					target = storages.reduce((p,c) => (p.store.getUsedCapacity(RESOURCE_ENERGY) < 2*constants.START_UPGRADING_ENERGY &&
-																							p.store.getUsedCapacity(RESOURCE_ENERGY) * tools.getRangeTo(creep.pos,p.pos)
-																	 						< c.store.getUsedCapacity(RESOURCE_ENERGY) * tools.getRangeTo(creep.pos,c.pos))? p:c);
+					target = storages.reduce((p,c) => p.store.getUsedCapacity(RESOURCE_ENERGY) * tools.getRangeTo(creep.pos,p.pos)
+																	 				< c.store.getUsedCapacity(RESOURCE_ENERGY) * tools.getRangeTo(creep.pos,c.pos)? p:c);
 					const range_to_store = tools.getRangeTo(creep.pos,target.pos);
 					const store_energy_value = target.store.getUsedCapacity(RESOURCE_ENERGY);
 					if(range_to_store >= constants.RANGE_TO_STORE_2_TO_CONSOLE_LOG &&
@@ -196,7 +194,7 @@ var roleEnergyTransferer = {
 				var storages = cash.getStorages();
 				if(storages.length > 0) {
 					target = storages.reduce((p,c) =>  tools.getRangeTo(creep.pos,p.pos)
-																	 < tools.getRangeTo(creep.pos,c.pos)? p:c);
+																	 				 < tools.getRangeTo(creep.pos,c.pos)? p:c);
 					const range_to_store = tools.getRangeTo(creep.pos,target.pos);
 					const store_energy_value = target.store.getUsedCapacity(RESOURCE_ENERGY);
 					if(range_to_store >= constants.RANGE_TO_STORE_3_TO_CONSOLE_LOG &&
