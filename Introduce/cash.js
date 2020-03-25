ad3d1cef0c5a3d1f6026b21e595d8144797ec0b1
Creeps Proger <creepsproger@gmail.com>
2020-03-25 05:35:17 +0300

    +

--- Introduce/cash.js
+++ Introduce/cash.js
@@ -16,11 +16,10 @@ var cash = {
 		if((Game.time % constants.TICKS_TO_RESET_CASH != 0) || !Memory.cash.storages_ids) {
 			Memory.cash.storages_ids = _.filter(Game.structures,
 				 (structure) => !!structure.my && structure.structureType == STRUCTURE_STORAGE).map((obj) => obj.id);
-				 console.log( '🔜💡1️⃣', Math.trunc(Game.time/10000), Game.time%10000
-										 , 'Memory.cash.storages_ids:', JSON.stringify(Memory.cash.storages_ids)
-										 , 'storages:', JSON.stringify(Memory.cash.storages_ids.map((id) => Game.getObjectById(id)))
-									);
-
+				 // console.log( '🔜💡1️⃣', Math.trunc(Game.time/10000), Game.time%10000
+					// 					 , 'Memory.cash.storages_ids:', JSON.stringify(Memory.cash.storages_ids)
+					// 					 , 'storages:', JSON.stringify(Memory.cash.storages_ids.map((id) => Game.getObjectById(id)))
+					// 				);
 		}
 		if(Memory.cash.storages_time != Game.time){
 			Memory.cash.storages = Memory.cash.storages_ids.map((id) => Game.getObjectById(id));
