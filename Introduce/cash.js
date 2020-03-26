47c11879021e0af09e09d8e4903ef9242b5e05d6
Creeps Proger <creepsproger@gmail.com>
2020-03-26 17:57:10 +0300

    getRoomEntity

--- Introduce/cash.js
+++ Introduce/cash.js
@@ -31,18 +31,12 @@ var cash = {
 		return cash_objects;
 	},
 
-	controller: {},
+	controller: 0,
 	getController: function(room) {
-		var property = cash.initEntity(STRUCTURE_CONTROLLER,room.name);
-		if(Game.time % constants.TICKS_TO_RESET_CASH == 0 || property.time == 0) {
-			property.ids = room.find(FIND_STRUCTURES, {
-				filter: (structure) => 	structure.structureType == STRUCTURE_CONTROLLER }).map((obj) => obj.id);
-		}
-		if(property.time != Game.time) {
-			cash.controller[room.name] = property.ids.map((id) => Game.getObjectById(id))[0];
- 			property.time = Game.time;
- 		}
-		return cash.controller[room.name];
+		return cash.getRoomEntity(STRUCTURE_CONTROLLER+1, cash.controller, room, (room) => {
+			return room.find(FIND_STRUCTURES, { filter:
+				(structure) => 	structure.structureType == STRUCTURE_CONTROLLER }).map((obj) => obj.id);
+			})[0];
 	},
 
 	storage: 0,
@@ -53,7 +47,7 @@ var cash = {
 			})[0];
 	},
 
-	containers: [],
+	containers: {},
 	getContainers: function(room) {
 		return cash.getRoomEntity(STRUCTURE_CONTAINER, cash.containers, room, (room) => {
 			return room.find(FIND_STRUCTURES, { filter:
@@ -63,74 +57,44 @@ var cash = {
 
 	extensions: {},
 	getExtensions: function(room) {
-		var property = cash.initEntity(STRUCTURE_EXTENSION,room.name);
-		if(Game.time % constants.TICKS_TO_RESET_CASH == 0 || property.time == 0) {
-			property.ids = room.find(FIND_STRUCTURES, {
-				filter: (structure) => 	structure.structureType == STRUCTURE_SPAWN ||
-																structure.structureType == STRUCTURE_EXTENSION }).map((obj) => obj.id);
-		}
-		if(property.time != Game.time) {
-			cash.extensions[room.name] = property.ids.map((id) => Game.getObjectById(id));
- 			property.time = Game.time;
- 		}
-		return cash.extensions[room.name];
+		return cash.getRoomEntity(STRUCTURE_EXTENSION, cash.extensions, room, (room) => {
+			return room.find(FIND_STRUCTURES, { filter:
+				(structure) => 	structure.structureType == STRUCTURE_SPAWN ||
+												structure.structureType == STRUCTURE_EXTENSION }).map((obj) => obj.id);
+			});
 	},
 
 	links: {},
 	getLinks: function(room) {
-		var property = cash.initEntity(STRUCTURE_LINK,room.name);
-		if(Game.time % constants.TICKS_TO_RESET_CASH == 0 || property.time == 0) {
-			property.ids = room.find(FIND_STRUCTURES, {
-				filter: (structure) => 	structure.structureType == STRUCTURE_LINK }).map((obj) => obj.id);
-		}
-		if(property.time != Game.time) {
-			cash.links[room.name] = property.ids.map((id) => Game.getObjectById(id));
- 			property.time = Game.time;
- 		}
-		return cash.links[room.name];
+		return cash.getRoomEntity(STRUCTURE_LINK, cash.links, room, (room) => {
+			return room.find(FIND_STRUCTURES, { filter:
+				(structure) => 	structure.structureType == STRUCTURE_LINK }).map((obj) => obj.id);
+			});
 	},
 
 	towers: {},
 	getTowers: function(room) {
-		var property = cash.initEntity(STRUCTURE_TOWER,room.name);
-		if(Game.time % constants.TICKS_TO_RESET_CASH == 0 || property.time == 0) {
-			property.ids = room.find(FIND_STRUCTURES, {
-				filter: (structure) => 	structure.structureType == STRUCTURE_TOWER }).map((obj) => obj.id);
-		}
-		if(property.time != Game.time) {
-			cash.towers[room.name] = property.ids.map((id) => Game.getObjectById(id));
- 			property.time = Game.time;
- 		}
-		return cash.towers[room.name];
+		return cash.getRoomEntity(STRUCTURE_LINK, cash.towers, room, (room) => {
+			return room.find(FIND_STRUCTURES, { filter:
+				(structure) => 	structure.structureType == STRUCTURE_TOWER }).map((obj) => obj.id);
+			});
 	},
 
-	all_my_towers: 0,
+	all_my_towers: {},
 	getAllMyTowers: function() {
-		var property = cash.initEntity(STRUCTURE_TOWER);
-		if(Game.time % constants.TICKS_TO_RESET_CASH == 0 || property.time == 0) {
-			property.ids = _.filter(Game.structures,
+		return cash.getRoomEntity(STRUCTURE_TOWER, all_my_towers, 'all', (room) => {
+			return _.filter(Game.structures,
 				 (structure) => !!structure.my && structure.structureType == STRUCTURE_TOWER).map((obj) => obj.id);
-		}
-		if(property.time != Game.time) {
-			cash.all_my_towers = property.ids.map((id) => Game.getObjectById(id));
- 			property.time = Game.time;
- 		}
-		return cash.all_my_towers;
-	},
+			 });
+ 	},
 
-	storages: 0,
+	all_my_storages: {},
 	getStorages: function() {
-		var property = cash.initEntity(STRUCTURE_STORAGE);
- 		if(Game.time % constants.TICKS_TO_RESET_CASH == 0 || property.time == 0) {
- 			property.ids = _.filter(Game.structures,
+		return cash.getRoomEntity(STRUCTURE_STORAGE, all_my_storages, 'all', (room) => {
+ 			return _.filter(Game.structures,
  				 (structure) => !!structure.my && structure.structureType == STRUCTURE_STORAGE).map((obj) => obj.id);
- 		}
-		if(property.time != Game.time) {
-			cash.storages = property.ids.map((id) => Game.getObjectById(id));
- 			property.time = Game.time;
- 		}
-		return cash.storages;
-	}
+			 });
+ 	}
 
 };
 
