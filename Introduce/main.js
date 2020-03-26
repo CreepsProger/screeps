7e76d4edd8e68300065667dda056238c797c8bd1
Creeps Proger <creepsproger@gmail.com>
2020-03-26 11:28:50 +0300

    +

--- Introduce/main.js
+++ Introduce/main.js
@@ -92,7 +92,7 @@ module.exports.loop = function () {
 	var main_part_dt = Math.round((Game.cpu.getUsed()) * 100)/100;
 	Memory.cpu_main_part_dt += main_part_dt;
 
-	Memory.cpu_main_part.others. += (dt = Game.cpu.getUsed()-dt,dt);
+	Memory.cpu_main_part.others += (dt = Game.cpu.getUsed()-dt,dt);
 	if(Game.time % constants.TICKS_TO_CHECK_CPU_MAIN_PART == 0) {
 		console.log( '⏳', Math.trunc(Game.time/10000), Game.time%10000, 'main delete run dt:', (dt = Game.cpu.getUsed()-dt,dt));
 		Memory.cpu_main_part = {perf:0, clearing:0, metrix:0, config:0, flags:0, links:0, towers:0, spawns:0, metrix2:0, others:0};
