8d7fba6ffc511899fb5d4cabcca7b1127bec562c
Creeps Proger <creepsproger@gmail.com>
2020-03-26 21:32:02 +0300

    0.4

--- Introduce/role.energy.transferer.js
+++ Introduce/role.energy.transferer.js
@@ -113,7 +113,7 @@ var roleEnergyTransferer = {
 						if(!!target) {
 							if(creep.memory.prev_target_id || creep.memory.prev_target_id != target.id || true) {
 								var dt = Math.round((Game.cpu.getUsed() - t)*100)/100;
-								if(dt > 0.2)
+								if(dt > 0.4)
 									console.log( '⭕️', Math.trunc(Game.time/10000), Game.time%10000, 'dt=' + dt, creep
 															, 'infra id:', target.id
 															, use_look, 'infras:', JSON.stringify(infras)
