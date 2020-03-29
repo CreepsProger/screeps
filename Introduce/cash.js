5f898fff323b5fd94a469c3249b98374ab85e64c
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-03-29 05:49:31 +0300

    Update cash.js

--- Introduce/cash.js
+++ Introduce/cash.js
@@ -40,8 +40,8 @@ var cash = {
  			entry.time = Game.time;
  		}
 		var objects = cash_objects[entry_id][subentry_id];
-		var dt = Math.round((Game.cpu.getUsed() - t)*100)/100;
-		if(dt > 0.01 || (false && type == STRUCTURE_TOWER)) {
+		var dt = Math.round((Game.cpu.getUsed() - t)*1000)/1000;
+		if(dt > 0.001 || (false && type == STRUCTURE_TOWER)) {
 			console.log( '💵', Math.trunc(Game.time/10000), Game.time%10000, 'dt=' + dt
 									, '[' + type + '][' + entry_id + '][' + subentry_id + ']'
 									, 'objects.length:', objects.length
