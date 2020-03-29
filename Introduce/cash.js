8c2fec9cf625c08b9bd52d5e9f3dde843eb8141e
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-03-29 08:56:56 +0300

    Update cash.js

--- Introduce/cash.js
+++ Introduce/cash.js
@@ -39,7 +39,7 @@ var cash = {
 		}
 		var cash_o = cash_objects[entry_id][subentry_id];
 		if(entry.time < cash.time || cash.time == 0) {
-			cash_o.objs: cash_o.ids.map((id) => Game.getObjectById(id))};
+			cash_o.objs: cash_o.ids.map((id) => Game.getObjectById(id));
  			entry.time = Game.time;
 			cash.time = Game.time;
  		}
