289ffb365bd72d5d4c012e35b689419ba2fae35a
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-03-29 10:35:07 +0300

    Update cash.js

--- Introduce/cash.js
+++ Introduce/cash.js
@@ -30,7 +30,7 @@ var cash = {
 		const entry_id = !entry_path ? 0 : !entry_path.length ? entry_path : entry_path.length > 0 ? entry_path[0]:0;
 		const subentry_id = !entry_path ? 100 : !entry_path.length ? 100 : entry_path.length > 1 ? entry_path[1]:100;
 		var entry = cash.initEntry(type, entry_id, subentry_id);
- 		if(Game.time % constants.TICKS_TO_RESET_CASH == 0 || entry.time == 0 || entry.time < cash.time) {
+ 		if(Game.time % constants.TICKS_TO_RESET_CASH == 0 || entry.time == 0 || cash.time == 0) {
  			entry.ids = get_ids();
 			if(!cash_objects[entry_id]) {
 				cash_objects[entry_id] = {};
