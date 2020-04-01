d012cfeddd1ce3c7dcb642ac803abe8ddfa6ae46
Creeps Proger <creepsproger@gmail.com>
2020-04-01 12:41:00 +0300

    console.log( '📲

--- Introduce/main.terminals.js
+++ Introduce/main.terminals.js
@@ -28,9 +28,16 @@ var terminals = {
 		var to_a = to.store.getUsedCapacity(RESOURCE_ENERGY) + to.room.storage.store.getUsedCapacity(RESOURCE_ENERGY);
 		var amount = Math.max(Math.floor((from_a - to_a) / 2) - constants.MIN_TERMINAL_ENERGY,constants.MIN_ENERGY_TO_TERMINAL_SEND);
 
+		console.log( '📲', Math.trunc(Game.time/10000), Game.time%10000
+							 , '\namount:', amount
+							 , '\nvalues:', before
+							 , '\nfrom:', from.pos.roomName
+							 , '\nto:', to.pos.roomName
+						 	 );
+
 	 	if(	!!from && !!to &&
 			 	amount > constants.MIN_ENERGY_TO_TERMINAL_SEND &&
-		  	t.store.getUsedCapacity(RESOURCE_ENERGY) > constants.MIN_TERMINAL_ENERGY) {
+		  	t.store.getUsedCapacity(RESOURCE_ENERGY) > amount + constants.MIN_TERMINAL_ENERGY) {
 			var before = all.map((t) => t.pos.roomName
 			 														+ '(' + t.store.getUsedCapacity(RESOURCE_ENERGY)
 				 											 		+ '+' + t.room.storage.store.getUsedCapacity(RESOURCE_ENERGY)
