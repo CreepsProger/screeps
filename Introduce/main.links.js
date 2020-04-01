c511453a07d95cf70e5a759ff76626935f0107ca
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-04-01 20:45:52 +0300

    Update main.links.js

--- Introduce/main.links.js
+++ Introduce/main.links.js
@@ -8,8 +8,8 @@ var links = {
 		   { from: '5e62c05bf15a4888dff60e26', to: '5e5ab4f1142d6b46f3c86280'}
 		 , { from: '5e61d337f15a48007bf5b603', to: '5e5ab4f1142d6b46f3c86280'}
 		 , { from: '5e6778dfcfa64f5485786f84', to: '5e5ab4f1142d6b46f3c86280'}
-		 , { from: '5e56dc7a28e44c6f77878b87', to: '5e583a7b7a54e3585a982b96'}
-		 , { from: '5e5f8ed0124b9b1087db5d47', to: '5e583a7b7a54e3585a982b96'}
+		 , { from: '5e56dc7a28e44c6f77878b87', to: '5e84c3a416b8c6668c29708e'}
+		 , { from: '5e5f8ed0124b9b1087db5d47', to: '5e84c3a416b8c6668c29708e'}
 		 , { from: '5e70e2cac19c1cd2f3542b55', to: '5e7dc9b130d83f0ebc107bf0'}
 		 , { from: '5e7dd868b4377d741d9d7465', to: '5e7dc9b130d83f0ebc107bf0'}
 	 ],
