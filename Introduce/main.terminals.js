2006f6a95bf15f5ea2e06c397584c2751c5c972e
Creeps Proger <creepsproger@gmail.com>
2020-04-01 11:14:31 +0300

    , 'err:', err

--- Introduce/main.terminals.js
+++ Introduce/main.terminals.js
@@ -21,12 +21,15 @@ var terminals = {
 			 		< (c.store.getUsedCapacity(RESOURCE_ENERGY) + c.room.storage.store.getUsedCapacity(RESOURCE_ENERGY))/10000
 			 ? p:c;
 		 });
+		 var from_a = from.store.getUsedCapacity(RESOURCE_ENERGY) + from.room.storage.store.getUsedCapacity(RESOURCE_ENERGY))
+		 var to_a = to.store.getUsedCapacity(RESOURCE_ENERGY) + to.room.storage.store.getUsedCapacity(RESOURCE_ENERGY))
 
-		 if(!!from && !!to) {
-			 var err = from.send(RESOURCE_ENERGY,from.store.getUsedCapacity(RESOURCE_ENERGY) - 20000, to.pos.roomName);
+		 if(!!from && !!to && from_a - to_a > 20000) {
+			 var v = Math.min(from_a - to_a, from.store.getUsedCapacity(RESOURCE_ENERGY));
+			 var err = from.send(RESOURCE_ENERGY, v, to.pos.roomName);
 
 	 		 console.log( '📲', Math.trunc(Game.time/10000), Game.time%10000
-			 						, 'err:', err
+			 						, 'amount:', v, 'err:', err
 			 						, '\nfrom:', JSON.stringify(from)
 	 							 	, '\nto:', JSON.stringify(to)
 	 							 	, '\nall:', JSON.stringify(all)
