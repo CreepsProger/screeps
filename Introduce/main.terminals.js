5e775ad5335019f81b181d1166615714353b7784
Creeps Proger <creepsproger@gmail.com>
2020-04-01 11:56:30 +0300

    new

--- Introduce/main.terminals.js
+++ Introduce/main.terminals.js
@@ -26,7 +26,7 @@ var terminals = {
 					});
 		var from_a = from.store.getUsedCapacity(RESOURCE_ENERGY) + from.room.storage.store.getUsedCapacity(RESOURCE_ENERGY);
 		var to_a = to.store.getUsedCapacity(RESOURCE_ENERGY) + to.room.storage.store.getUsedCapacity(RESOURCE_ENERGY);
-		var values = all.map((t) => t.pos.roomName);
+		var values = all.map((t) => new {t.pos.roomName:t.store.getUsedCapacity(RESOURCE_ENERGY)});
 		  // return { room: t.pos.roomName
 			//  		   , terminal: t.store.getUsedCapacity(RESOURCE_ENERGY)
 			//     	 , storage: t.storage.store.getUsedCapacity(RESOURCE_ENERGY)
