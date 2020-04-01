1a3a491f10395a642d4594b2cbea04edeced604a
Creeps Proger <creepsproger@gmail.com>
2020-04-01 11:53:36 +0300

    console.log

--- Introduce/main.terminals.js
+++ Introduce/main.terminals.js
@@ -4,39 +4,42 @@ const cash = require('cash');
 
 var terminals = {
 
-   run: function() {
-		 if(Game.time % constants.TICKS_TO_TERMINAL_SEND)
-		  	return;
+	run: function() {
+		console.log( '📲', Math.trunc(Game.time/10000), Game.time%10000);
+		 	if(Game.time % constants.TICKS_TO_TERMINAL_SEND)
+		 		return;
 
-		 var all = cash.getAllMyTerminals();
-		 var from = all.reduce((p,c) => {
-			 return p.store.getUsedCapacity(RESOURCE_ENERGY) > constants.MIN_ENERGY_TO_TERMINAL_SEND &&
-			 	(p.store.getUsedCapacity(RESOURCE_ENERGY) + p.room.storage.store.getUsedCapacity(RESOURCE_ENERGY))/constants.MIN_ENERGY_TO_TERMINAL_SEND
-			 		> (c.store.getUsedCapacity(RESOURCE_ENERGY) + c.room.storage.store.getUsedCapacity(RESOURCE_ENERGY))/constants.MIN_ENERGY_TO_TERMINAL_SEND
-			 ? p:c;
-		 });
-		 var to = all.reduce((p,c) => {
-			 return p.store.getFreeCapacity(RESOURCE_ENERGY) > constants.MIN_ENERGY_TO_TERMINAL_SEND &&
-			 	(p.store.getUsedCapacity(RESOURCE_ENERGY) + p.room.storage.store.getUsedCapacity(RESOURCE_ENERGY))/constants.MIN_ENERGY_TO_TERMINAL_SEND
-			 		< (c.store.getUsedCapacity(RESOURCE_ENERGY) + c.room.storage.store.getUsedCapacity(RESOURCE_ENERGY))/constants.MIN_ENERGY_TO_TERMINAL_SEND
-			 ? p:c;
-		 });
-		 var from_a = from.store.getUsedCapacity(RESOURCE_ENERGY) + from.room.storage.store.getUsedCapacity(RESOURCE_ENERGY);
-		 var to_a = to.store.getUsedCapacity(RESOURCE_ENERGY) + to.room.storage.store.getUsedCapacity(RESOURCE_ENERGY);
-		 var values = all.map((t) => t.pos.roomName);
+		console.log( '📲', Math.trunc(Game.time/10000), Game.time%10000);
+
+		var all = cash.getAllMyTerminals();
+		var from = all.reduce((p,c) => {
+			return p.store.getUsedCapacity(RESOURCE_ENERGY) > constants.MIN_ENERGY_TO_TERMINAL_SEND &&
+			 			(p.store.getUsedCapacity(RESOURCE_ENERGY) + p.room.storage.store.getUsedCapacity(RESOURCE_ENERGY))/constants.MIN_ENERGY_TO_TERMINAL_SEND
+			 			> (c.store.getUsedCapacity(RESOURCE_ENERGY) + c.room.storage.store.getUsedCapacity(RESOURCE_ENERGY))/constants.MIN_ENERGY_TO_TERMINAL_SEND
+			 			? p:c;
+					});
+		var to = all.reduce((p,c) => {
+			return p.store.getFreeCapacity(RESOURCE_ENERGY) > constants.MIN_ENERGY_TO_TERMINAL_SEND &&
+			 			(p.store.getUsedCapacity(RESOURCE_ENERGY) + p.room.storage.store.getUsedCapacity(RESOURCE_ENERGY))/constants.MIN_ENERGY_TO_TERMINAL_SEND
+			 			< (c.store.getUsedCapacity(RESOURCE_ENERGY) + c.room.storage.store.getUsedCapacity(RESOURCE_ENERGY))/constants.MIN_ENERGY_TO_TERMINAL_SEND
+			 			? p:c;
+					});
+		var from_a = from.store.getUsedCapacity(RESOURCE_ENERGY) + from.room.storage.store.getUsedCapacity(RESOURCE_ENERGY);
+		var to_a = to.store.getUsedCapacity(RESOURCE_ENERGY) + to.room.storage.store.getUsedCapacity(RESOURCE_ENERGY);
+		var values = all.map((t) => t.pos.roomName);
 		  // return { room: t.pos.roomName
 			//  		   , terminal: t.store.getUsedCapacity(RESOURCE_ENERGY)
 			//     	 , storage: t.storage.store.getUsedCapacity(RESOURCE_ENERGY)
 			// 		   });
- 		 console.log( '📲', Math.trunc(Game.time/10000), Game.time%10000
-		 						, 'VALUES:', JSON.stringify(values)
- 								);
+ 		console.log( '📲', Math.trunc(Game.time/10000), Game.time%10000
+							 , 'VALUES:', JSON.stringify(values)
+ 							 );
 
-		 if(!!from && !!to && from_a - to_a > constants.MIN_ENERGY_TO_TERMINAL_SEND) {
-			 var v = Math.min(from_a - to_a, from.store.getUsedCapacity(RESOURCE_ENERGY));
-			 var err = from.send(RESOURCE_ENERGY, v, to.pos.roomName);
+	 	if(!!from && !!to && from_a - to_a > constants.MIN_ENERGY_TO_TERMINAL_SEND) {
+			var v = Math.min(from_a - to_a, from.store.getUsedCapacity(RESOURCE_ENERGY));
+			var err = from.send(RESOURCE_ENERGY, v, to.pos.roomName);
 
-	 		 console.log( '📲', Math.trunc(Game.time/10000), Game.time%10000
+	 		console.log( '📲', Math.trunc(Game.time/10000), Game.time%10000
 			 						, 'amount:', v, 'err:', err
 			 						, '\nfrom:', JSON.stringify(from)
 	 							 	, '\nto:', JSON.stringify(to)
