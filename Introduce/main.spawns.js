bdb12af10cee47ccdbd15da8f8d704e445e32cdc
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-04-04 06:04:04 +0300

    Update main.spawns.js

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -259,7 +259,7 @@ var spawns = {
 									}
 									else {
 										if(spawn.name == 'Spawn3' || !!rerun) spawns.tryCreateCreep(spawn,    2000000000002,130, 1, 1); // V 1-1 E   Claimer
-										if(spawn.name == 'Spawn3' || !!rerun) spawns.tryCreateCreep(spawn,           140711,134, 1, 1); // V 1-1 E    Worker
+										if(spawn.name == 'Spawn3' || !!rerun) spawns.tryCreateCreep(spawn,           140711,134, 1, 2); // V 1-1 E    Worker
 										if(spawn.name == 'Spawn3' || !!rerun) spawns.tryCreateCreep(spawn,             2412,131, 1, 1); // V 1-1 E    Carier
 									}
 									// if(conditions.TO_SPAWN_ROOM_DEFENDERS('W28S36')) {
