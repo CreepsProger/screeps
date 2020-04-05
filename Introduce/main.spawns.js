d12216eb35aa2c45b583b6323338afaea52acaed
Creeps Proger <creepsproger@gmail.com>
2020-04-05 22:50:33 +0300

    +

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -222,9 +222,9 @@ var spawns = {
 					if(conditions.TO_SPAWN_MAIN_ROOMS()) {
 						if(Sp12	)	spawns.tryCreateCreep(spawn, WORKER[2][M], 34, 1, 1);
 						if(Sp12	)	spawns.tryCreateCreep(spawn,         3015, 31, 1, 1); // Carier
-						if(Sp12	)	spawns.tryCreateCreep(spawn, WORKER[3][M], 44, 1, extra_upgrade?4:2);
+						if(Sp12	)	spawns.tryCreateCreep(spawn, WORKER[3][M], 44, 1, extra_upgrade?3:2);
 						if(Sp12	)	spawns.tryCreateCreep(spawn,         3216, 41, 1, 2); // Carier
-						if(Sp234)	spawns.tryCreateCreep(spawn, WORKER[3][H], 64, 1, 1);
+						if(Sp234)	spawns.tryCreateCreep(spawn, WORKER[3][H], 64, 1, extra_upgrade?3:1);
 						if(Sp234)	spawns.tryCreateCreep(spawn,         3015, 61, 1, 2); // Carier
             if(conditions.TO_SPAWN_ROOM_DEFENDERS('W28S35')) {
               if(Sp34) spawns.tryCreateCreep(spawn, ATTACKER,125, 1, 1);
