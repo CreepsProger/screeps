7601c8048ef6b71a202058dc67ffb67df64dcf54
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-04-06 08:40:04 +0300

    Update role.repairer.js

--- Introduce/role.repairer.js
+++ Introduce/role.repairer.js
@@ -61,7 +61,7 @@ var roleRepairer = {
 				const D2 = Game.flags['D1'];// dismanle
 				if(!target) {
 					var structures = cash.getStructuresToRepaire(creep.room).filter((s) => {
-							if(s.hitsMax - s.hits > s.hitsMax/(2+98*(!!creep.memory.target && s.id == creep.memory.target.id))) {
+							if(!!s && s.hitsMax - s.hits > s.hitsMax/(2+98*(!!creep.memory.target && s.id == creep.memory.target.id))) {
 								 if(!!D1 && D1.pos.roomName == my_room &&
  									D1.pos.getRangeTo(s) < 11-D1.color) {
  									return false;
