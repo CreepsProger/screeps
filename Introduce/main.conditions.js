d05af755a4b40c0e6dc435aa12e778963a7f7db8
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-04-09 05:48:29 +0300

    Update main.conditions.js

--- Introduce/main.conditions.js
+++ Introduce/main.conditions.js
@@ -34,6 +34,9 @@ const conditions = {
 																			const room = Game.rooms[roomName];
 																			if(!room) return false;
 																			return 	room.find(FIND_HOSTILE_CREEPS).length > 0;}
+	, TO_EXTRA_UPGRADE:			function(total_energy) {
+																			return Game.cpu.bucket > constants.CPU_BUCKET_TO_EXTRA_UPGRADE &&
+															total_energy > constants.TOTAL_ENERGY_TO_EXTRA_UPGRADE;}
 };
 
 
