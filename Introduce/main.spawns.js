f51f2cd661c70f00b93808269e7d73bd712f97b5
Creeps Proger <creepsproger@gmail.com>
2020-04-12 17:00:11 +0300

    100 and 110 up

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -254,17 +254,17 @@ var spawns = {
 							if(Sp3) spawns.tryCreateCreep(spawn, ATTACKER[M], 105, 1, 1);
             }
             else {
-							// if(Sp3) spawns.tryCreateCreep(spawn,      CLAIMER, 100, 1, 1);
-							// if(Sp3) spawns.tryCreateCreep(spawn, WORKER[4][H], 104, 1, 1);
-							// if(Sp3) spawns.tryCreateCreep(spawn, CARIER[4][H], 101, 1, 1);
+							if(Sp3) spawns.tryCreateCreep(spawn,      CLAIMER, 100, 1, 1);
+							if(Sp3) spawns.tryCreateCreep(spawn, WORKER[4][H], 104, 1, 1);
+							if(Sp3) spawns.tryCreateCreep(spawn, CARIER[4][H], 101, 1, 1);
             }
 						if(conditions.TO_SPAWN_ROOM_DEFENDERS('W28S34')) {
 							if(Sp34) spawns.tryCreateCreep(spawn, ATTACKER[M], 115, 1, 1);
 						}
 						else {
-							// if(Sp34) spawns.tryCreateCreep(spawn,      CLAIMER, 110, 1, 1);
-							// if(Sp34) spawns.tryCreateCreep(spawn, WORKER[4][H], 114, 1, 1);
-							// if(Sp34) spawns.tryCreateCreep(spawn, CARIER[4][H], 111, 1, 1);
+							if(Sp34) spawns.tryCreateCreep(spawn,      CLAIMER, 110, 1, 1);
+							if(Sp34) spawns.tryCreateCreep(spawn, WORKER[4][H], 114, 1, 1);
+							if(Sp34) spawns.tryCreateCreep(spawn, CARIER[4][H], 111, 1, 1);
 						}
             if(Sp34) spawns.tryCreateCreep(spawn, WORKER[7][H], 124, 1, 2+X+X+X);
             if(Sp4 ) spawns.tryCreateCreep(spawn, CARIER[6][H], 121, 1, 1);
