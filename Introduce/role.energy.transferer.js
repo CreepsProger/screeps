cf3f22578765966e32f00306bb895e21d8c84d1f
Creeps Proger <creepsproger@gmail.com>
2020-04-12 14:06:09 +0300

    target.id

--- Introduce/role.energy.transferer.js
+++ Introduce/role.energy.transferer.js
@@ -373,7 +373,7 @@ var roleEnergyTransferer = {
 						console.log( '💡⚠️', Math.trunc(Game.time/10000), Game.time%10000
 												, creep.name
 												, 'transfering energy to:'
-												, target.name?target.name:target.structureType
+												, target.name?target.name:target.structureType, target.id
 												, 'with err:'
 												, err);
 					}
