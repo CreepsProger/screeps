d090d26368e16dd7e2e4d5fea7083398067012af
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-04-13 06:49:15 +0300

    Update main.spawns.js

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -249,8 +249,8 @@ var spawns = {
               if(Sp23)	spawns.tryCreateCreep(spawn, WORKER[7][H], 54, 1, 1);
               if(Sp23)	spawns.tryCreateCreep(spawn, CARIER[6][H], 51, 1, 1);
             }
-            if(Sp34) spawns.tryCreateCreep(spawn, WORKER[7][H], 124, 1, 1+X+X+X);
-            if(Sp4 ) spawns.tryCreateCreep(spawn, CARIER[6][H], 121, 1, 2);
+            if(Sp34) spawns.tryCreateCreep(spawn, WORKER[7][H], 124, 1, 2+X+X+X);
+            if(Sp4 ) spawns.tryCreateCreep(spawn, CARIER[6][H], 121, 1, 1);
 					}
 
 					if(conditions.TO_SPAWN_CLAIMING_ROOMS() && !X) {
