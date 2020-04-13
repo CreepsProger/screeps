c7f96cda4a35aedc13b39358a65ac133206b8674
Creeps Proger <creepsproger@gmail.com>
2020-04-13 12:30:44 +0300

    141, 1, 1);

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -287,7 +287,7 @@ var spawns = {
 						else {
               // if(Sp4) spawns.tryCreateCreep(spawn,      CLAIMER, 140, 1, 1);
               if(Sp4) spawns.tryCreateCreep(spawn, WORKER[4][M], 144, 1, 1);
-              // if(Sp4) spawns.tryCreateCreep(spawn, CARIER[6][H], 141, 1, 3);
+              if(Sp4) spawns.tryCreateCreep(spawn, CARIER[6][H], 141, 1, 1);
               if(conditions.TO_SPAWN_ROOM_DEFENDERS('W28S37')) {
                 if(Sp4) spawns.tryCreateCreep(spawn, ATTACKER[L], 155, 1, 1);
                 if(Sp4) spawns.tryCreateCreep(spawn,   HEALER, 157, 1, 1);
@@ -303,7 +303,7 @@ var spawns = {
 								else {
 									// if(Sp4) spawns.tryCreateCreep(spawn,      CLAIMER, 160, 1, 1);
 									if(Sp4) spawns.tryCreateCreep(spawn, WORKER[6][H], 164, 1, 1);
-									if(Sp4) spawns.tryCreateCreep(spawn, CARIER[6][H], 161, 1, 8);
+									if(Sp4) spawns.tryCreateCreep(spawn, CARIER[6][H], 161, 1, 6);
 								}
               }
 						}
