b82102d76fbcb6e8340c350c37877c751857865a
Creeps Proger <creepsproger@gmail.com>
2020-04-17 23:19:12 +0300

    CARIER[7][M], 161, 1, 1);

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -315,10 +315,10 @@ var spawns = {
 								}
 								else {
                   if(conditions.TO_SPAWN_ROOM_CLAIMER('W29S37')) {
-                    if(Sp4) spawns.tryCreateCreep(spawn,     CLAIMER[M], 160, 1, 1);
+                    if(Sp4) spawns.tryCreateCreep(spawn, CLAIMER[M], 160, 1, 1);
                   }
-									if(Sp4) spawns.tryCreateCreep(spawn,     WORKER[7][H], 164, 1, 1);
-									if(Sp4) spawns.tryCreateCreep(spawn, CARIER[6+X][H-X], 161, 1, 1);
+									if(Sp4) spawns.tryCreateCreep(spawn, WORKER[7][H], 164, 1, 1);
+									if(Sp4) spawns.tryCreateCreep(spawn, CARIER[7][M], 161, 1, 1);
 								}
               }
 						}
