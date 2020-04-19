852c0024b77c0ba6f672bbd660a3b80b7cee6bb9
Creeps Proger <creepsproger@gmail.com>
2020-04-19 19:32:35 +0300

    !!p.store && !!c.store &&

--- Introduce/main.terminals.js
+++ Introduce/main.terminals.js
@@ -13,7 +13,8 @@ var terminals = {
 
 		var all = cash.getAllMyTerminals();
 		var from = all.reduce((p,c) => {
-			return !!p && !!c && p.store.getUsedCapacity(RESOURCE_ENERGY) + !!p.room.storage?p.room.storage.store.getUsedCapacity(RESOURCE_ENERGY):0
+			return !!p && !!c && !!p.store && !!c.store &&
+							p.store.getUsedCapacity(RESOURCE_ENERGY) + !!p.room.storage?p.room.storage.store.getUsedCapacity(RESOURCE_ENERGY):0
 			 				> c.store.getUsedCapacity(RESOURCE_ENERGY) + !!c.room.storage?c.room.storage.store.getUsedCapacity(RESOURCE_ENERGY):0
 			 				? p:c;
 					});
