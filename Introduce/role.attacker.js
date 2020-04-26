28a95b28227af53837d1f6e9872af240c412fffc
Creeps Proger <creepsproger@gmail.com>
2020-04-26 22:44:01 +0300

    target

--- Introduce/role.attacker.js
+++ Introduce/role.attacker.js
@@ -220,7 +220,7 @@ var role = {
     		if(!target && this_room != my_room && creep.hitsMax == creep.hits) {
     			const exitDir = Game.map.findExit(this_room , my_path_room);
     			target = creep.pos.findClosestByPath(exitDir);
-    			console.log('🔜⚡', creep, 'exit:', this_room, 'to', my_room);
+    			console.log('🔜⚡', creep, 'exit:', this_room, 'to', my_room, 'target', target);
     		}
 
 				const DP1 = Game.flags['DP1'];
