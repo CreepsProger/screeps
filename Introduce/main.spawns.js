e72c67ae59778a36b777a85198081c089c99c782
Creeps Proger <creepsproger@gmail.com>
2020-04-27 18:55:40 +0300

    WORKER[7][H],174, 2, 2);

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -287,7 +287,7 @@ var spawns = {
 							if(Sp35) spawns.tryCreateCreep(spawn,   HEALER[7][M], 177, 1, 1);
 						}
 						else {
-              if(Sp35) spawns.tryCreateCreep(spawn, WORKER[7][H],174, 3, 3);
+              if(Sp35) spawns.tryCreateCreep(spawn, WORKER[7][H],174, 2, 2);
 							if(Sp5) spawns.tryCreateCreep(spawn, CARIER[5][H],171, 1, 1);
 						}
 					}
