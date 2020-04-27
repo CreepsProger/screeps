3a1c76b0198af366d9841cae138d2984a710709f
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-04-27 14:52:17 +0300

    Update main.spawns.js

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -401,10 +401,10 @@ var spawns = {
 					}
 					else {
 						if(Sp1) spawns.tryCreateCreep(spawn, WORKER[7][H], 194, 1, 1);
-						if(Sp1) spawns.tryCreateCreep(spawn, CARIER[7][H], 191, 2, 2);
+						if(Sp1) spawns.tryCreateCreep(spawn, CARIER[7][M], 191, 2, 2);
 					}
-					if(Sp1) spawns.tryCreateCreep(spawn,   WORKER[7][H], 204, 1, 1);
-					if(Sp1) spawns.tryCreateCreep(spawn,   CARIER[7][H], 201, 1, 1);
+					if(Sp1) spawns.tryCreateCreep(spawn,   WORKER[7][H], 204, 2, 2);
+					if(Sp1) spawns.tryCreateCreep(spawn,   CARIER[7][M], 201, 1, 1);
 				}
 
 				if(conditions.TO_SPAWN_TO_ATTACK()) {
