c5da669fd78abc6edd5eb80c7c7e76f1a616106c
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-04-27 09:36:49 +0300

    Update role.attacker.js

--- Introduce/role.attacker.js
+++ Introduce/role.attacker.js
@@ -279,7 +279,7 @@ var role = {
 							return (left < right)? p:c;
 						});
 						if(!!lair) {
-							console.log('lair', 'this_room:', this_room, 'lair:', left, JSON.stringify(lair));
+							console.log('lair', 'this_room:', this_room, 'lair:', JSON.stringify(lair));
 							target = lair;
 						}
 					}
