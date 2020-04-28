08367a5a6d2cf2a3faca28bfc07eaf024af024ff
Creeps Proger <creepsproger@gmail.com>
2020-04-29 00:24:24 +0300

    +

--- Introduce/role.claimer.js
+++ Introduce/role.claimer.js
@@ -62,13 +62,13 @@ var role = {
 	test_weight: 300,
 
 	run: function(creep) {
+		if(creep.memory.weight == role.test_weight) {
+			console.log(creep, role.name, JSON.stringify(creep.memory[role.name]));
+		}
 		role.init(creep);
 		role.checkOff(creep);
 		role.checkOn(creep);
 
-		if(creep.memory.weight == role.test_weight) {
-			console.log(creep, role.name, JSON.stringify(creep.memory[role.name]));
-		}
 
 		if(creep.memory[role.name].on) {
 
