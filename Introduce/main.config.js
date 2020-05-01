d050e9fc7454846216c83f94f5ea404e9a836cb7
Creeps Proger <creepsproger@gmail.com>
2020-05-01 11:16:14 +0300

    !target.pos

--- Introduce/main.config.js
+++ Introduce/main.config.js
@@ -73,7 +73,8 @@ var config = {
 		}
 
 		console.log(creep, target, JSON.stringify({creep:creep, target:target}));
-		if(creep.room.name != target.pos.roomName) {
+		if((!!target.pos && creep.room.name != target.pos.roomName) ||
+		 		(!target.pos && creep.room.name != target.roomName) {
 			const my_path_room = Memory.config.main_path[creep.room.name];
 			const exit = creep.room.findExitTo(my_path_room);
 			target = creep.pos.findClosestByPath(exit);
