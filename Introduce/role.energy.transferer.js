2cd6774bc2eca46394455bf8137c1e3e7a7af6f8
Creeps Proger <creepsproger@gmail.com>
2020-05-01 09:19:13 +0300

    my_shard

--- Introduce/role.energy.transferer.js
+++ Introduce/role.energy.transferer.js
@@ -51,7 +51,7 @@ var roleEnergyTransferer = {
 			const this_room_config = my_shard_config[this_room];
 			const my_room_config = my_shard_config[my_room];
 			console.log( '🚸', Math.trunc(Game.time/10000), Game.time%10000, Game.shard.name
-									, 'link:', JSON.stringify({this_room:this_room, this_room_config:this_room_config})
+									, 'link:', JSON.stringify({my_shard:my_shard,this_room:this_room, this_room_config:this_room_config})
 									);
 
 			// const this_room_sources_is_empty = !creep.pos.findClosestByRange(FIND_SOURCES, { no use because: storage->containers->storage
