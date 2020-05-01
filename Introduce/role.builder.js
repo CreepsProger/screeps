8b0a581a3a23be45f03b0f046cd5ba24ae5f5210
Creeps Proger <creepsproger@gmail.com>
2020-05-01 12:59:58 +0300

    +

--- Introduce/role.builder.js
+++ Introduce/role.builder.js
@@ -49,7 +49,7 @@ var roleBuilder = {
 				//console.log(JSON.stringify(Game.rooms['W29S35'].controller));
 				const UU = !!Game.flags['UU'] && Game.flags['UU'].pos.roomName == my_room;
 
-				if(!target && (this_room_sources_are_empty || this_room_is_being_claiming || UU || B || BB || true )) {
+				if(!target && (this_room_sources_are_empty || this_room_is_being_claiming || UU || B || BB )) {
 					target = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES)
 				}
 
