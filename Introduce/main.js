927391e510546aca41c40c5758a55443cbc75cad
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-05-02 06:43:14 +0300

    Update main.js

--- Introduce/main.js
+++ Introduce/main.js
@@ -21,11 +21,11 @@ module.exports.loop = function () {
 																 , "🛒":Game.cpu.bucket
 																 , "🧀": Game.cpu.bucket - Memory.cpu_prev_bucket})
 								);
-			// const err = Game.cpu.setShardLimits({shard0:50,shard1:5,shard2:5,shard3:20});
-			// console.log( '📙⏳', Math.trunc(Game.time/10000), Game.time%10000, Game.shard.name
-			// 						, '📟 setShardLimits err:'
-			// 						, err
-			// 						);
+			const err = Game.cpu.setShardLimits({shard0:50,shard1:5,shard2:5,shard3:20});
+			console.log( '📙⏳', Math.trunc(Game.time/10000), Game.time%10000, Game.shard.name
+			 						, '📟 setShardLimits err:'
+			 						, err
+			 						);
 		}
 
 		config.run();
