f00a4bb247a185d8c28500ab5f007a8bfe1bfce8
Creeps Proger <creepsproger@gmail.com>
2020-05-02 23:41:06 +0300

    fix null

--- Introduce/main.js
+++ Introduce/main.js
@@ -76,13 +76,14 @@ module.exports.loop = function () {
 				if(n >= 0 && n < 10)
 					weight = weight*10+n;
 				const	total_sum_pst = Object.keys(Memory.CreepsIdleTicksByWeight).reduce((p,w) => p +
-														Object.keys(Memory.CreepsIdleTicksByWeight[w]).reduce((pp,c) => pp +
+															Object.keys(Memory.CreepsIdleTicksByWeight[w]).reduce((pp,c) => pp +
 																				(!Memory.CreepsIdleTicksByWeight[w][c].pc?1:Memory.CreepsIdleTicksByWeight[w][c].pc),0),0);
 				const	total_cnt_pst = Object.keys(Memory.CreepsIdleTicksByWeight).reduce((p,w) => p +
-														Object.keys(Memory.CreepsIdleTicksByWeight[w]).reduce((pp,c) => pp +
+															Object.keys(Memory.CreepsIdleTicksByWeight[w]).reduce((pp,c) => pp +
 																				!!Memory.CreepsIdleTicksByWeight[w][c].w,0),0);
 				const	total_avg_pst = Math.round(total_sum_pst/total_cnt_pst);
-				const sum_pst = Object.keys(Memory.CreepsIdleTicksByWeight[weight]).reduce((p,c) => p +
+				const sum_pst = (!Memory.CreepsIdleTicksByWeight[weight])?1:
+															Object.keys(Memory.CreepsIdleTicksByWeight[weight]).reduce((p,c) => p +
 																(!Memory.CreepsIdleTicksByWeight[weight][c].pc?1:Memory.CreepsIdleTicksByWeight[weight][c].pc),0);
 
 				console.log( '✒️', Math.trunc(Game.time/10000), Game.time%10000
