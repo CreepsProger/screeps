4cd9acbda446ca1b0e077be3feae484aded4fcc8
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-05-03 08:46:32 +0300

    Update main.towers.js

--- Introduce/main.towers.js
+++ Introduce/main.towers.js
@@ -170,15 +170,11 @@ var towers = {
 							 return hostile.pos.x%48 > 1 || hostile.pos.y%48 > 1;
 						 }
 					 });
-				console.log('1 towers.flags.NAT:'
-					, JSON.stringify({NAT:NAT, target:target}));
 				if(!!target) {
 					tower.attack(target);
 					delete towers.sleep[i];
 				}
 			}
-			console.log('2 towers.flags.NAT:'
-					, JSON.stringify({NAT:NAT, target:target}));
 
 			if(!target) {
 			 	target = tower.pos.findClosestByRange(FIND_MY_CREEPS, {
@@ -187,7 +183,7 @@ var towers = {
 				 	}
 			 	});
 
-			 	if(target) {
+			 	if(!!target) {
 				 	tower.heal(target);
 				 	delete towers.sleep[i];
 			 	}
@@ -205,7 +201,7 @@ var towers = {
 				 	target = targets[0];
 			 	}
 
-			 	if(target) {
+			 	if(!!target) {
 				 	tower.heal(target);
 				 	target.memory.healer = tower.id;
 				 	target.memory.heal_time = Game.time;
@@ -246,20 +242,6 @@ var towers = {
 							(2+98*(!!towers.prev_target[i] && structure.id == towers.prev_target[i])) ;
  					}
  				});
-
-				// var structures = cash.getStructures(tower.room).filter((structure) => {
-				// 	 if(structure.structureType == STRUCTURE_WALL) {
-				// 		 return structure.hits < 32000;// 8000 E = 10 * 8000 / 800 = 100
-				// 	 }
-				// 	 if(structure.structureType == STRUCTURE_RAMPART) {
-				// 		 return structure.hits < 160000;// 8000 E = 10 * 8000 / 800 = 100
-				// 	 }
-				// 	 return structure.hitsMax - structure.hits > 2400; //towers.length*800;
-				// });
-				//
-				// if(structures.length > 0) {
- 				// 	target = structures.reduce((p,c) => tower.pos.getRangeTo(p) < tower.pos.getRangeTo(c)? p:c);
- 				// }
 
  				if(!!target && OK == tower.repair(target)) {
 					towers.prev_target[i] = target.id;
