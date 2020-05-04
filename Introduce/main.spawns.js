2cf1661a184bcbeff067b4bc54ee26ac286931ee
Creeps Proger <creepsproger@gmail.com>
2020-05-04 19:35:49 +0300

    44* after 45*

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -308,16 +308,6 @@ var spawns = {
 						if(Sp1)	spawns.tryCreateCreep(spawn, WORKER[4][M], 434, 1, 1);
 						if(Sp1)	spawns.tryCreateCreep(spawn, CARIER[4][L], 431, 2, 2);
 					}
-					if(conditions.TO_SPAWN_ROOM_DEFENDERS('W56S52')) {
-						//if(Sp1)	spawns.tryCreateCreep(spawn, ATTACKER[4][M], 445, 1, 1);
-					}
-					else {
-						if(conditions.TO_SPAWN_ROOM_CLAIMER('W56S52')) {
-							if(Sp1)	spawns.tryCreateCreep(spawn, CLAIMER[4][H], 440, 1, 1);
-						}
-						if(Sp1)	spawns.tryCreateCreep(spawn, CARIER[4][L], 441, 1, 1);
-						if(Sp1)	spawns.tryCreateCreep(spawn, WORKER[4][M], 444, 1, 1);
-					}
 					if(conditions.TO_SPAWN_ROOM_DEFENDERS('W56S51')) {
 						if(Sp1)	spawns.tryCreateCreep(spawn, ATTACKER[4][M], 455, 1, 1);
 					}
@@ -327,6 +317,16 @@ var spawns = {
 						}
 						if(Sp1)	spawns.tryCreateCreep(spawn, WORKER[4][M], 454, 1, 1);
 						if(Sp1)	spawns.tryCreateCreep(spawn, CARIER[4][L], 451, 2, 2);
+					}
+          if(conditions.TO_SPAWN_ROOM_DEFENDERS('W56S52')) {
+						//if(Sp1)	spawns.tryCreateCreep(spawn, ATTACKER[4][M], 445, 1, 1);
+					}
+					else {
+						if(conditions.TO_SPAWN_ROOM_CLAIMER('W56S52')) {
+							if(Sp1)	spawns.tryCreateCreep(spawn, CLAIMER[4][H], 440, 1, 1);
+						}
+						if(Sp1)	spawns.tryCreateCreep(spawn, CARIER[4][L], 441, 1, 1);
+						if(Sp1)	spawns.tryCreateCreep(spawn, WORKER[4][M], 444, 1, 1);
 					}
 				}
 
