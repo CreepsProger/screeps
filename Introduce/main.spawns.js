880368484eeda81b4735acf87ec2099c99190cd8
Creeps Proger <creepsproger@gmail.com>
2020-05-06 01:05:24 +0300

    CARIER[7][M], 201, 3, 3);

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -506,7 +506,7 @@ var spawns = {
 						// 	if(Sp12) spawns.tryCreateCreep(spawn, CARIER[7][M], 191, 3, 3);
 						// }
 						if(Sp1) spawns.tryCreateCreep(spawn,   WORKER[7][H], 204, 2, 2);
-						if(Sp1) spawns.tryCreateCreep(spawn,   CARIER[7][M], 201, 2, 2);
+						if(Sp1) spawns.tryCreateCreep(spawn,   CARIER[7][M], 201, 3, 3);
 					}
 
 					if(conditions.TO_SPAWN_TO_ATTACK()) {
