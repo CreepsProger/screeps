9ef20b5a63f80ef0a4e7848d3bf0dab9a4baf2a0
Creeps Proger <creepsproger@gmail.com>
2020-05-05 13:30:55 +0300

    -404

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -385,7 +385,7 @@ var spawns = {
 								// if(Sp5) spawns.tryCreateCreep(spawn,   CLAIMER[7][L], 400, 1, 1);
 								// if(Sp5) spawns.tryCreateCreep(spawn, ATTACKER[7][H], 335, 1, 1);
 								// if(Sp5) spawns.tryCreateCreep(spawn, ATTACKER[7][S+1], 336, 1, 1);
-								if(Sp5) spawns.tryCreateCreep(spawn,   WORKER[7][H], 404, 1, 1);
+								// if(Sp5) spawns.tryCreateCreep(spawn,   WORKER[7][H], 404, 1, 1);
 							}
 						}
 
