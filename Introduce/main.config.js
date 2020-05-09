ea1bb33641e615d5d1c7cc3542ac605109c1c7c3
Creeps Proger <creepsproger@gmail.com>
2020-05-10 00:21:55 +0300

    Default

--- Introduce/main.config.js
+++ Introduce/main.config.js
@@ -7,7 +7,7 @@ var git = '$Format:%H$';
 
 var config = {
 
-	version: 239,
+	version: 240,
 
 	log_flags: ['MC','MCF ','M'],
 
@@ -123,7 +123,24 @@ var config = {
 			, shards:
 				{	shard0:
 					{	rooms:
-						{	W52S57:
+						{	Default:
+							{ containers: {weight: 1000}
+							, heal_room:
+								{ shard: 'shard0', room: 'W57S52'}
+							, path_rooms:
+								{ W57S52: 'W56S52', W57S52y:24, W56S52: 'W56S53', W56S53: 'W56S54'
+								, W56S54: 'W57S54', W57S54: 'W57S55', W57S55: 'W56S55'
+								, W56S55: 'W55S55', W55S55: 'W55S56', W55S56: 'W55S57'
+								, W55S57: 'W55S58', W55S58: 'W56S58', W56S58: 'W56S59'
+								, W56S59: 'W56S60', W56S60: 'W55S60', W55S60: 'W54S60'
+								, W54S60: 'W54S59', W54S59: 'W53S59', W53S59: 'W53S58'
+								, W53S58: 'W52S58', W52S58: 'W52S57', W52S57: 'W52S57'
+								}
+								, escape_path:
+								{ W56S54: 'W56S53', W56S53: 'W56S52', W56S52: 'W57S52'
+								}
+							},
+							W52S57:
 							{ containers: {weight: 1000}
 							, sites:
 								[ {x:37, y:39, type:STRUCTURE_CONTAINER}
