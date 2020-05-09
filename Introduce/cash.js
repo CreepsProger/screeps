227cc994e957b0374021f2f6320aac3ef3addb8d
Creeps Proger <creepsproger@gmail.com>
2020-05-09 18:00:24 +0300

    item != type+entry_id

--- Introduce/cash.js
+++ Introduce/cash.js
@@ -19,7 +19,7 @@ var cash = {
 											, JSON.stringify({cash:'try haveToReset', type:type, entry_id:entry_id, resetList:cash.resetList, shard:Game.shard}))
 		}
 		if(cash.resetList.find((item) => item == type+entry_id)) {
-			cash.resetList = cash.resetList.filter((item) => item == type+entry_id);
+			cash.resetList = cash.resetList.filter((item) => item != type+entry_id);
 			console.log('🎉', Math.trunc(Game.time/10000), Game.time%10000
 											, JSON.stringify({cash:'haveToReset', type:type, entry_id:entry_id, resetList:cash.resetList}))
 			return true;
