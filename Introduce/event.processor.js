258afd1cd564df2d9434b6e6396f21921544bdde
Creeps Proger <creepsproger@gmail.com>
2020-05-09 13:05:57 +0300

    }

--- Introduce/event.processor.js
+++ Introduce/event.processor.js
@@ -7,18 +7,18 @@ var event_processor = {
 
 	run: function() {
 
-	_.forEach(Game.rooms, room => {
-		const eventLog = room.getEventLog();
-		const buildEvents = _.filter(eventLog, {event:EVENT_BUILD});
-		buildEvents.forEach(event => {
-			const target = Game.getObjectById(event.data.targetId);
-			if(target && target.my) {
-				console.log('🎉', Math.trunc(Game.time/10000), Game.time%10000
-									 			, JSON.stringify({room:room.name, event:event, target:target}))
-			}
+		_.forEach(Game.rooms, room => {
+			const eventLog = room.getEventLog();
+			const buildEvents = _.filter(eventLog, {event:EVENT_BUILD});
+			buildEvents.forEach(event => {
+				const target = Game.getObjectById(event.data.targetId);
+				if(target && target.my) {
+					console.log('🎉', Math.trunc(Game.time/10000), Game.time%10000
+										 			, JSON.stringify({room:room.name, event:event, target:target}))
+				}
+			});
 		});
-	});
-
+	}
 };
 
 module.exports = event_processor;
