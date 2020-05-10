8434aa1fbb508eeb4c6a34f703bfa8ff2456eef6
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-05-10 22:59:55 +0300

    Update cash.js

--- Introduce/cash.js
+++ Introduce/cash.js
@@ -13,14 +13,14 @@ var cash = {
 										, JSON.stringify({cash:'onBuilt', type:type, roomName:roomName, resetList:cash.resetList}))
 	},
 
-	haveToReset: function(type, entry_id) {
+	haveToReset: function(type, entry_id) {/*
 		 if(Game.shard.name == 'shard0' && entry_id == 5752 &&
-		 		(/*	type == STRUCTURE_EXTENSION ||
-		 			type == STRUCTURE_CONTAINER ||*/
+		 		(	type == STRUCTURE_EXTENSION ||
+		 			type == STRUCTURE_CONTAINER ||
 		 			type == STRUCTURE_ROAD + '&' + STRUCTURE_CONTAINER)) {
 			 console.log('🎉', Math.trunc(Game.time/10000), Game.time%10000
 		 									, JSON.stringify({cash:'try haveToReset', type:type, entry_id:entry_id, resetList:cash.resetList, shard:Game.shard}))
-		}
+		}*/
 		if(!!cash.resetList.find((item) => item == type+entry_id)) {
 			cash.resetList = cash.resetList.filter((item) => item != type+entry_id);
 			console.log('🎉', Math.trunc(Game.time/10000), Game.time%10000
