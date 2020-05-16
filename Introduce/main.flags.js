3cf99338eb5879ddbe289e2482628eae5ac047b0
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-05-16 07:43:05 +0300

    Update main.flags.js

--- Introduce/main.flags.js
+++ Introduce/main.flags.js
@@ -34,7 +34,7 @@ var flags = {
 			 	,   MW: Game.flags['MW']		// multiplier to repair wall
 			 	,   MR: Game.flags['MR']		// multiplier to repair rampart
 				,   21: Game.flags['21']		// start spawning 21-st one more creep
-				,  -21: Game.flags['-21']		// start spawning 21-st one less creep
+				,(-21): Game.flags['-21']		// start spawning 21-st one less creep
 				,   11: Game.flags['11']		// start spawning 11-th one more creep
 				,   31: Game.flags['31']		// start spawning 31-st one more creep
 				,   41: Game.flags['41']		// start spawning 41-st one more creep
