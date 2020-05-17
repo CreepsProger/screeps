36ca70b6f3f2015f3be77dfcb117f4cee06de57f
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-05-17 11:18:51 +0300

    Update main.spawns.js

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -278,7 +278,7 @@ var spawns = {
   				else {
 						const m = 0+!!flags.flags['-5011'];
 						const p = 0+!!flags.flags['5011'];
-						if(Sp1) spawns.tryCreateCreep(spawn, WORKER[4][H], 5014, 5, 5);
+						if(Sp1) spawns.tryCreateCreep(spawn, WORKER[4][H], 5014, 4, 4);
   					if(Sp1) spawns.tryCreateCreep(spawn, CARIER[4][H], 5011, 2-m+p, 2-m+p);
   					//if(Sp1) spawns.tryCreateCreep(spawn,   WORKER[7][H], 404, 3, 3);
   				}
@@ -339,7 +339,7 @@ var spawns = {
 					else {
 						const g = 0+!!flags.flags['404'];
 						const f = 0+!!flags.flags['401'];
-						if(Sp1) spawns.tryCreateCreep(spawn, WORKER[6][H], 404, 4+g, 4+g);
+						if(Sp1) spawns.tryCreateCreep(spawn, WORKER[6][H], 404, 3+g, 3+g);
 						if(Sp1) spawns.tryCreateCreep(spawn, CARIER[4][H], 401, 2+f, 2+f);
 						//if(Sp1) spawns.tryCreateCreep(spawn,   WORKER[7][H], 404, 3, 3);
 					}
