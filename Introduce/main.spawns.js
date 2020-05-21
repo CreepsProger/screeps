596a4ed778568e99c7a4ecfa2e45dcfba5acac91
Creeps Proger <creepsproger@gmail.com>
2020-05-21 22:23:02 +0300

    5051, 3-m+p, 3-m+p);

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -327,7 +327,7 @@ var spawns = {
 						if(Sp2)	spawns.tryCreateCreep(spawn, WORKER[4][M], 5054, 2-n+r, 2-n+r);
 						const m = 0+!!flags.flags['-5051'];
 						const p = 0+!!flags.flags['5051'];
-						if(Sp2)	spawns.tryCreateCreep(spawn, CARIER[4][M], 5051, 2-m+p, 2-m+p);
+						if(Sp2)	spawns.tryCreateCreep(spawn, CARIER[4][M], 5051, 3-m+p, 3-m+p);
 					}
         }
 
