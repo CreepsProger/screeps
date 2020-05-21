27c24d183872289a4803fabe3ff1b26a34d0e624
Creeps Proger <creepsproger@gmail.com>
2020-05-21 22:57:57 +0300

    5024, 4, 4);

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -288,7 +288,7 @@ var spawns = {
 					else {
 						const m = 0+!!flags.flags['-5021'];
 						const p = 0+!!flags.flags['5021'];
-						if(Sp1)	spawns.tryCreateCreep(spawn, WORKER[5][H], 5024, 3, 3);
+						if(Sp12)	spawns.tryCreateCreep(spawn, WORKER[4][H], 5024, 4, 4);
 						if(Sp2)	spawns.tryCreateCreep(spawn, CARIER[4][H], 5021, 2-m+p, 2-m+p);
 					}
 					if(conditions.TO_SPAWN_ROOM_DEFENDERS('W29S28')) {
