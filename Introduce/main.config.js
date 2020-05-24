f49e43fe8424ecd2d72fa53adfca75247c1f61bf
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-05-24 08:55:06 +0300

    Update main.config.js

--- Introduce/main.config.js
+++ Introduce/main.config.js
@@ -7,7 +7,7 @@ var git = '$Format:%H$';
 
 var config = {
 
-	version: 269,
+	version: 270,
 
 	log_flags: ['MC','MCF ','M'],
 
@@ -718,7 +718,7 @@ var config = {
 							, heal_room:
 								{ shard: 'shard1', room: 'W29S29'}
 							, path_rooms:
-								{ W29S29: 'W28S29', W28S29: 'W27S29', W27S29: 'W26S29'
+								{ W29S29: 'W28S29', W28S29: 'W27S29', W27S29: 'W26S29', W27S29y:9
 								, W29S31: 'W30S31', W30S31: 'W30S30'
 								, W30S30: 'W29S30', W29S30: 'W29S29', W29S30x:25
 								}
