19b19b840d9fd92cd1dda5955cd0924f9d2b1240
Creeps Proger <creepsproger@gmail.com>
2020-05-25 16:35:01 +0300

    config.version: 278

--- Introduce/main.config.js
+++ Introduce/main.config.js
@@ -7,7 +7,7 @@ var git = '$Format:%H$';
 
 var config = {
 
-	version: 277,
+	version: 278,
 
 	log_flags: ['MC','MCF ','M'],
 
@@ -172,7 +172,7 @@ var config = {
 									, W25S35: 'W25S34', W25S34: 'W25S33', W25S33: 'W25S33'
 									, W59S52: 'W58S52', W58S52: 'W57S52', W56S52: 'W57S52'
 									, W57S53: 'W57S52', W57S53x:30
-									, W54S51: 'W55S51', W55S51: 'W56S51', W56S51: 'W57S51'
+									, W54S51: 'W55S51', W55S51: 'W56S51', W56S51: 'W57S51', W56S51y:7
 									, W59S51: 'W58S51', W58S51: 'W57S51'
 									, W29S29: 'W29S29', W28S29: 'W29S29', W29S28: 'W29S29', W28S28: 'W29S28'
 									, W25S29: 'W26S29', W26S29: 'W27S29', W27S29: 'W28S29'
@@ -550,7 +550,7 @@ var config = {
 									, W60S52: 'W59S52', W59S52: 'W58S52', W58S52: 'W57S52'
 									// , W57S52: 'W57S52'
 									, W50S60: 'shard1'
-								, W57S51: 'W56S51', W56S51: 'W56S52', W56S52: 'W57S52'
+								  , W57S51: 'W56S51', W56S51: 'W56S52', W56S52: 'W57S52'
 									}
 								}
 								, escape_path:
