24bef4597a6967a73598594b58127f9af7e48341
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-05-27 01:53:32 +0300

    Update role.noenergy.transferer.js

--- Introduce/role.noenergy.transferer.js
+++ Introduce/role.noenergy.transferer.js
@@ -47,8 +47,9 @@ var role = {
 
 	checkOn: function(creep) {
 		if(!creep.memory[role.name].on &&
-			 creep.store.getFreeCapacity() < creep.store.getCapacity() - creep.store.getUsedCapacity(RESOURCE_ENERGY) &&
-			 (creep.store.getUsedCapacity(RESOURCE_ENERGY) == 0 || creep.memory.rerun)) {
+			 (creep.store.getFreeCapacity() < creep.store.getCapacity() - creep.store.getUsedCapacity(RESOURCE_ENERGY) &&
+			  (creep.store.getUsedCapacity(RESOURCE_ENERGY) == 0 || creep.memory.rerun)) ||
+			 tasks.needToTransfer(creep) ) {
 			creep.memory[role.name].on = true;
 			config.setRoom(creep, constants.ROLE_ENERGY_HARVESTING);
 		}
