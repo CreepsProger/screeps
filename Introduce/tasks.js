ccab64b02c3fd9429e76c2ac4f2506106df781ed
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-05-27 03:41:05 +0300

    Update tasks.js

--- Introduce/tasks.js
+++ Introduce/tasks.js
@@ -6,18 +6,29 @@ const cash = require('cash');
 var tasks = {
 
   taskToFillBoostingLab: {
-		pos:{}, 
+		pos:{},
+		lab:{},
+		storage:{},
 		isTask:false,
 		harvestingBy: function(creep) {
-			return ERR_NOT_IN_RANGE;
+			if(!pos.inRangeTo(creep,1))
+				return ERR_NOT_IN_RANGE;
+			return creep.withdraw(taskToFillBoostingLab.storage, RESOURCE_CATALYZED_UTRIUM_ACID,100);
 		},
 		transferingBy: function(creep) {
-			return ERR_NOT_IN_RANGE;
+			if(!pos.inRangeTo(creep,1))
+				return ERR_NOT_IN_RANGE;
+			return creep.transfer(taskToFillBoostingLab.lab, RESOURCE_CATALYZED_UTRIUM_ACID);
 		}
 	},
 	isToFillBoostingLab: function(creep) {
 		if(Game.shard.name != 'shard1')
 			return undefined;
+		if(!creep.room.storage)
+			return undefined;
+		
+		if(tasks.taskToFillBoostingLab.isTask)
+			return tasks.taskToFillBoostingLab;
 		
 		const pos = new RoomPosition(11, 25, creep.room.name);
 		
@@ -25,22 +36,50 @@ var tasks = {
 		if(found.length == 0)
 			return undefined;
 		const lab = found.reduce((p,c) => p.structureType == STRUCTURE_LAB);
+		if(!lab)
+			return undefined;
 		if(true) {//✅ ☑️ ✔️ ✖️ ❌ ❎ 
 			console.log('✅', Math.trunc(Game.time/10000), Game.time%10000
 											, JSON.stringify({tasks:'isToFillBoostingLab', creep:creep.name, lab:lab}));
 		}
 		if(lab.store.getUsedCapacity(RESOURCE_CATALYZED_UTRIUM_ACID) == 100)
 			return undefined;
-    return undefined;
+		if(lab.store.getUsedCapacity(RESOURCE_CATALYZED_UTRIUM_ACID) +
+			 creep.store.getUsedCapacity(RESOURCE_CATALYZED_UTRIUM_ACID) +
+			 creep.room.storage.store.getUsedCapacity(RESOURCE_CATALYZED_UTRIUM_ACID) < 100)
+			return undefined;
+
+		tasks.taskToFillBoostingLab.isTask = true;
+		tasks.taskToFillBoostingLab.storage = creep.room.storage;
+		tasks.taskToFillBoostingLab.lab = lab;
+		tasks.taskToFillBoostingLab.pos = pos;
+    return tasks.taskToFillBoostingLab;
 	}, 
 	needToHarvest: function(creep) {
 		if(Game.shard.name != 'shard1')
 			return false;
+		if(!tasks.taskToFillBoostingLab.isTask)
+			return false;
+		
+		const lab = tasks.taskToFillBoostingLab.lab;
+		if(lab.store.getUsedCapacity(RESOURCE_CATALYZED_UTRIUM_ACID) +
+			 creep.store.getUsedCapacity(RESOURCE_CATALYZED_UTRIUM_ACID)  < 100) {
+			tasks.taskToFillBoostingLab.pos = creep.room.storage.pos;
+			return true;
+		}
+		
 		return false;
 	}, 
 	needToTransfer: function(creep) {
 		if(Game.shard.name != 'shard1')
 			return false;
+		if(!tasks.taskToFillBoostingLab.isTask)
+			return false;
+		if(creep.store.getUsedCapacity(RESOURCE_CATALYZED_UTRIUM_ACID) > 0) {
+			tasks.taskToFillBoostingLab.pos = tasks.taskToFillBoostingLab.lab.pos;
+			return true;
+		}
+
 		return false;
 	}
 };
