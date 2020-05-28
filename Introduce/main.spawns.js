c6c9df2107114042e70c1b78e86fe0493831d731
Creeps Proger <creepsproger@gmail.com>
2020-05-28 11:10:54 +0300

    5051, 1-m+p, 1-m+p); 5054, 3-n+r, 3-n+r);

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -339,10 +339,10 @@ var spawns = {
 						}
 						const n = 0+!!flags.flags['-5054'];
 						const r = 0+!!flags.flags['5054'];
-						if(Sp2)	spawns.tryCreateCreep(spawn, WORKER[6][M], 5054, 2-n+r, 2-n+r);
+						if(Sp2)	spawns.tryCreateCreep(spawn, WORKER[6][M], 5054, 3-n+r, 3-n+r);
 						const m = 0+!!flags.flags['-5051'];
 						const p = 0+!!flags.flags['5051'];
-						if(Sp2)	spawns.tryCreateCreep(spawn, CARIER[5][M], 5051, 2-m+p, 2-m+p);
+						if(Sp2)	spawns.tryCreateCreep(spawn, CARIER[5][M], 5051, 1-m+p, 1-m+p);
 					}/*
 					if(conditions.TO_SPAWN_ROOM_DEFENDERS('W26S29')) {
 						if(Sp2)	spawns.tryCreateCreep(spawn, ATTACKER[4][M], 5065, 1, 1);
