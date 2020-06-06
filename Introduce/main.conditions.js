a4f67951449ff45a6bfb0af4d8db697fe51d7d73
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-06-06 08:57:32 +0300

    Update main.conditions.js

--- Introduce/main.conditions.js
+++ Introduce/main.conditions.js
@@ -38,8 +38,11 @@ const conditions = {
 	, TO_SPAWN_ROOM_DEFENDERS:			function(roomName) {
 																			const room = Game.rooms[roomName];
 																			if(!room) return false;
-																			return room.find(FIND_HOSTILE_CREEPS).length > 0 ||
-																				room.find(FIND_HOSTILE_STRUCTURES, { filter: (hs) => !!hs.level && hs.level == 0} ).length > 0;}
+																			const to_spawn_rd = (room.find(FIND_HOSTILE_CREEPS).length > 0) ||
+																						(room.find(FIND_HOSTILE_STRUCTURES, { filter: (hs) => !!hs.level && hs.level == 0} ).length > 0);
+		                                  if(!to_spawn_rd)
+																				console.log('TO_SPAWN_ROOM_DEFENDERS('+ roomName +') =' , to_spawn_rd, JSON.stringify(room.find(FIND_HOSTILE_STRUCTURES)));
+																		  return to_spawn_rd;}
 	, TO_SPAWN_ROOM_EXTRA_DEFENDERS:function(roomName) {
 																			const room = Game.rooms[roomName];
 																			if(!room) return false;
