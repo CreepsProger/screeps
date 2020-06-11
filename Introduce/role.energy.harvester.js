084184f3c60f419649c2db94f9d5147f5bfec3e9
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-06-11 13:25:32 +0300

    Update role.energy.harvester.js

--- Introduce/role.energy.harvester.js
+++ Introduce/role.energy.harvester.js
@@ -388,6 +388,7 @@ var role = {
 			//cash.renewCreep(creep);
 
 			if(	creep.pos.findInRange(FIND_MY_SPAWNS, 1).length > 0 ||
+				  creep.pos.findInRange(FIND_MY_CREEPS, 1).length > 0 ||
 					(!!creep.room.storage && creep.pos.inRangeTo(creep.room.storage, 1)) ||
 					(!!creep.room.terminal && creep.pos.inRangeTo(creep.room.terminal, 1)) ) {
 				const random = Math.floor(Math.random()*8+Game.time)%8+1;
