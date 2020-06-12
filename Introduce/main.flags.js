5a30d563b11f4e4d081134437a9397750cd6dfac
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-06-12 08:46:33 +0300

    Update main.flags.js

--- Introduce/main.flags.js
+++ Introduce/main.flags.js
@@ -15,7 +15,8 @@ var flags = {
 		if(flags.time != Game.time) {
 			flags.time = Game.time;
 			flags.flags =
-				{    NW1: Game.flags['NW1']			// don't withdraw in range 11-NW1.color
+				{    NP1: Game.flags['NP1']			// don't pick up in range 11-NW1.color
+				,    NW1: Game.flags['NW1']			// don't withdraw in range 11-NW1.color 
 				,DSOURCE: Game.flags['DSOURCE']			// DISMATLING SOURCE
 				,     XU: Game.flags['XU']			// eXtraUpgrade
 				,      C: Game.flags['C']			// Claim the room
