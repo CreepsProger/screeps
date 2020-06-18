4121881483835a871d496ab672d7261544d820ea
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-06-18 09:53:08 +0300

    Update tasks.js

--- Introduce/tasks.js
+++ Introduce/tasks.js
@@ -406,7 +406,8 @@ var tasks = {
 		}
 		const task = tasks.getTask(creep);
 
-		if(task.isToBoostCreeps !== undefined &&
+		if(task !== undefined &&
+			 task.isToBoostCreeps !== undefined &&
 			 tasks.taskToBoostCreeps.needToRun(creep)) {
 			return tasks.taskToBoostCreeps.run(creep);
 		}
