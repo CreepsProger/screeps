c2c1c1e179572e1d143eddd02e82329d90874b45
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-06-20 21:12:54 +0300

    Update role.pickuper.js

--- Introduce/role.pickuper.js
+++ Introduce/role.pickuper.js
@@ -43,6 +43,8 @@ var rolePickuper = {
 				if(!target) {
 					var droppeds = creep.room.find(FIND_DROPPED_RESOURCES, {
 						filter: (dropped) => {
+							console.log('👊', Math.trunc(Game.time/10000), Game.time%10000
+											, JSON.stringify({creep:creep.name, dropped:dropped}));
 							return /*dropped.resourceType == RESOURCE_ENERGY &&*/
 							 			(!dropped.pos.findInRange(FIND_HOSTILE_CREEPS, 5).length > 0 ||
 										 (!!DP && DP.pos.roomName == creep.room.name) ||
