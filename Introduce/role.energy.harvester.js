f4f6395fea7a9ef21cc5122c98cba198ff1fb1a6
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-06-20 19:31:41 +0300

    Update role.energy.harvester.js

--- Introduce/role.energy.harvester.js
+++ Introduce/role.energy.harvester.js
@@ -90,8 +90,8 @@ var role = {
 			 (tools.getWeight(creep.name) < my_room_config.containers.weight || U) &&
 			 creep.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
 			var conts = cash.getContainers(creep.room).filter((cont) => !!cont && !!cont.store &&
-                   cont.store.getFreeCapacity() < cont.store.getCapacity() - cont.store.getUsedCapacity(RESOURCE_ENERGY));
-			 																								//cont.store.getUsedCapacity(RESOURCE_ENERGY) > 0);
+                   (cont.store.getFreeCapacity() < cont.store.getCapacity() - cont.store.getUsedCapacity(RESOURCE_ENERGY) ||
+			 																								cont.store.getUsedCapacity(RESOURCE_ENERGY) > 0);
 
 			if(conts.length > 0) {
 				// var cont = conts.reduce((p,c) => tools.checkTarget(executer,p.id) && creep.pos.getRangeTo(p) < creep.pos.getRangeTo(c)? p:c);
