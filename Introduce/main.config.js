b0f8ca0a178a86181f5988aaec183a7699185f62
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-06-24 19:21:31 +0300

    Update main.config.js

--- Introduce/main.config.js
+++ Introduce/main.config.js
@@ -7,7 +7,7 @@ var git = '$Format:%H$';
 
 var config = {
 
-	version: 332,
+	version: 333,
 
 	log_flags: ['MC','MCF ','M'],
 
@@ -923,6 +923,45 @@ var config = {
 								}
 								, escape_path:
 								{ W25S28: 'W26S28', W26S27: 'W26S28', W26S28: 'W26S29', W26S29: 'W27S29', W27S29: 'W28S29', W28S29: 'W29S29'
+								}
+							},
+							W25S27:
+							{ containers: {weight: 5193}
+							, sites:
+							 	[ {x:42, y:43, type:STRUCTURE_CONTAINER}
+							 	]
+							, energy_harvesting:
+								[ {name: '1', time: 0, min_weight: 5190, max_weight: 5199}
+								, {name: '2', time: 0, min_weight: 5190, max_weight: 5199}
+								, {name: '3', time: 0, min_weight: 5190, max_weight: 5199}
+								, {name: '4', time: 0, min_weight: 5190, max_weight: 5199}
+								, {name: '5', time: 0, min_weight: 5190, max_weight: 5199}
+								, {name: '6', time: 0, min_weight: 5190, max_weight: 5199}
+								]
+							, claiming:
+							 	[ {name: '1', time: 0, min_weight: 5190, max_weight: 5199}
+								, {name: '2', time: 0, min_weight: 5190, max_weight: 5199}
+								]
+							, attacker:
+								[ {name: '1', time: 0, min_weight: 5190, max_weight: 5199}
+								, {name: '2', time: 0, min_weight: 5190, max_weight: 5199}
+								, {name: '3', time: 0, min_weight: 5190, max_weight: 5199}
+								, {name: '4', time: 0, min_weight: 5190, max_weight: 5199}
+								]
+							, heal_room:
+								{ shard: 'shard1', room: 'W26S28'}
+							, path_rooms:
+								{ W29S29: 'W28S29', W28S29: 'W27S29', W28S29y:30
+								, W27S29: 'W26S29', W27S29y:9
+								, W26S29: 'W26S28', W26S27: 'W26S28', W26S28: 'W25S28', W26S28y:42
+								, W24S28: 'W25S28', W24S28y:16
+								, W26S28: 'W25S28'
+								, W25S28: 'W25S27'
+								, W29S31: 'W30S31', W30S31: 'W30S30'
+								, W30S30: 'W29S30', W29S30: 'W29S29', W29S30x:32
+								}
+								, escape_path:
+								{ W25S27: 'W25S28', W25S28: 'W26S28', W26S27: 'W26S28', W26S28: 'W26S29', W26S29: 'W27S29', W27S29: 'W28S29', W28S29: 'W29S29'
 								}
 							},
 							W26S28:
