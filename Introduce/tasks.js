a6393bf454701b37ad91b2212d90aa733e68ac10
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-06-26 04:02:46 +0300

    Update tasks.js

--- Introduce/tasks.js
+++ Introduce/tasks.js
@@ -1,5 +1,5 @@
 const constants = require('main.constants');
-const harvester = require('role.harvester');
+//const harvester = require('role.harvester');
 const config = require('main.config');
 const flags = require('main.flags');
 const tools = require('tools');
@@ -451,9 +451,17 @@ var tasks = {
 				}
 			}
 			else {
-				if(!creep.memory[constants.ROLE_ENERGY_HARVESTING]) {
-				return harvester.run(creep);
-			}
+				const role = {name:constants.ROLE_ENERGY_HARVESTING};
+				if(creep.memory[role.name] === undefined ||
+			 creep.memory[role.name].v === undefined ||
+			 creep.memory[role.name].v != config.version) {
+			creep.memory[role.name] = { v: config.version
+																, on: false
+																, room: creep.room.name
+																, shard: Game.shard.name
+																};
+		}
+				
 				const target = config.findPathToMyRoom(creep,constants.ROLE_ENERGY_HARVESTING);
 				return OK == tools.moveTo(creep, target);
 			}
