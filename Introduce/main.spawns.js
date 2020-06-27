e1f2bd42f2fd3bce6b5dd5baae66e657b11c268e
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-06-27 18:46:55 +0300

    Update main.spawns.js

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -597,7 +597,7 @@ var spawns = {
 						if(Sp2)	spawns.tryCreateCreep(spawn, WORKER[7][L], 504);
             if(Sp2)	spawns.tryCreateCreep(spawn, CARIER[7][L], 501);
 					}
-					if(Sp2 && (Game.time % 500 < 500) ) spawns.tryCreateCreep(spawn,   CARIER[2][L], 173);
+					if(Sp2 && (Game.time % 500 < 500) ) spawns.tryCreateCreep(spawn,   CARIER[7][H], 173);
 					if(Sp12 && (Game.time % 50000 < 500) ) spawns.tryCreateCreep(spawn, HEALER[7][L], 1000);
 				}
 
