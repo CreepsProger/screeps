eefdcb954c604ce0e5a9bb0978f8affccd55371e
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-06-27 13:09:10 +0300

    Update main.config.js

--- Introduce/main.config.js
+++ Introduce/main.config.js
@@ -7,7 +7,7 @@ var git = '$Format:%H$';
 
 var config = {
 
-	version: 341,
+	version: 342,
 
 	log_flags: ['MC','MCF ','M'],
 
@@ -1775,7 +1775,11 @@ var config = {
 								, shard2:
 									{ W30S30: 'shard3'}
 								, shard3:
-									{ W30S30: 'W30S31', W30S31: 'W30S32', W30S32: 'W29S32' }
+									{ W30S30: 'W30S31', W30S31: 'W30S32', W30S32: 'W29S32'
+									, W25S33: 'W26S33', W26S33: 'W27S33', W27S33: 'W28S33'
+									, W28S33: 'W28S32', W28S32: 'W29S32', W28S35: 'W28S34'
+									, W28S34: 'W28S33', W27S34: 'W28S34'
+									}
 							 	}
 							, escape_path:
 								{ W30S30: 'W30S31', W30S31: 'W30S32', W30S32: 'W29S32', W29S32: 'W30S32', W30S32: 'W30S33', W30S33: 'W29S33'
