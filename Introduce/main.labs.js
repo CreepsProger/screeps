12254404bb0179f742d0e2e9e1af75cc256fc730
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-07-02 13:56:11 +0300

    Update main.labs.js

--- Introduce/main.labs.js
+++ Introduce/main.labs.js
@@ -8,6 +8,8 @@ const labs = {
 
 	getLabsToInOut: function(roomName) {
 		const conf = config.getLabsConfig(roomName);
+		if(!conf)
+			return [];
 		console.log('⚗️', Math.trunc(Game.time/10000), Game.time%10000
                     , JSON.stringify( { labs:'getLabsToInOut', roomName:roomName, conf:conf})); 
     return  cash.getLabs(roomName)
