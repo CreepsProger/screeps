e0e9d35582ffc34509e1f73bdd1cb20047c78b96
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-07-02 04:24:52 +0300

    Create main.labs.js

---
+++ Introduce/main.labs.js
@@ -0,0 +1,25 @@
+const constants = require('main.constants');
+const tools = require('tools');
+const cash = require('cash');
+
+var labs = {
+
+	getLabsToInOut: function() {
+    return cash.getLabs(creep.room).filter((l) => !!l && !!l.my && !!l.store);
+  },
+
+  getLabsToRun: function() {
+    return cash.getLabs(creep.room).filter((l) => !!l && !!l.my && !!l.store); 
+  },
+  
+  run: function() { 
+    if(Game.time % constants.TICKS_TO_TERMINAL_SEND != 0)
+      return;
+      
+    const labs = labs.getLabsToRun();
+    console.log('⚗️🧪🔬🧬🧫', Math.trunc(Game.time/10000), Game.time%10000
+                            , JSON.stringify( { labs:'run', labs:labs}); 
+	 }
+};
+
+module.exports = labs
