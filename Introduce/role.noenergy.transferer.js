b854c858afb52fa3e81dcf51bcfc3ccaa9b37dab
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-07-04 11:06:06 +0300

    Update role.noenergy.transferer.js

--- Introduce/role.noenergy.transferer.js
+++ Introduce/role.noenergy.transferer.js
@@ -114,7 +114,7 @@ var role = {
 			if(!!creep.room.terminal && !!creep.room.terminal.my) {
 				const resources = Object.keys(creep.store).filter((k) => k != RESOURCE_ENERGY);
 				if(resources.length == 1 &&
-					 terminals.getAmountToSend(creep, resources[0]) > 0) {
+					 terminals.getStorageAmountAvgDiff(creep.room.terminal, resources[0], tools.nvl(creep.store[resources[0]],0)) > 0) {
 					const target = creep.room.terminal;
 					return target;
 				} 
