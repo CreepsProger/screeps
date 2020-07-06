64b77688bc8f8488f476c5695e56debe209e27d1
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-07-06 17:27:12 +0300

    Update tasks.js

--- Introduce/tasks.js
+++ Introduce/tasks.js
@@ -455,7 +455,7 @@ var tasks = {
 				console.log('💉', Math.trunc(Game.time/10000), Game.time%10000
 							, JSON.stringify( { tasks:'onRun.boost', creep:creep.name
 																, room:creep.room.name, resToBoost:resToBoost}));
-				const labsToBoost = labs.getLabsToInOut(creep.room.name).filter((lab) => lab.mineralType == resToBoost);
+				const labsToBoost = labs.getLabsToInOut(creep.room.name).filter((lab) => !!lab.mineralType && lab.mineralType == resToBoost);
 				if(labsToBoost.length > 0) { 
 					console.log('💉', Math.trunc(Game.time/10000), Game.time%10000
 								, JSON.stringify( { tasks:'onRun.boost', creep:creep.name
