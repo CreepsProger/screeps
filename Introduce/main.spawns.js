57adf40b2b4239433e08b09718a0a4c80c6e9942
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-07-09 13:29:36 +0300

    Update main.spawns.js

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -494,7 +494,7 @@ var spawns = {
 							if(Sp6)	spawns.tryCreateCreep(spawn, CLAIMER[5][H], 5220);
 						}
 						if(Sp6)	spawns.tryCreateCreep(spawn, WORKER[5][M], 5224);
-						if(Sp6)	spawns.tryCreateCreep(spawn, CARIER[5][L], 5221);
+						if(Sp6)	spawns.tryCreateCreep(spawn, CARIER[5][M], 5221);
 					}
 					if(conditions.TO_SPAWN_ROOM_DEFENDERS('W22S28')) {
 						if(Sp6)	spawns.tryCreateCreep(spawn, ATTACKER[5][M], 5245);
@@ -504,7 +504,7 @@ var spawns = {
 							if(Sp6)	spawns.tryCreateCreep(spawn, CLAIMER[5][H], 5240);
 						}
 						if(Sp6)	spawns.tryCreateCreep(spawn, WORKER[5][M], 5244);
-						if(Sp6)	spawns.tryCreateCreep(spawn, CARIER[5][L], 5241);
+						if(Sp6)	spawns.tryCreateCreep(spawn, CARIER[5][M], 5241);
 					}
         }
 
