8140a8c4e8697059c1394f314ef2f2162024696a
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-07-19 18:55:25 +0300

    Update main.terminals.js

--- Introduce/main.terminals.js
+++ Introduce/main.terminals.js
@@ -10,10 +10,10 @@ var terminals = {
 
 	getAmountToDeal: function(terminal,resource) {
 		const amountToDeal = tools.nvl(config.getAmountToDeal(terminal.pos.roomName,resource),0);
-		if(amountToDeal > 0) {
+		if(amountToDeal > 0) {/*
 			console.log( '🤝💲💠📜', Math.trunc(Game.time/10000), Game.time%10000
                     , JSON.stringify( { terminals:'getAmountToDeal', roomName:terminal.pos.roomName
-																			, resource:resource, amountToDeal:amountToDeal }));
+																			, resource:resource, amountToDeal:amountToDeal }));/*
 		}
 		return amountToDeal;
 	},
