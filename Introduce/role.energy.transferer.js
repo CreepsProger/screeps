ce727c6dd49e3510679bf81ce7ba1ef3fea8039b
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-07-25 11:08:06 +0300

    Update role.energy.transferer.js

--- Introduce/role.energy.transferer.js
+++ Introduce/role.energy.transferer.js
@@ -289,7 +289,7 @@ var roleEnergyTransferer = {
 			}
 			
 			if(!target && creep.memory.rerun) {
-				var nukers = cash.getNukers(creep.room).filter((n) =>
+				var nukers = cash.getNukers(creep.room.name).filter((n) =>
 						!!n && !!n.store && n.store.getFreeCapacity(RESOURCE_ENERGY) > 0);
 				if(nukers.length > 0) {
 					target = nukers.reduce((p,c) => !!p && !!c && creep.pos.getRangeTo(p) < creep.pos.getRangeTo(c)? p:c);
