186f8e0e2df33aecdce86afdfb7b4356bc20c042
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-07-25 18:10:20 +0300

    Update main.flags.js

--- Introduce/main.flags.js
+++ Introduce/main.flags.js
@@ -251,6 +251,8 @@ var flags = {
 				,      D: Game.flags['D']			// dismanle all
 				,     D1: Game.flags['D1']		// dismanle in range 11-D1.color
 			 	,     D2: Game.flags['D2']		// dismanle in range 11-D2.color
+				,    DR1: Game.flags['DR1']		// dismanle ramparts only in range 11-DR1.color
+			 	,    DR2: Game.flags['DR2']		// dismanle ramparts only in range 11-DR2.color
 				,     NB: Game.flags['NB']		// don't build
 			 	,    NB1: Game.flags['NB1']		// don't build
 			 	,    NB2: Game.flags['NB2']		// don't build
