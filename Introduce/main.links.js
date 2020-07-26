42b8c08731bb85fc8cac9f16da4b1d19dde5a268
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-07-26 06:47:34 +0300

    Update main.links.js

--- Introduce/main.links.js
+++ Introduce/main.links.js
@@ -112,6 +112,8 @@ var links = {
 		 // W21S28
      , { from: '5f06dc8407c460ca3d230a0a', to: '5f048e5511093c2c6f7062bc'}
 		 , { from: '5f06f2f3ae021357155fe904', to: '5f048e5511093c2c6f7062bc'}
+     , { from: '5f06dc8407c460ca3d230a0a', to: '5f1cfbc3a923e96f80b70649'}
+		 , { from: '5f06f2f3ae021357155fe904', to: '5f1cfbc3a923e96f80b70649'}
 	 ],
 
 	 getTargetLinkToTransferEnergy: function(creep, executer, role_run, link_weight) {
