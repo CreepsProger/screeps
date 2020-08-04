3afba61d64db86a760bb657a56d2b78c14895e81
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-08-04 05:58:42 +0300

    Update main.power.js

--- Introduce/main.power.js
+++ Introduce/main.power.js
@@ -7,7 +7,7 @@ const cash = require('cash');
 const power = {
 
 	getConfig: function(roomName) {
-		return config.getPowerConf(roomName);
+		return config.getPowerConfig(roomName);
 	},
 
 	run: function() {
@@ -15,21 +15,53 @@ const power = {
 		cash.getAllMyPowerSpawns()
 				.filter((s) => s.store.getUsedCapacity('power') > 0 &&
 												s.store.getUsedCapacity('energy') > 50)
-				.forEach(function(spawn,i) {
-					const err = spawn.processPower();
+				.forEach(function(powerSpawn,i) {
+					const err = powerSpawn.processPower();
 					if(err != OK) {
 						console.log('🔴🌀⚠️', Math.trunc(Game.time/10000), Game.time%10000
-												, JSON.stringify({main:'processPower', room:spawn.room.name, err:err, spawn:spawn}));
+												, JSON.stringify({main:'processPower', room:powerSpawn.room.name, err:err, powerSpawn:powerSpawn}));
 					}
 		});
 
-		Object.keys(Game.powerCreeps)
-					.forEach(function(name,i) {
-						const pc = Game.powerCreeps[name];
-						if(true) {
-							console.log('🔴👨‍🚒', Math.trunc(Game.time/10000), Game.time%10000
-												, JSON.stringify({main:'power', pc:pc}));
+		Object
+			.keys(Game.powerCreeps)
+			.forEach(function(name,i) {
+
+			const pc = Game.powerCreeps[name];
+			if(true) {
+				console.log('🔴👨‍🚒', Math.trunc(Game.time/10000), Game.time%10000
+										, JSON.stringify({main:'power', pc:pc}));
+			}
+			if(!pc.ticksToLive) {
+				cash.getAllMyPowerSpawns()
+					.forEach(function(powerSpawn,i) {
+					const roomName = powerSpawn.pos.roomName;
+					const conf = power.getConfig(roomName,name);
+					if(!!conf && conf.spawn) {
+						const err = pc.spawn(powerSpawn);
+						if(err != OK) {
+							console.log('🔴👨‍🚒⚠️', Math.trunc(Game.time/10000), Game.time%10000
+													, JSON.stringify( { main:'spawnPower', room:roomName
+																						, err:err, name:name, powerSpawn:powerSpawn}));
+						}
 					}
+				});
+			}
+			else {
+				const roomName = pc.pos.roomName;
+				const conf = power.getConfig(roomName,name);
+				if(!!conf && conf.factory) {
+					cash.getFactories(roomName)
+						.forEach(function(factory,i) {
+						const err = pc.usePower(factory);
+						if(err != OK) {
+							console.log('🔴👨‍🚒⚠️', Math.trunc(Game.time/10000), Game.time%10000
+													, JSON.stringify( { main:'usePower', room:roomName
+																						 , err:err, name:name, factory:factory}));
+						}
+					});
+				}
+			}
 		});
 	}
 };
