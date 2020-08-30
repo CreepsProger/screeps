7f35a4b84fae35171601170926b9d6d6a236c17f
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-08-30 09:59:59 +0300

    Update tools.js

--- Introduce/tools.js
+++ Introduce/tools.js
@@ -14,7 +14,7 @@ var tools = {
 	dontGetInWay: function(creep) {
 		if(creep.pos.findInRange(FIND_MY_SPAWNS, 1).length > 0 ||
 			 creep.pos.findInRange(FIND_MY_CREEPS, 1).length > 1 ||
-			 (!!creep.room.storage && creep.pos.inRangeTo(creep.room.storage, 1)) ||
+			 //(!!creep.room.storage && creep.pos.inRangeTo(creep.room.storage, 1)) ||
 			 (!!creep.room.terminal && creep.pos.inRangeTo(creep.room.terminal, 1)) ) {
 				const random = Math.floor(Math.random()*8+Game.time)%8+1;
 				creep.move(random); // TOP:1 ,..., TOP_LEFT:8
