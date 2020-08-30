5f712cb2b5b58209c369dc51843c338c71498d2c
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-08-30 23:30:59 +0300

    Update main.spawns.js

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -527,7 +527,9 @@ var spawns = {
 						if(Sp7)	spawns.tryCreateCreep(spawn,   WORKER[8][M], 5195);
 					}
 					if(true) {
-						if(Sp7)	spawns.tryCreateCreep(spawn,   ATTACKER[8][L2], 5266);
+						if(Sp7)	spawns.tryCreateCreep(spawn,ATTACKER[8][L2], 5266);
+						if(Sp7)	spawns.tryCreateCreep(spawn,   WORKER[7][L], 5264);/*
+						if(Sp7)	spawns.tryCreateCreep(spawn,   CARIER[7][L], 5261);*/
 					}
 					/*
 					if(spawn.name == 'Spawn10') spawns.tryCreateCreep(spawn, ATTACKER[1][L], 5254);
