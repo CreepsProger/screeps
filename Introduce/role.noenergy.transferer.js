899446aed5ba7ea798bf0e1113a3db9d9f2933c0
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-09-05 12:07:45 +0300

    Update role.noenergy.transferer.js

--- Introduce/role.noenergy.transferer.js
+++ Introduce/role.noenergy.transferer.js
@@ -243,7 +243,7 @@ var role = {
 						console.log( '💦', Math.trunc(Game.time/10000), Game.time%10000
 												, creep.name
 												, 'transfering resources to:'
-												, target.name?target.name:target.structureType);
+												, target.name?target.name:target.structureType, target.target);
 					}
 				}
 				else {
