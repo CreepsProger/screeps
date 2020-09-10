de733d7a1b8d6561ca4de28a40b574a986743bb7
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-09-10 03:49:02 +0300

    Update role.harvester.js

--- Introduce/role.harvester.js
+++ Introduce/role.harvester.js
@@ -393,7 +393,7 @@ var role = {
 													.shift();
 			if(!!tower) {
 				tools.setTarget(creep,tower,tower.id,role.run);
-				if(tools.getWeight(creep.name) == 5151) {
+				if(tools.getWeight(creep.name) == 999999) {
 						console.log('🗼⚡', Math.trunc(Game.time/10000), Game.time%10000
 																, JSON.stringify( { creep:creep.name, roomName:creep.room.name
 																									, tower_id:tower.id}));
