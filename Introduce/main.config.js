85d483ba09d49332a4af001abce4d680c74d85c8
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-09-13 12:39:26 +0300

    Update main.config.js

--- Introduce/main.config.js
+++ Introduce/main.config.js
@@ -7,7 +7,7 @@ var git = '$Format:%H$';
 
 var config = {
 
-	version: 442,
+	version: 443,
 
 	log_flags: ['MC','MCF ','M'],
 
@@ -724,7 +724,7 @@ var config = {
 								{ W57S51: 'W56S51'
 								, W57S52: 'W56S52', W57S52y:45, W56S52: 'W56S52'
 								, W54S51: 'W55S51', W55S51: 'W56S51', W56S51: 'W56S52', W56S52: 'W56S53', W56S52x:25
-								, W55S53: 'W56S53'
+								, W55S53: 'W56S53', W56S54: 'W56S53'
 								}
 								, escape_path:
 								{ W56S53: 'W56S52', W56S52: 'W57S52'
