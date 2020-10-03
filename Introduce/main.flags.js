88d0c542819ace0dff93d4a6cfbe031904c72b4b
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-10-03 17:16:54 +0300

    Update main.flags.js

--- Introduce/main.flags.js
+++ Introduce/main.flags.js
@@ -267,6 +267,7 @@ var flags = {
 				,   CONT: Game.flags['CONT']		// use setTarget for a container in this room
 				,    NP1: Game.flags['NP1']			// don't pick up in range 11-NW1.color
 				,    NW1: Game.flags['NW1']			// don't withdraw in range 11-NW1.color 
+				,      W: Game.flags['W']			// withdraw by all
 				,DSOURCE: Game.flags['DSOURCE']	// DISMATLING SOURCE
 				,     XB: Game.flags['XB']			// eXtraBuild - 'B' in all rooms
 				,     XU: Game.flags['XU']			// eXtraUpgrade
