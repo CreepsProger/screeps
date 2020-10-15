38dc669a1d34c4efcfa9a913eda5b94b570ca775
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-10-15 07:14:11 +0300

    Update main.links.js

--- Introduce/main.links.js
+++ Introduce/main.links.js
@@ -96,6 +96,7 @@ var links = {
 		 , { from: '5f6d3aeab0011940ed7b2905', to: '5f734c376c236600d938bc4c'}
 		 // W54S53
 		 , { from: '5f8510578eb937f14fc91101', to: '5f850c61de14c3175ea48e1d'}
+		 , { from: '5f87c9b2ab8aedb1d194c7c4', to: '5f850c61de14c3175ea48e1d'}
      //shard1
 		 // W29S29
 		 , { from: '5ec2330dce2f77348cbbc2ae', to: '5ec1fb20a882200050a21624'}
