c8b0440fe912bd32f5f0d2e727d716e07a4c7746
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2020-10-26 02:52:21 +0300

    Update main.labs.js

--- Introduce/main.labs.js
+++ Introduce/main.labs.js
@@ -90,8 +90,8 @@ const labs = {
 		conf.subConfigN = N;
 		const storage = Game.rooms[roomName].storage;
 		const ret = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-				.map((n) =>  labs.getConfLabRes(conf,n))
-				.filter((res,i) => labs.getConfLabAgs(conf,i) == 0 && !!res && res != '-');/*
+				.map((n) =>  labs.getConfLabAgs(conf,n));/*
+				.filter((res,i) => labs.getConfLabAgs(conf,i) == 0 && !!res && res != '-')
 				.map((res) =>  tools.nvl(storage.store[res],0) )
 				.reduce((p,c) =>  Math.min(p,c), Infinity );*/
 		console.log('⚗️⚖️', Math.trunc(Game.time/10000), Game.time%10000
