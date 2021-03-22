e4be1e350674341d3cbbec856863d30e899d2874
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2021-03-22 22:00:11 +0300

    Update main.config.js

--- Introduce/main.config.js
+++ Introduce/main.config.js
@@ -7,7 +7,7 @@ var git = '$Format:%H$';
 
 var config = {
 
-	version: 559,
+	version: 560,
 
 	log_flags: ['MC','MCF ','M'],
 
@@ -1319,6 +1319,43 @@ var config = {
 								}
 								, escape_path:
 								{ W55S54: 'W55S53', W55S53: 'W56S53', W55S53y:27, W56S53: 'W56S52', W56S52: 'W57S52'
+								}
+							},
+							W53S54:
+							{ containers: {weight: 643}
+							, sites:
+								[ {x:20, y:27, type:STRUCTURE_CONTAINER}
+								]
+							, energy_harvesting:
+								[ {name: '1', time: 0, min_weight: 640, max_weight: 649}
+								, {name: '2', time: 0, min_weight: 640, max_weight: 649}
+								, {name: '3', time: 0, min_weight: 640, max_weight: 649}
+								, {name: '4', time: 0, min_weight: 640, max_weight: 649}
+								, {name: '5', time: 0, min_weight: 640, max_weight: 649}
+								, {name: '6', time: 0, min_weight: 640, max_weight: 649}
+								, {name: '7', time: 0, min_weight: 640, max_weight: 649}
+								, {name: '8', time: 0, min_weight: 640, max_weight: 649}
+								]
+							, attacker:
+								[ {name: '1', time: 0, min_weight: 640, max_weight: 649}
+								, {name: '2', time: 0, min_weight: 640, max_weight: 649}
+								, {name: '3', time: 0, min_weight: 640, max_weight: 649}
+								, {name: '4', time: 0, min_weight: 640, max_weight: 649}
+								, {name: '5', time: 0, min_weight: 640, max_weight: 649}
+								, {name: '6', time: 0, min_weight: 640, max_weight: 649}
+								]
+							, heal_room:
+								{ shard: 'shard0', room: 'W57S52'}
+							, path_rooms:
+								{ W57S51: 'W56S51'
+								, W57S52: 'W56S52', W57S52y:45, W56S52: 'W56S52'
+								, W54S51: 'W55S51', W55S51: 'W56S51', W56S51: 'W56S52', W56S52: 'W56S53', W56S52x:25
+								, W56S54: 'W56S53', W56S54y:27, W56S53: 'W55S53'
+								, W55S53: 'W54S53'
+								, W54S53: 'W54S54', W54S53x:38, W54S54: 'W53S54'
+								}
+								, escape_path:
+								{ W53S54: 'W54S54', W54S54: 'W54S53', W54S53: 'W55S53', W55S53: 'W56S53'
 								}
 							}
 							//, 
