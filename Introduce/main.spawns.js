db8a9ab4c90fa5035c127d80909f4e1ba321a9d2
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2021-03-24 00:07:29 +0300

    Update main.spawns.js

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -978,9 +978,7 @@ var spawns = {
 						if(Sp5)	spawns.tryCreateCreep(spawn, ATTACKER[5][M], 656);
 					}
 					else {
-						if(conditions.TO_SPAWN_ROOM_CLAIMER('W57S55')) {
-              if(Sp5)	spawns.tryCreateCreep(spawn, CLAIMER[7][H], 650);
-						}
+						if(Sp5)	spawns.tryCreateCreep(spawn, ATTACKER[5][M], 656);
 						if(Sp5)	spawns.tryCreateCreep(spawn, WORKER[7][M], 654);
             if(Sp5)	spawns.tryCreateCreep(spawn, CARIER[7][L], 651);
 					}
