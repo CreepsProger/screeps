d2083c33d0de769d9323c88ba161230b765fc9aa
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2021-03-23 23:21:59 +0300

    Update main.observer.js

--- Introduce/main.observer.js
+++ Introduce/main.observer.js
@@ -17,7 +17,7 @@ const observer = {
           const o_rooms = observer.getConfig(o.room.name);
           const o_room = o_rooms[Game.time%o_rooms.length];
 					const err = o.observeRoom(o_room);
-					/*if(err != OK)*/ {
+					if(err != OK) {
 						console.log('🔴🌀⚠️', Math.trunc(Game.time/10000), Game.time%10000
 												, JSON.stringify({main:'observeRoom', room:o.room.name, o_room:o_room, err:err, o_rooms:o_rooms, observer:o}));
 					}
