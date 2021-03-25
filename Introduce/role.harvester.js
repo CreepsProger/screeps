4e00185dabab6882d6d4e9972704f7b6b1a8b388
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2021-03-25 15:39:56 +0300

    Update role.harvester.js

--- Introduce/role.harvester.js
+++ Introduce/role.harvester.js
@@ -241,8 +241,8 @@ var role = {
 			}
 		}
 		
-		const roomST = flags.getFlag(my_room + '.ST')
-		const ST  = (!!flags.flags.ST && flags.flags.ST.pos.roomName == my_room) || roomST;
+		const roomST = flags.getFlag(my_room + '.ST');
+		const ST  = (!!flags.flags.ST && flags.flags.ST.pos.roomName == my_room) || !!roomST;
 		const st = role.getST(creep.name);
 
 		if(!creep.getActiveBodyparts(WORK) &&
