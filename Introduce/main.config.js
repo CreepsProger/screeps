b49b9b2cb99c0bd688d9bff8f243e771f09c34ef
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2021-03-27 10:17:54 +0300

    Update main.config.js

--- Introduce/main.config.js
+++ Introduce/main.config.js
@@ -7,7 +7,7 @@ var git = '$Format:%H$';
 
 var config = {
 
-	version: 579,
+	version: 580,
 
 	log_flags: ['MC','MCF ','M'],
 
@@ -1724,7 +1724,7 @@ var config = {
 								{ shard: 'shard0', room: 'W57S55'}
 							, path_rooms:
 								{ W57S51: 'W56S51', W56S51: 'W56S52', W56S52: 'W57S52', W57S52: 'W57S53'
-								, W57S53: 'W58S53', W58S53: 'W58S54', W58S54: 'W58S55'
+								, W57S53: 'W58S53', W58S53: 'W58S54', W58S54: 'W58S55', W58S54x:35
 								, W59S53: 'W58S53'
 								, W54S51: 'W55S51', W55S51: 'W56S51'
 								, W54S53: 'W55S53', W55S53: 'W56S53', W56S53: 'W56S54'
