98c83bd551a638690e5d4479cb5e737e66845857
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2021-04-09 16:24:57 +0300

    Update main.config.js

--- Introduce/main.config.js
+++ Introduce/main.config.js
@@ -7,7 +7,7 @@ var git = '$Format:%H$';
 
 var config = {
 
-	version: 662,
+	version: 663,
 
 	log_flags: ['MC','MCF ','M'],
 
@@ -4025,8 +4025,8 @@ var config = {
 									}
 								, shard2:
 									{ shard1_W20S30: 'W20S30'
-									 //, W20S30: 'W19S30', W20S30y:43
-									 , W20S30: 'W20S31', W20S31: 'W19S31'
+									, W20S30: 'W19S30', W20S30y:43
+									//, W20S30: 'W20S31', W20S31: 'W19S31'
 									, W19S30: 'W19S31'
 									, W20S31: 'W19S31'
 									, W18S31: 'W19S31'
