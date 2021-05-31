53ed72fb29ecef674e1a7c2c6004ede2da49c563
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2021-05-31 17:19:05 +0300

    Update main.spawns.js

--- Introduce/main.spawns.js
+++ Introduce/main.spawns.js
@@ -1015,7 +1015,7 @@ var spawns = {
 					}
 					else {
 						if(Sp16 && !All)	spawns.tryCreateCreep(spawn, ATTACKER[5][M], 5589);
-						if(Sp16 && !All)	spawns.tryCreateCreep(spawn, WORKER[7][L], 5584);
+						if(Sp16 && !All)	spawns.tryCreateCreep(spawn, WORKER[7][M], 5584);
 						if(Sp16 && !All)	spawns.tryCreateCreep(spawn, CARIER[7][L], 5581);
 					}
 					if(conditions.TO_SPAWN_ROOM_DEFENDERS('W23S21')) {
