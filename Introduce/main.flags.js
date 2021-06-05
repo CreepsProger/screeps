36df13f4a6609b089e04a8ddc0f986b049c6fd97
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2021-06-05 13:19:40 +0300

    Update main.flags.js

--- Introduce/main.flags.js
+++ Introduce/main.flags.js
@@ -509,7 +509,7 @@ var flags = {
 	Limits: function(Limits) {
 
 		if(Game.shard.name == 'shard1' && Limits.color == 9) {
-			Game.cpu.setShardLimits({shard0:6,shard1:200,shard2:12,shard3:12});
+			Game.cpu.setShardLimits({shard0:5,shard1:198,shard2:15,shard3:12});
 		}
 		if(Game.shard.name == 'shard0' && Limits.color == 4) {
 			Game.cpu.setShardLimits({shard0:185,shard1:5,shard2:5,shard3:5});;
