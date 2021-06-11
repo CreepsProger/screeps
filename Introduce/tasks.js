d98c80bedb89e77527ed26fe9c0a70e6a7c7c26a
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2021-06-11 19:32:47 +0300

    Update tasks.js

--- Introduce/tasks.js
+++ Introduce/tasks.js
@@ -1170,7 +1170,7 @@ var tasks = {
 				var err = OK;
 				target = cash.getTowers(creep.room)
 											.filter((t) => !!t && !! t.my && !!t.store &&
-																			t.store.getFreeCapacity(RESOURCE_ENERGY) > 700 &&
+																			t.store.getFreeCapacity(RESOURCE_ENERGY) > constants.TOWER_NO_ENERGY_TO_FILL &&
 																			creep.pos.getRangeTo(t) < 7)
 											.shift();
 				if(!!target) {
