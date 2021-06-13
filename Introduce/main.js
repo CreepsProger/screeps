b9422bf805ad18131371f38e8bc9e1e7c4402805
CreepsProger <60812718+CreepsProger@users.noreply.github.com>
2021-06-13 10:25:00 +0300

    Update main.js

--- Introduce/main.js
+++ Introduce/main.js
@@ -21,7 +21,7 @@ const attacker = require('role.attacker');
 
 module.exports.loop = function () {
 	
-	if(!conditions.TO_SPAWN_MAIN_ROOMS() && Game.time % 3 > 0) {
+	if(!conditions.TO_SPAWN_MAIN_ROOMS() && Game.time % 5 > 0) {
 		console.log( '⏳', Math.trunc(Game.time/10000), Game.time%10000
 								, JSON.stringify({ '🛐':Game.cpu.limit, "🛒":Game.cpu.bucket}))
 		return;
